e5f17051277161dfa23b11afb14bb6ffe766e0b7
dhilipsiva <dhilipsiva@gmail.com>
2015-04-16 20:07:18 +0530

    Fixes to file view

--- Brocfile.js
+++ Brocfile.js
@@ -13,4 +13,17 @@ app.import('bower_components/animate.css/animate.min.css');
 
 app.import('vendor/scripts/jquery.drawPieChart.js');
 
+app.import('bower_components/no-vnc/include/util.js')
+app.import('bower_components/no-vnc/include/webutil.js')
+app.import('bower_components/no-vnc/include/base64.js')
+app.import('bower_components/no-vnc/include/websock.js')
+app.import('bower_components/no-vnc/include/des.js')
+app.import('bower_components/no-vnc/include/keysymdef.js')
+app.import('bower_components/no-vnc/include/keyboard.js')
+app.import('bower_components/no-vnc/include/input.js')
+app.import('bower_components/no-vnc/include/display.js')
+app.import('bower_components/no-vnc/include/jsunzip.js')
+app.import('bower_components/no-vnc/include/rfb.js')
+app.import('bower_components/no-vnc/include/keysym.js')
+
 module.exports = app.toTree();
