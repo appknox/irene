73bf2dd95ebbabf64a3ab01dccc612a6abbf6d91
dhilipsiva <dhilipsiva@gmail.com>
2015-04-16 18:34:43 +0530

    Animate.css addded

--- Brocfile.js
+++ Brocfile.js
@@ -9,5 +9,6 @@ var app = new EmberApp({
 app.import('bower_components/socket.io-client/socket.io.js');
 app.import('bower_components/ember-sockets/dist/ember-sockets.js');
 app.import('bower_components/ember-uploader/dist/ember-uploader.js');
+app.import('bower_components/animate.css/animate.min.css');
 
 module.exports = app.toTree();
