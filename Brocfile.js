f983e672e934ccee46944a05fc0abc5c0f891d83
dhilipsiva <dhilipsiva@gmail.com>
2015-04-20 12:22:16 +0530

    Added some deployment packages

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,8 @@
 /* global require, module */
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
+  assetRev = require('broccoli-asset-rev');
+  htmlmin = require('broccoli-htmlmin');
 
 var app = new EmberApp({
   storeConfigInMeta: false
@@ -13,6 +15,9 @@ app.import('bower_components/animate.css/animate.min.css');
 
 app.import('vendor/scripts/jquery.drawPieChart.js');
 
+/*
+ * No VNC includes
+ */
 app.import('bower_components/no-vnc/include/util.js')
 app.import('bower_components/no-vnc/include/webutil.js')
 app.import('bower_components/no-vnc/include/base64.js')
@@ -26,4 +31,43 @@ app.import('bower_components/no-vnc/include/jsunzip.js')
 app.import('bower_components/no-vnc/include/rfb.js')
 app.import('bower_components/no-vnc/include/keysym.js')
 
-module.exports = app.toTree();
+/*
+ * FIXME: THis is a hack to get handlebars working with production builds.
+ * This needs to be fixed when handlebars minification is fixed.
+ *
+ * - dhilipsiva
+ */
+/*
+var index = app.legacyFilesToAppend.indexOf('bower_components/handlebars/handlebars.runtime.js');
+if(index) {
+  app.legacyFilesToAppend[index] = 'bower_components/handlebars/handlebars.js';
+}
+*/
+
+var tree = app.toTree();
+
+options = {
+  quotes: true
+};
+
+if (app.env == 'production') {
+  tree = assetRev(tree, {
+    enabled: true,
+    extensions: ['js', 'css', 'png', 'jpg', 'gif'],
+    replaceExtensions: ['html', 'js', 'css'],
+    prepend: '//du6tdhcax0qep.cloudfront.net/'
+  });
+}
+
+if (app.env == 'staging') {
+  tree = assetRev(tree, {
+    enabled: true,
+    extensions: ['js', 'css', 'png', 'jpg', 'gif'],
+    replaceExtensions: ['html', 'js', 'css'],
+    prepend: '//sherlock-assets-staging.s3-us-west-2.amazonaws.com/'
+  });
+}
+
+tree = htmlmin(tree, options);
+
+module.exports = tree;
