8de3f65da3a9499e91d966102e9158f75ae927cc
dhilipsiva <dhilipsiva@gmail.com>
2016-03-03 13:54:31 +0530

    Working deployment

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM python:3.5.1
 
 MAINTAINER dhilipsiva@gmail.com
 
-RUN echo"=================="; pwd; echo "============="
+RUN echo "=================="; pwd; echo "============="
 
-COPY server.py  /usr/local/server.py
+COPY server.py ./server.py
 
 EXPOSE 8083
 
-CMD /bin/bash -c "python /usr/local/server.py"
+CMD ["python", "./server.py"]
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,7 @@
 set -e
 
 rm -rf dist
+mkdir dist
 
 # ember build --environment production
 # sed -i 's/\.\.\/fonts/\/\/staging-assets.appknox.com\/fonts/g' dist/assets/vendor-*.css
@@ -16,17 +17,13 @@ rm -rf dist
 # gsutil -m cp -R dist/* gs://staging-assets.appknox.com
 # gsutil -m acl set -R -a public-read gs://staging-assets.appknox.com
 
-ember build --environment production
-sed -i 's/\.\.\/fonts/\/\/du6tdhcax0qep.cloudfront.net\/fonts/g' dist/assets/vendor-*.css
-aws s3 rm --recursive s3://sherlock-assets-v2/
-aws s3 sync dist/ s3://sherlock-assets-v2/ --acl public-read
+# ember build --environment production
+# sed -i 's/\.\.\/fonts/\/\/du6tdhcax0qep.cloudfront.net\/fonts/g' dist/assets/vendor-*.css
+# aws s3 rm --recursive s3://sherlock-assets-v2/
+# aws s3 sync dist/ s3://sherlock-assets-v2/ --acl public-read
 
-ember build --environment production
-# cp Procfile dist/
-# cp .buildpacks dist/
 cp Dockerfile dist/
 cp server.py dist/
-# sed -i 's/\.\.\/fonts/\/\/du6tdhcax0qep.cloudfront.net\/fonts/g' dist/assets/vendor-*.css
 cd dist
 git init
 git add server.py
