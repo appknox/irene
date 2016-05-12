9134776b6fe4c9c386d479f9479b21b77cb4b21f
dhilipsiva <dhilipsiva@gmail.com>
2016-05-12 11:38:17 +0530

    Add a procfile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,17 +11,19 @@ set -e
 rm -rf dist
 mkdir dist
 
-# ember build --environment production
-# sed -i 's/\.\.\/fonts/\/\/staging-assets.appknox.com\/fonts/g' dist/assets/vendor-*.css
-# gsutil -m rm gs://staging-assets.appknox.com/**
-# gsutil -m cp -R dist/* gs://staging-assets.appknox.com
-# gsutil -m acl set -R -a public-read gs://staging-assets.appknox.com
-
 ember build --environment production
-sed -i 's/\.\.\/fonts/\/\/sherlock-assets-v2.s3.amazonaws.com\/fonts/g' dist/assets/vendor-*.css
-sed -i 's/\.\.\/fonts/\/\/sherlock-assets-v2.s3.amazonaws.com\/fonts/g' dist/assets/vendor-*.css
-aws s3 rm --recursive s3://sherlock-assets-v2/
-aws s3 sync dist/ s3://sherlock-assets-v2/ --acl public-read
+# find ./dist/assets -name "*.css" -exec sed -i 's/\.\.\/fonts/https:\/\/storage.googleapis.com\/deis-staging/\/fonts/g' {} \;
+# sed -i 's/\.\.\/fonts/https:\/\/storage.googleapis.com\/deis-staging/\/fonts/g' dist/assets/*.css
+gsutil -m rm gs://deis-staging/**
+gsutil -m cp -R dist/* gs://deis-staging
+gsutil -m acl set -R -a public-read gs://deis-staging
+https://apk.apk
+
+# ember build --environment production
+# sed -i 's/\.\.\/fonts/\/\/sherlock-assets-v2.s3.amazonaws.com\/fonts/g' dist/assets/vendor-*.css
+# sed -i 's/\.\.\/fonts/\/\/sherlock-assets-v2.s3.amazonaws.com\/fonts/g' dist/assets/vendor-*.css
+# aws s3 rm --recursive s3://sherlock-assets-v2/
+# aws s3 sync dist/ s3://sherlock-assets-v2/ --acl public-read
 
 cp Dockerfile dist/
 cp server.py dist/
@@ -31,5 +33,5 @@ git add server.py
 git add Dockerfile
 git add index.html
 git commit -m "Initial Commit"
-git remote add deis ssh://git@deis.suttawala.co:2222/irene.git
+git remote add deis ssh://git@deis-builder.104.154.26.43.xip.io:2222/irene.git
 git push -f deis master
