4c58a8b17313e27b582953b1cabc42c5f0cfc4da
dhilipsiva <dhilipsiva@gmail.com>
2016-09-01 12:06:26 +0530

    Add support for bohemia

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: 'sass',
+      includePaths: [
+        'bower_components/bohemia/'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
