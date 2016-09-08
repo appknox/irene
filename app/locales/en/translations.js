be361410c600eb6cac388c6ba4e728d71db76ce8
dhilipsiva <dhilipsiva@gmail.com>
2016-09-08 18:25:57 +0530

    Implement translations

---
+++ app/locales/en/translations.js
@@ -0,0 +1,18 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  "auth": {
+    "login": "Login",
+    "forgotPassword": "Forgot Password?",
+    "username": "Username",
+    "password": "Password",
+    "greet": "Security fanatics at your service"
+  }
+};
