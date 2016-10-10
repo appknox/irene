bb1212f9b17ef599d38879fc8ab263ba6e30a96a
dhilipsiva <dhilipsiva@gmail.com>
2016-10-10 12:02:19 +0530

    Make mirage generate relational data

--- mirage/factories/file.js
+++ mirage/factories/file.js
@@ -2,16 +2,15 @@ import { faker } from 'ember-cli-mirage';
 import Base from './base';
 
 export default Base.extend({
-  // uuid: DS.attr 'string'
-  // deviceToken: DS.attr 'string'
   version: faker.random.number,
   iconUrl: faker.internet.avatar,
   md5hash: faker.random.number,
   sha1hash: faker.random.number,
-  name: faker.company.companyName,
   dynamicStatus: faker.random.arrayElement([0, 1, 2]),
-  // analysis: DS.hasMany 'analysis', inverse: 'file'
   report: faker.internet.avatar,
   manual: faker.internet.boolean,
 
+  name(){
+    return faker.company.companyName();
+  }
 });
--- mirage/factories/project.js
+++ mirage/factories/project.js
@@ -2,14 +2,11 @@ import { faker } from 'ember-cli-mirage';
 import Base from './base';
 
 export default Base.extend({
-  // owner: DS.belongsTo 'user', inverse: 'ownedProjects'
   name: faker.company.companyName,
   packageName:  faker.internet.domainName,
   platform: faker.random.arrayElement([0, 1]),
   source: faker.random.arrayElement([0, 1, 2]),
   version : faker.random.number,
-  // files: DS.hasMany 'file', inverse :'project'
-  // collaboration: DS.hasMany 'collaboration', inverse: 'project'
   fileCount: faker.random.number,
   githubRepo: faker.random.firstName,
   jiraProject: faker.random.firstName,
