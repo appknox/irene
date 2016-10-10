23e7e3e629e850347737a7bf854d5b1ae4172857
dhilipsiva <dhilipsiva@gmail.com>
2016-10-10 12:02:19 +0530

    Make mirage generate relational data

--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -10,14 +10,10 @@ export default Factory.extend({
   emailmd5:  faker.name.firstName,
   firstName: faker.name.firstName,
   lastName: faker.name.lastName,
-  // ownedProjects: DS.hasMany 'project', inverse:'owner'
-  // submissions: DS.hasMany 'submission', inverse:'user'
   namespaces: faker.internet.domainName,
-  // collaboration: DS.hasMany 'collaboration', inverse:'user'
   expiryDate: faker.date.future,
   hasGithubToken: faker.random.boolean,
   hasJiraToken: faker.random.boolean,
-  // socketId: faker.random.uuid,
   limitedScans: faker.random.number,
   scansLeft: faker.random.number
 });
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -7,37 +7,26 @@ function getRandomInt(min, max) {
 }
 
 export default function(server) {
-
-  /*
-    Seed your development database using your factories.
-    This data will not be loaded in your tests.
-
-    Make sure to define a factory for each model you want to create.
-    */
-
-  var userCount = getRandomInt(3, 10),
+  var userCount = getRandomInt(3, 5),
     pricingCount = getRandomInt(3, 3),
-    vulnerabilityCount = getRandomInt(10, 20),
-    projectId = 0, userId = 0, projectCount = 0,
-    project = null, file = null;
+    vulnerabilityCount = getRandomInt(5, 15),
+    projectCount = 0, project = null, file = null;
   server.createList('user', userCount);
   server.createList('pricing', pricingCount);
   server.createList('vulnerability', vulnerabilityCount);
-  projectCount =  getRandomInt(0, 10);
-  for (projectId = 1; projectId < projectCount; projectId++) {
+  projectCount =  getRandomInt(1, 5);
+  for (var projectId = 1; projectId <= projectCount; projectId++) {
     project = server.create('project', {user: 1});
     var fileCount = getRandomInt(1, 4);
-    for (var fileId = 1; fileId < fileCount; fileId++) {
+    for (var fileId = 1; fileId <= fileCount; fileId++) {
       file = server.create('file', {project: project.id});
-      for (var vulnerabilityId = 1; vulnerabilityId < vulnerabilityCount; vulnerabilityId++) {
+      for (var vulnerabilityId = 1; vulnerabilityId <= vulnerabilityCount; vulnerabilityId++) {
         server.createList('analysis', fileCount, {file: file.id, vulnerability: vulnerabilityId});
       }
     }
     server.db.projects.update(project.id, {lastFile: file.id});
-  }
-  for (projectId = 0; projectId < projectCount; projectId++) {
     var collaborationCount = getRandomInt(0, userCount);
-    for (userId=1; userId <= collaborationCount; userId++) {
+    for (var userId = 1; userId <= collaborationCount; userId++) {
       server.create('collaboration', {project: projectId, user: userId});
     }
   }
