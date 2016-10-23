9f1e375eb7a04b77978f5286237c619781a0651b
Subho Halder <subho.halder@gmail.com>
2016-10-24 04:32:09 +0800

    fix: Keep consistent naming, either Home or Dashboard

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -11,7 +11,7 @@ export default {
   "confirmPassword": "Confirm Password",
   "created": "Created",
   "currentPassword": "Current Password",
-  "dashboard": "Dashboard",
+  "dashboard": "Home",
   "description": "Description",
   "deviceBooting": "Device is booting",
   "deviceShuttingDown": "Device is shutting down",
