d3265693b6a7697bf736c797347446f9ad471861
dhilipsiva <dhilipsiva@gmail.com>
2016-11-21 15:23:22 +0530

    Enable all logging to investiga an error ...
    ... in production

--- config/environment.js
+++ config/environment.js
@@ -154,6 +154,11 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV['ember-cli-mirage'] = {
       enabled: false
     };
