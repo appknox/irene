99181fe228795cdb4fa504e15c33b461feb29095
Subho Halder <subho.halder@gmail.com>
2016-12-03 21:58:48 +0530

    For staging, use environment staging

--- config/deploy.js
+++ config/deploy.js
@@ -22,7 +22,7 @@ module.exports = function(deployTarget) {
   }
 
   if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
+    ENV.build.environment = 'staging';
     // configure other plugins for staging deploy target here
 
   }
