7ef63e86611996a922b981698dda1cf2cc3ed84c
Subho Halder <subho.halder@gmail.com>
2016-12-05 16:20:25 +0530

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,3 +7,14 @@
 #
 
 ember deploy production --verbose=true --activate=true --show-progress=true
+
+ACCESS_TOKEN=f1d76383c2d346a3aeedc21823f18c76
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git log -n 1 --pretty=format:"%H"`
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ACCESS_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
