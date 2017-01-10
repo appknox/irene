9281552538211ef8290abb37f919511aa938518f
Subho Halder <subho.halder@gmail.com>
2017-01-11 00:27:49 +0530

    enhance: remove unrequired code

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -66,11 +66,6 @@ module.exports = function(defaults) {
    */
   app.import('bower_components/pace/pace.js');
 
-  // /*
-  //  * Include OpBeat
-  //  */
-  // app.import('bower_components/opbeat-js/opbeat.min.js')
-
   app.import('bower_components/zeroclipboard/dist/ZeroClipboard.min.js');
   app.import('bower_components/zeroclipboard/dist/ZeroClipboard.swf', {
     destDir: 'assets'
