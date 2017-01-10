8d93e3725ff34b1a282b4ac38af2474da6d0667c
Subho Halder <subho.halder@gmail.com>
2017-01-11 00:25:29 +0530

    deployment script update the opbeat also

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,3 +18,9 @@ curl https://api.rollbar.com/api/1/deploy/ \
   -F environment=$ENVIRONMENT \
   -F revision=$REVISION \
   -F local_username=$LOCAL_USERNAME
+
+curl https://intake.opbeat.com/api/v1/organizations/1ff25e9c6a1d40bbad1293635d201fcb/apps/61501c19d2/releases/ \
+  -H "Authorization: Bearer afe70fde72c21c3fc240c182d027551edb305eb9" \
+  -d rev=`git log -n 1 --pretty=format:%H` \
+  -d branch=`git rev-parse --abbrev-ref HEAD` \
+  -d status=completed
