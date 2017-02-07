734a5f863c1ba47468c0eeb189f60b59f3dc5c02
yashwin <yashwinpoojary@gmail.com>
2017-02-07 13:13:15 +0530

    enhance: add mirage factory and model for invoices

---
+++ mirage/factories/invoice.js
@@ -0,0 +1,11 @@
+import { Factory, faker } from 'ember-cli-mirage';
+import ENUMS from 'irene/enums';
+
+export default Factory.extend({
+  amount: faker.commerce.price,
+  paidOn: faker.date.past,
+
+  source(){
+    return faker.random.arrayElement(ENUMS.PAYMENT_SOURCE.VALUES);
+  },
+});
