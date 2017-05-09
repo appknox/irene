2620df4591556fde7cd878dc63697465ef5b16ce
chillaranand <anand21nanda@gmail.com>
2017-05-09 12:43:05 +0530

    Updated node version in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-sudo pip install --upgrade pip
+set -x
+
+sudo -H pip install --upgrade pip
 sudo -H pip install nodeenv
 
 mkdir -p ~/.nodeenvs/
-nodeenv ~/.nodeenvs/irene --node=0.12.13
+nodeenv ~/.nodeenvs/irene --node=7.10.0
 . ~/.nodeenvs/irene/bin/activate
 npm install
 npm install -g bower
