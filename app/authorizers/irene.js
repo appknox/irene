bd9464e5e5962d41d14dbe6356d9dccd6d56f246
yashwin <yashwinpoojary@gmail.com>
2018-02-12 18:51:03 +0530

    refactor: decaffeinate authorizers

---
+++ app/authorizers/irene.js
@@ -0,0 +1,14 @@
+import Base from 'ember-simple-auth/authorizers/base';
+import ENV from 'irene/config/environment';
+
+
+const IreneAuthorizer = Base.extend({
+
+  authorize(data, block){
+    block('Authorization', `Basic ${data.b64token}`);
+    return block('X-Product', ENV.product);
+  }
+});
+
+
+export default IreneAuthorizer;
