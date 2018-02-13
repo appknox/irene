2efb477b79aee5a5966db26a130518099acedad5
yashwin <yashwinpoojary@gmail.com>
2018-02-13 11:00:48 +0530

    refactor: decaffeinate other app files

---
+++ app/router.js
@@ -0,0 +1,50 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+import Ember from 'ember';
+import ENV from 'irene/config/environment';
+
+const Router = Ember.Router.extend({
+  location: ENV.locationType,
+  rootURL: ENV.rootURL
+});
+
+Router.map(function() {
+  this.route('freestyle');
+  this.route('login');
+  this.route('recover');
+  this.route('reset', {path: '/reset/:uuid/:token'});
+  this.route('setup', {path: '/setup/:uuid/:token'});
+  this.route('authenticated', {path: '/'}, function() {
+    this.route("index", {path: '/'});
+    this.route("teams", {path: '/teams'});
+    this.route("team", {path: '/team/:teamId'});
+    this.route("settings", {path: '/settings'});
+    this.route("billing", {path: '/billing'});
+    this.route('projects', {path: '/projects'});
+    this.route("project", {path: '/project/:projectId'}, function() {
+      this.route('settings');
+      return this.route('files');
+    });
+    this.route("file", {path: '/file/:fileId'});
+    this.route("choose",{path: '/choose/:fileId'});
+    this.route('compare', {path: '/compare/:files'});
+    this.route('payment-success');
+    return this.route('payment-failure');
+  });
+  this.route('invitation', {path: '/invitation/:uuid'});
+
+  // 404 path -this should be at the last.
+  return this.route('not-found', {path: '/*path'});
+});
+
+const CSBMap = {
+  "authenticated.settings": {feature: "Account Settings", module: "Setup", product: "Appknox" },
+  "authenticated.project.files": { feature: "All Scans", module: "Security", product: "Appknox" },
+  "authenticated.choose": { feature: "Compare Scans", module: "Security", product: "Appknox" }
+};
+
+
+export { Router as default, CSBMap as CSBMap};
