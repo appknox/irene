90e74d0f79dd428d4fb395da33bf428e7f8b80fa
yashwin <yashwinpoojary@gmail.com>
2018-02-21 12:08:16 +0530

    chore: more test case

--- tests/unit/models/collaboration-test.js
+++ tests/unit/models/collaboration-test.js
@@ -1,10 +1,16 @@
 import { moduleForModel, test} from 'ember-qunit';
 
+
 moduleForModel('collaboration', 'Unit | Model | collaboration', {
   needs: ['model:project', 'model:user', 'model:team']
 });
 
 test('it exists', function(assert) {
   const collaboration = this.subject();
-  assert.ok(collaboration.get('hasRole'));
+  assert.equal(collaboration.get('hasRole'), true);
+});
+
+test('it exists', function(assert) {
+  const collaboration = this.subject();
+  assert.equal(collaboration.get('roleHumanized'), "noPreference");
 });
