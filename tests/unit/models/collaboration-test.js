1d67a623225500e200bcfd009de84b61c654b142
yashwin <yashwinpoojary@gmail.com>
2018-02-21 12:38:49 +0530

    chore: test case for switch

--- tests/unit/models/collaboration-test.js
+++ tests/unit/models/collaboration-test.js
@@ -1,16 +1,17 @@
 import { moduleForModel, test} from 'ember-qunit';
 
-
 moduleForModel('collaboration', 'Unit | Model | collaboration', {
   needs: ['model:project', 'model:user', 'model:team']
 });
 
 test('it exists', function(assert) {
   const collaboration = this.subject();
-  assert.equal(collaboration.get('hasRole'), true);
+  assert.equal(collaboration.get('hasRole'), true, "Has Role");
 });
 
 test('it exists', function(assert) {
   const collaboration = this.subject();
-  assert.equal(collaboration.get('roleHumanized'), "noPreference");
+  assert.equal(collaboration.get('roleHumanized'), "noPreference", "No Preference");
+  collaboration.set('roleHumanized', "admin");
+  assert.equal(collaboration.get('roleHumanized'), "admin", "Admin");
 });
