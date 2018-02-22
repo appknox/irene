14264fb4e36be4f6be3c547df31630bd52ecb3b1
yashwin <yashwinpoojary@gmail.com>
2018-02-22 18:39:21 +0530

    test: for collaboration model

--- tests/unit/models/collaboration-test.js
+++ tests/unit/models/collaboration-test.js
@@ -7,5 +7,7 @@ moduleForModel('collaboration', 'Unit | Model | collaboration', {
 test('it exists', function(assert) {
   const collaboration = this.subject();
   assert.equal(collaboration.get('hasRole'), true, "Has Role");
+  collaboration.set('hasRole', false);
+  assert.equal(collaboration.get('hasRole'), false, "No role");
   assert.equal(collaboration.get('roleHumanized'), "noPreference", "No Preference");
 });
