14a4ab1319ea2d9bffa4505b10ebd0721ea10b86
yashwin <yashwinpoojary@gmail.com>
2018-02-23 14:16:29 +0530

    test: testing model functions

--- tests/unit/models/analysis-test.js
+++ tests/unit/models/analysis-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('analysis', 'Unit | Model | analysis', {
 
@@ -8,5 +9,14 @@ moduleForModel('analysis', 'Unit | Model | analysis', {
 test('it exists', function(assert) {
   const analysis = this.subject();
   assert.equal(analysis.get('isScanning'), 0, "Unknown");
+  let store = this.store();
+  Ember.run(function() {
+    assert.equal(analysis.hasType(), false, "No type");
+    analysis.set('vulnerability', store.createRecord('vulnerability', {
+        types: [1,2,3]
+      })
+    );
+    assert.equal(analysis.hasType(1), true, "Has Type");
+  });
   assert.equal(analysis.get('isRisky'), true, "Is Risky");
 });
