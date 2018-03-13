52f4bf775a93795f32a35fa24a5a41edc377764f
yashwin <yashwinpoojary@gmail.com>
2018-03-13 18:43:39 +0530

    enhance: mixin test case (COVERAGE = 60.53%)

--- tests/unit/mixins/base-model-test.js
+++ tests/unit/mixins/base-model-test.js
@@ -1,11 +1,31 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import BaseModelMixin from 'irene/mixins/base-model';
-import { module, test } from 'qunit';
+import { moduleFor, test } from 'ember-qunit';
 
-module('Unit | Mixin | base model');
+moduleFor('mixin:base-model', 'Unit | Mixin | base model', {
+  needs: [
+    'model:user'
+  ],
 
-test('it works', function(assert) {
-  const BaseModelObject = Ember.Object.extend(BaseModelMixin);
-  const subject = BaseModelObject.create();
-  assert.ok(subject);
+  subject() {
+    const BaseModelObject = DS.Model.extend(BaseModelMixin);
+    this.register('model:base-model-object', BaseModelObject);
+    return Ember.run(() => {
+      let store = Ember.getOwner(this).lookup('service:store');
+      return store.createRecord('base-model-object', {});
+    });
+  }
+});
+
+test('the mixin does what it should', function(assert) {
+  const mixin = this.subject();
+
+  Ember.run(() => {
+    assert.notOk(mixin.get('createdOnHumanized'));
+    assert.notOk(mixin.get('createdOnDateTime'));
+    mixin.set("createdOn", new Date("25 March 2015"));
+    assert.equal(mixin.get('createdOnHumanized'), "March 25, 2015", "Created On Humanized");
+    assert.equal(mixin.get('createdOnDateTime'), "Wed Mar 25 2015, 00:00:00", "Created On Date Time");
+  });
 });
