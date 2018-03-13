5826992cb33d6ec4c111fdc5fa7c767391d54290
yashwin <yashwinpoojary@gmail.com>
2018-03-13 20:04:34 +0530

    fix: circle ci test case

--- tests/unit/mixins/base-model-test.js
+++ tests/unit/mixins/base-model-test.js
@@ -26,6 +26,6 @@ test('the mixin does what it should', function(assert) {
     assert.notOk(mixin.get('createdOnDateTime'));
     mixin.set("createdOn", new Date("25 March 2015"));
     assert.equal(mixin.get('createdOnHumanized'), "March 25, 2015", "Created On Humanized");
-    assert.equal(mixin.get('createdOnDateTime'), "Wed Mar 25 2015, 00:00:00", "Created On Date Time");
+    assert.equal(mixin.get('createdOnDateTime'), "Wed Mar 25 2015, 12:00:00 AM GMT", "Created On Date Time");
   });
 });
