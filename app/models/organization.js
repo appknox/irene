5cc0ea4fb2ddf03de998f43501a324b66f3f99d3
yashwin <yashwinpoojary@gmail.com>
2018-03-26 18:06:06 +0530

    enhance: display actual count

--- app/models/organization.js
+++ app/models/organization.js
@@ -3,6 +3,9 @@ import BaseModelMixin from 'irene/mixins/base-model';
 
 const Organization = DS.Model.extend(BaseModelMixin, {
   name: DS.attr('string'),
+  userCount: DS.attr('number'),
+  teamCount: DS.attr('number'),
+  invitationCount: DS.attr('number'),
   users: DS.hasMany('user', {inverse: 'organization'}),
   teams: DS.hasMany('team', {inverse: 'organization'})
 });
