3bef31ee9cd338abee44b81ec5cdbcd49c0140fb
yashwin <yashwinpoojary@gmail.com>
2018-04-09 19:39:40 +0530

    fix: minor ui fix

--- app/components/file-header.js
+++ app/components/file-header.js
@@ -218,10 +218,10 @@ const FileHeaderComponent = Ember.Component.extend({
           owaspM6Count, owaspM7Count, owaspM8Count, owaspM9Count, owaspM10Count
         ],
         backgroundColor: [
-         "#e6194b", "#3cb44b", "#ffe119", "#0082c8", "#f58231", 
-         "#e6194b", "#911eb4", "#46f0f0", "#f032e6", "#d2f53c",
-         "#fabebe", "#008080", "#e6beff", "#aa6e28", "#fffac8",
-         "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000080",
+         "#36A2EB", "#36A2EB", "#36A2EB", "#36A2EB", "#36A2EB",
+         "#36A2EB", "#36A2EB", "#36A2EB", "#36A2EB", "#36A2EB",
+         "#00008b", "#00008b", "#00008b", "#00008b", "#00008b",
+         "#00008b", "#00008b", "#00008b", "#00008b", "#00008b",
         ]
       } ]
     };
