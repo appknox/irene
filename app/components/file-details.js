51e20ad7317bf3462d656507c969def8375814ba
yashwin <yashwinpoojary@gmail.com>
2018-04-10 14:17:57 +0530

    feat: sort analysis by impact level

--- app/components/file-details.js
+++ app/components/file-details.js
@@ -10,6 +10,8 @@ const FileDetailsComponent = Ember.Component.extend({
     return this.get("file.sortedAnalyses");
   }).property("file.sortedAnalyses"),
 
+  sortImpactAscending: false,
+
   filteredAnalysis: Ember.computed('analyses', 'vulnerabilityType',  function() {
     const vulnerabilityType = parseInt(this.get("vulnerabilityType"));
     const analyses = this.get("analyses");
@@ -22,13 +24,31 @@ const FileDetailsComponent = Ember.Component.extend({
         filteredAnalysis.push(analysis);
       }
     }
-    return filteredAnalysis;
+    return this.set("analyses", filteredAnalysis);
   }),
+
+  sortedAnalyses: Ember.computed.sort('analyses', 'analysesSorting'),
 
   actions: {
     filterVulnerabilityType() {
+      this.set("analyses", this.get("file.sortedAnalyses"));
+      this.set("sortImpactAscending", false);
       const select = $(this.element).find("#filter-vulnerability-type");
       this.set("vulnerabilityType", select.val());
+    },
+
+    sortImpact() {
+      const sortImpactAscending = this.get("sortImpactAscending");
+      if(!sortImpactAscending) {
+        this.set("analysesSorting", ['risk:asc']);
+        this.set("sortImpactAscending", true);
+      }
+      else {
+        this.set("analysesSorting", ['risk:desc']);
+        this.set("sortImpactAscending", false);
+      }
+      const sortedAnalyses = this.get("sortedAnalyses");
+      this.set("analyses", sortedAnalyses);
     }
   }
 });
