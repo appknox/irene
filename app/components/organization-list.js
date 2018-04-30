78de85d9c3328d83a969b4aeddfd6d358237f51d
yashwin <yashwinpoojary@gmail.com>
2018-04-30 18:26:33 +0530

    enhance: organisations redirects to org users

--- app/components/organization-list.js
+++ app/components/organization-list.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  organizations: (function() {
-    return this.get("store").query('organization', {id: null});
-  }).property(),
 });
--- app/routes/authenticated/organizations.js
+++ app/routes/authenticated/organizations.js
@@ -3,5 +3,15 @@ import config from 'irene/config/environment';
 import ScrollTopMixin from 'irene/mixins/scroll-top';
 
 export default Ember.Route.extend(ScrollTopMixin, {
-  title: `Organizations${config.platform}`
+  title: `Organizations${config.platform}`,
+  model() {
+    return this.get("store").query('organization', {id: null});
+  },
+  redirect(model, transition){
+    const currentRoute = transition.targetName;
+    if(currentRoute === "authenticated.organizations") {
+        this.transitionTo(`/organization/${model.content[0].id}/users`);
+    }
+  }
+
 });
