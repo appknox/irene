7a1b3d10fd80e4c31d113bc1bc286f6b68e79bf0
yashwin <yashwinpoojary@gmail.com>
2018-05-09 14:16:25 +0530

    fix: circle ci test case

--- tests/integration/components/pcidss-details-test.js
+++ tests/integration/components/pcidss-details-test.js
@@ -12,14 +12,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{pcidss-details}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#pcidss-details}}
-      template block text
-    {{/pcidss-details}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'more details');
 });
