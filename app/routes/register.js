2ee6480b29634cb539327af3035d8292d7943c44
Sharat M R <cosmosgenius@gmail.com>
2018-05-10 18:30:15 +0530

    Fix recaptcha issue

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 import config from 'irene/config/environment';
+import RSVP from 'rsvp';
 
 const RegisterRoute = Ember.Route.extend({
-  title: `Register${config.platform}`
+  title: `Register${config.platform}`,
+  model() {
+    return new RSVP.Promise(function(resolve){
+      window.grecaptcha.ready(function(){
+        resolve({});
+      });
+    });
+  }
 });
 
 export default RegisterRoute;
