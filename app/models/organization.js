638514e6f5ecf296008b73b5fd63a4bea198d7a6
Sharat M R <cosmosgenius@gmail.com>
2018-08-15 22:15:16 +0530

    Add member relationship

--- app/models/organization.js
+++ app/models/organization.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 const Organization = DS.Model.extend({
   name: DS.attr('string'),
-  logo: DS.attr('string')
+  logo: DS.attr('string'),
+  members: DS.hasMany('organization-user')
 });
 
 export default Organization;
--- app/serializers/organization-user.js
+++ app/serializers/organization-user.js
@@ -1,19 +1,5 @@
 import DRFSerializer from './drf';
 
 export default DRFSerializer.extend({
-  normalizeResponse: function (store, primaryModelClass, payload) {
-    return {
-      data: payload.results.map((item)=> {
-        return {
-          id: item.id,
-          type: 'organization-user',
-          attributes: {
-            role: item.role,
-            username: item.username,
-            email: item.email
-          }
-        };
-      })
-    };
-  }
+  primaryKey: 'member',
 });
