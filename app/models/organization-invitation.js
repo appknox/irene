5ea9b918e510d4d8767a38554c125836312c8179
Praseetha-KR <praseetha04@gmail.com>
2018-08-22 21:44:41 +0530

    Cleanup organization-invitation

--- app/models/organization-invitation.js
+++ app/models/organization-invitation.js
@@ -1,6 +1,4 @@
-import Ember from 'ember';
 import DS from 'ember-data';
-import dateTime from 'irene/utils/date-time';
 
 const OrganizationInvitation = DS.Model.extend({
   email: DS.attr('string'),
@@ -8,14 +6,6 @@ const OrganizationInvitation = DS.Model.extend({
   updatedOn: DS.attr('date'),
   team: DS.belongsTo('organization-team'),
   organization: DS.belongsTo('organization'),
-
-  createdOnHumanized: Ember.computed("createdOn", function() {
-    return dateTime(this.get("createdOn"));
-  }),
-
-  updatedOnHumanized: Ember.computed("updatedOn", function() {
-    return dateTime(this.get("updatedOn"));
-  }),
 });
 
 export default OrganizationInvitation;
