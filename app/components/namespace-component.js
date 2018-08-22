a270240a8070a38ffda5dec7da9727ff64a5157a
Praseetha-KR <praseetha04@gmail.com>
2018-08-22 22:27:47 +0530

    Cleanup namespace component

--- app/components/namespace-component.js
+++ app/components/namespace-component.js
@@ -1,59 +1,19 @@
 import Ember from 'ember';
-import ENV from 'irene/config/environment';
-import { translationMacro as t } from 'ember-i18n';
-import triggerAnalytics from 'irene/utils/trigger-analytics';
 import PaginateMixin from 'irene/mixins/paginate';
 
 const NamespaceComponentComponent = Ember.Component.extend(PaginateMixin, {
-
-  namespace: "",
-  added: false,
-  classNames: ["column"],
-  isAddingNamespace: false,
   i18n: Ember.inject.service(),
-  ajax: Ember.inject.service(),
-  notify: Ember.inject.service('notification-messages-service'),
-  tRequestToAddNamespace: t("requestToAddNamespace"),
-  tPleaseEnterAnyNamespace: t("pleaseEnterAnyNamespace"),
+  namespace: '',
+  classNames: ['column'],
 
   targetObject: 'organization-namespace',
   sortProperties: ['created:desc'],
-
   extraQueryStrings: Ember.computed('organization.id', function() {
     const query = {
-      id: this.get("organization.id")
+      id: this.get('organization.id')
     };
     return JSON.stringify(query, Object.keys(query).sort());
   }),
-
-
-  actions: {
-    addNamespace() {
-      const tRequestToAddNamespace = this.get("tRequestToAddNamespace");
-      const tPleaseEnterAnyNamespace = this.get("tPleaseEnterAnyNamespace");
-      const namespace = this.get("namespace");
-      if (!namespace) {
-        return this.get("notify").error(tPleaseEnterAnyNamespace, ENV.notifications);
-      }
-      const data =
-        {namespace};
-      triggerAnalytics('feature',ENV.csb.namespaceAdded);
-      this.set("isAddingNamespace", true);
-      this.get("ajax").post(ENV.endpoints.namespaceAdd, {data})
-      .then(() => {
-        this.get("notify").success(tRequestToAddNamespace);
-        if(!this.isDestroyed) {
-          this.set("isAddingNamespace", false);
-          this.set("namespace", "");
-        }
-      }, (error) => {
-        if(!this.isDestroyed) {
-          this.set("isAddingNamespace", false);
-          this.get("notify").error(error.payload.message);
-        }
-      });
-    }
-  }
 });
 
 export default NamespaceComponentComponent;
--- app/components/namespace-overview.js
+++ app/components/namespace-overview.js
@@ -1,18 +1,21 @@
 import Ember from 'ember';
 import { task } from 'ember-concurrency';
+import ENV from 'irene/config/environment';
+import triggerAnalytics from 'irene/utils/trigger-analytics';
 
 const NamespaceComponentComponent = Ember.Component.extend({
-  tagName: ["tr"],
   i18n: Ember.inject.service(),
   ajax: Ember.inject.service(),
   notify: Ember.inject.service('notification-messages-service'),
 
+  tagName: ['tr'],
   showRejectNamespaceConfirm: false,
   isRejectingNamespace: false,
 
   approveNamespace: task(function * () {
     const ns = this.get('namespace');
     ns.set('isApproved', true);
+    triggerAnalytics('feature', ENV.csb.namespaceAdded);
     yield ns.save();
   }),
 
@@ -21,11 +24,12 @@ const NamespaceComponentComponent = Ember.Component.extend({
   }),
 
   confirmReject: task(function * () {
-    this.set("isRejectingNamespace", true);
+    this.set('isRejectingNamespace', true);
     const ns = this.get('namespace');
     ns.deleteRecord();
     yield ns.save();
   }),
+
   actions: {
     confirmRejectProxy() {
       this.get('confirmReject').perform();
