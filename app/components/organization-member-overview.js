b4d89706f41dfb8b112ce2b02f65690c971fec3f
Praseetha-KR <praseetha04@gmail.com>
2018-08-24 22:44:10 +0530

    concurrent task for selectMemberRole

--- app/components/organization-member-overview.js
+++ app/components/organization-member-overview.js
@@ -1,45 +1,29 @@
 import Ember from 'ember';
 import ENUMS from 'irene/enums';
-import ENV from 'irene/config/environment';
 import { translationMacro as t } from 'ember-i18n';
+import { task } from 'ember-concurrency';
+import { on } from '@ember/object/evented';
 
 export default Ember.Component.extend({
-
   i18n: Ember.inject.service(),
   ajax: Ember.inject.service(),
   notify: Ember.inject.service('notification-messages-service'),
-  user: null,
-  organization: null,
   tagName: ["tr"],
-
+  member: null,
+  organization: null,
   tUserRoleUpdated: t("userRoleUpdated"),
-
   roles: ENUMS.ORGANIZATION_ROLES.CHOICES.slice(0, -1),
 
-  filteredUserRoles: (function() {
-    const roles = this.get("roles");
-    const userRole = this.get("user.role");
-    return roles.filter(role => userRole !== role.value);
-  }).property("roles", "user.role"),
-
-  actions: {
-    selectUserRole() {
-      const userRole = parseInt(this.$('#org-user-role').val());
-      const orgId= this.get("organization.id");
-      const userId= this.get("user.id");
-      const tUserRoleUpdated= this.get("tUserRoleUpdated");
-      const data = {
-        role: userRole
-      };
-      const url = [ENV.endpoints.organizations, orgId, ENV.endpoints.users, userId].join('/');
-      const that = this;
-      this.get("ajax").put(url, {data})
-      .then(function() {
-        that.get("notify").success(tUserRoleUpdated);
-      })
-      .catch(function(error){
-        that.get("notify").error(error.payload.message);
-      });
-    }
-  }
+  selectMemberRole: task(function * () {
+    const role = parseInt(this.$('#org-user-role').val());
+    const member = this.get('member');
+    member.set('role', role);
+    yield member.save()
+  }).evented(),
+  selectMemberRoleSucceeded: on('selectMemberRole:succeeded', function() {
+    this.get('notify').success(this.get('tUserRoleUpdated'));
+  }),
+  selectMemberRoleErrored: on('selectMemberRole:errored', function(_, error) {
+    this.get("notify").error(error.message);
+  }),
 });
