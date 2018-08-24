ea568dd69c2535951c97c8dff415ede44f254c32
Praseetha-KR <praseetha04@gmail.com>
2018-08-24 17:45:33 +0530

    Use calculated org obj property counts

--- app/models/organization.js
+++ app/models/organization.js
@@ -5,12 +5,21 @@ const Organization = DS.Model.extend({
   name: DS.attr('string'),
   logo: DS.attr('string'),
   members: DS.hasMany('organization-member'),
-  projectsCount: DS.attr('number'),
-  namespacesCount: DS.attr('number'),
-  teamsCount: DS.attr('number'),
+  namespaces: DS.hasMany('organization-namespace'),
+  projects: DS.hasMany('organization-project'),
+  teams: DS.hasMany('organization-team'),
   membersCount: Ember.computed('members', function() {
     return this.get('members.meta.count');
-  })
+  }),
+  namespacesCount: Ember.computed('namespaces', function() {
+    return this.get('namespaces.meta.count');
+  }),
+  projectsCount: Ember.computed('projects', function() {
+    return this.get('projects.meta.count');
+  }),
+  teamsCount: Ember.computed('teams', function() {
+    return this.get('teams.meta.count');
+  }),
 });
 
 export default Organization;
