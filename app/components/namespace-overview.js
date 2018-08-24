2ff6362e970e0b9cf1e20be3e85fda0fcefc0da7
Praseetha-KR <praseetha04@gmail.com>
2018-08-24 17:40:05 +0530

    Lifecycle hooks for member & invite concurrency tasks

--- app/components/namespace-overview.js
+++ app/components/namespace-overview.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 import { task } from 'ember-concurrency';
+import { on } from '@ember/object/evented';
 import ENV from 'irene/config/environment';
 import triggerAnalytics from 'irene/utils/trigger-analytics';
+import { translationMacro as t } from 'ember-i18n';
 
 const NamespaceComponentComponent = Ember.Component.extend({
   i18n: Ember.inject.service(),
@@ -11,6 +13,7 @@ const NamespaceComponentComponent = Ember.Component.extend({
   tagName: ['tr'],
   showRejectNamespaceConfirm: false,
   isRejectingNamespace: false,
+  tNamespaceRejected: t('namespaceRejected'),
 
   approveNamespace: task(function * () {
     const ns = this.get('namespace');
@@ -22,12 +25,17 @@ const NamespaceComponentComponent = Ember.Component.extend({
   rejectNamespaceConfirm: task(function * () {
     yield this.set('showRejectNamespaceConfirm', true);
   }),
-
   confirmReject: task(function * () {
     this.set('isRejectingNamespace', true);
     const ns = this.get('namespace');
     ns.deleteRecord();
     yield ns.save();
+  }).evented(),
+  confirmRejectSucceeded: on('confirmReject:succeeded', function() {
+    this.get('notify').success(this.get('tNamespaceRejected'));
+  }),
+  confirmRejectErrored: on('confirmReject:errored', function(_, error) {
+    this.get("notify").error(error.message);
   }),
 
   actions: {
--- app/components/organization-invitation-overview.js
+++ app/components/organization-invitation-overview.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
 import { task } from 'ember-concurrency';
+import { on } from '@ember/object/evented';
 
 const OrgInvitationComponent = Ember.Component.extend({
   i18n: Ember.inject.service(),
@@ -9,6 +10,7 @@ const OrgInvitationComponent = Ember.Component.extend({
   tInvitation: t('invitation'),
   tDeleted: t('deleted'),
   tInvitationDeleted: t('invitationDeleted'),
+  tInvitationReSent: t('invitationReSent'),
 
   isDeletingInvitation: false,
   isResendingInvitation: false,
@@ -21,22 +23,34 @@ const OrgInvitationComponent = Ember.Component.extend({
   openResendInvitationConfirmBox: task(function * () {
     yield this.set('showResendInvitationConfirmBox', true);
   }),
-  openDeleteInvitationConfirmBox: task(function * () {
-    yield this.set('showDeleteInvitationConfirmBox', true);
-  }),
-
   confirmResend: task(function * () {
     this.set('isResendingInvitation', true);
     const invite = this.get('invitation');
     // TODO: hit /resend endpoint
     yield invite.save();
+  }).evented(),
+  confirmResendSucceeded: on('confirmResend:succeeded', function() {
+    this.get('notify').success(this.get('tInvitationReSent'));
+  }),
+  confirmResendErrored: on('confirmResend:errored', function(_, error) {
+    this.get("notify").error(error.message);
+  }),
+
+  openDeleteInvitationConfirmBox: task(function * () {
+    yield this.set('showDeleteInvitationConfirmBox', true);
   }),
   confirmDelete: task(function * () {
     this.set('isDeletingInvitation', true);
     const invite = this.get('invitation');
     invite.deleteRecord();
     yield invite.save();
+
+  }).evented(),
+  confirmDeleteSucceeded: on('confirmDelete:succeeded', function() {
     this.get('notify').success(this.get('tInvitationDeleted'));
+  }),
+  confirmDeleteErrored: on('confirmDelete:errored', function(_, error) {
+    this.get("notify").error(error.message);
   }),
 
   actions: {
