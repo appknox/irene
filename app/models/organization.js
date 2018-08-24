efbd622d0d17eade7e2186ac5ceae9bc6ff49eb5
Praseetha-KR <praseetha04@gmail.com>
2018-08-24 14:16:31 +0530

    Display org property objects counts

--- app/models/organization.js
+++ app/models/organization.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 const Organization = DS.Model.extend({
   name: DS.attr('string'),
   logo: DS.attr('string'),
-  members: DS.hasMany('organization-member')
+  members: DS.hasMany('organization-member'),
+  projectsCount: DS.attr('number'),
+  namespacesCount: DS.attr('number'),
+  teamsCount: DS.attr('number'),
+  membersCount: Ember.computed('members', function() {
+    return this.get('members.meta.count');
+  })
 });
 
 export default Organization;
