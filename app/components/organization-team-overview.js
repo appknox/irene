6b0cc6c6e2f759fafd0bb4d8e9a2e4b917dac2fb
Praseetha-KR <praseetha04@gmail.com>
2018-08-26 12:06:13 +0530

    Fixes to team listing

--- app/components/organization-team-overview.js
+++ app/components/organization-team-overview.js
@@ -6,34 +6,43 @@ import { on } from '@ember/object/evented';
 const TeamOverviewComponent = Ember.Component.extend({
   i18n: Ember.inject.service(),
 
-  tagName: ["tr"],
   team: null,
   isDeletingTeam: false,
   showDeleteTeamPrompt: false,
 
+  tEnterRightTeamName: t('enterRightTeamName'),
+  tTeamDeleted: t('teamDeleted'),
+  tPleaseTryAgain: t('pleaseTryAgain'),
+
+
+  /* Open delete-team confirmation */
   openDeleteTeamConfirmBox: task(function * () {
     yield this.set('showDeleteTeamPrompt', true);
   }),
 
+
+  /* Delete team */
   confirmDelete: task(function * (inputValue) {
     this.set('isDeletingTeam', true);
     const t = this.get('team');
     const teamName = t.get("name").toLowerCase();
     const promptedTeam = inputValue.toLowerCase();
     if (promptedTeam !== teamName) {
-      throw new Error(t("enterRightTeamName"));
+      throw new Error(this.get('tEnterRightTeamName'));
     }
     t.deleteRecord();
     yield t.save();
 
   }).evented(),
+
   confirmDeleteSucceeded: on('confirmDelete:succeeded', function() {
-    this.get('notify').success(`${this.get('team.name')} ${t("teamDeleted")}`);
+    this.get('notify').success(`${this.get('team.name')} ${this.get('tTeamDeleted')}`);
     this.set('showDeleteTeamPrompt', false);
     this.set('isDeletingTeam', false);
   }),
+
   confirmDeleteErrored: on('confirmDelete:errored', function(_, err) {
-    let errMsg = t('pleaseTryAgain');
+    let errMsg = this.get('tPleaseTryAgain');
     if (err.errors && err.errors.length) {
       errMsg = err.errors[0].detail || errMsg;
     } else if(err.message) {
@@ -44,11 +53,13 @@ const TeamOverviewComponent = Ember.Component.extend({
     this.set('isDeletingTeam', false);
   }),
 
+
   actions: {
     confirmDeleteProxy(inputValue) {
       this.get('confirmDelete').perform(inputValue);
     },
   }
+
 });
 
 
