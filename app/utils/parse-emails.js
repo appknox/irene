90e5ac9351e69346461666c56a39ef0b6bdd7ffe
Praseetha-KR <praseetha04@gmail.com>
2018-09-01 01:15:20 +0530

    Added parse-email util fn

---
+++ app/utils/parse-emails.js
@@ -0,0 +1,10 @@
+import {validate} from 'ember-validators';
+
+function validateEmail(email) {
+  return validate('format', email, { type: 'email' }, null, 'email');
+}
+
+export default function parseEmails(emailsStr) {
+  const emails = emailsStr.replace(/[\n\s\t]/g, '').split(',');
+  return emails.filter(m => m).filter(m => validateEmail(m));
+}
