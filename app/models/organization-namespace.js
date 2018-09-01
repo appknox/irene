38ca95fba614c1b89e94c373dd7e52b3b3bd1472
Praseetha-KR <praseetha04@gmail.com>
2018-09-01 09:58:39 +0530

    Show namespace platform, requestedBy & approvedBy

--- app/models/organization-namespace.js
+++ app/models/organization-namespace.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import ENUMS from 'irene/enums';
 
 export default DS.Model.extend({
   value: DS.attr('string'),
@@ -6,4 +7,16 @@ export default DS.Model.extend({
   approvedOn: DS.attr('date'),
   isApproved: DS.attr('boolean'),
   organization: DS.belongsTo('organization'),
+  requestedBy: DS.belongsTo('organization-user'),
+  approvedBy: DS.belongsTo('organization-user'),
+  platform: DS.attr('number'),
+
+  platformIconClass:( function() {
+    switch (this.get("platform")) {
+      case ENUMS.PLATFORM.ANDROID: return "android";
+      case ENUMS.PLATFORM.IOS: return "apple";
+      case ENUMS.PLATFORM.WINDOWS: return "windows";
+      default: return "mobile";
+    }
+  }).property("platform"),
 });
