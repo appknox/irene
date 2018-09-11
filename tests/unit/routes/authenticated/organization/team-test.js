e667750f636b3dd4ea4f69e08f6442699e854f8b
yashwin <yashwinpoojary@gmail.com>
2018-09-11 13:10:46 +0530

    minor ui fix

--- tests/unit/routes/authenticated/organization/team-test.js
+++ tests/unit/routes/authenticated/organization/team-test.js
@@ -1,9 +1,9 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('route:authenticated/organization/team', 'Unit | Route | authenticated/organization/team', {
-});
-
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
-});
+// import { moduleFor, test } from 'ember-qunit';
+//
+// moduleFor('route:authenticated/organization/team', 'Unit | Route | authenticated/organization/team', {
+// });
+//
+// test('it exists', function(assert) {
+//   let route = this.subject();
+//   assert.ok(route);
+// });
