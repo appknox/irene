0a815b6120ca7a1b2f69fdb209da3157c340dd12
Praseetha-KR <praseetha04@gmail.com>
2018-10-05 04:34:15 +0530

    Modal to add collaborators to project, sync list with popup

---
+++ tests/integration/components/add-project-collaborator/component-test.js
@@ -0,0 +1,10 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('add-project-collaborator', 'Integration | Component | add project collaborator', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.ok(true);
+  assert.equal(this.$().text().trim(), '');
+});
