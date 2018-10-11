5c2c0ffb4b2add0542ea5ff39c448e121a078d2c
Sharat M R <cosmosgenius@gmail.com>
2018-10-11 18:01:44 +0530

    Auto staging deployment

--- config/deploy.js
+++ config/deploy.js
@@ -22,13 +22,25 @@ module.exports = function(deployTarget) {
   }
 
   if (deployTarget === 'staging') {
+    ENV.s3 = {
+      bucket: process.env.STAGING_AWS_BUCKET,
+      region: process.env.AWS_REGION
+    };
+
+    ENV['s3-index'] = {
+      bucket: process.env.STAGING_AWS_BUCKET,
+      region: process.env.AWS_REGION,
+      allowOverwrite: true
+    };
+
     ENV.build.environment = 'staging';
+
     // configure other plugins for staging deploy target here
 
     ENV.cloudfront = {
       distribution: 'E2YVUU4RPYNUI2',
       objectPaths: ['/*']
-  };
+    };
   }
 
   if (deployTarget === 'production') {
@@ -37,17 +49,7 @@ module.exports = function(deployTarget) {
 
     ENV.cloudfront = {
       distribution: 'E1SR2PB8XTR9RC'
-  };
-  }
-  
-  if (deployTarget === 'whitelabel') {
-    ENV.build.environment = 'whitelabel';
-    // configure other plugins for production deploy target here
-
-    ENV.cloudfront = {
-      distribution: 'E1SR2PB8XTR9RC',
-      objectPaths: ['/*']
-  };
+    };
   }
 
   if (deployTarget === 'whitelabel') {
@@ -57,7 +59,17 @@ module.exports = function(deployTarget) {
     ENV.cloudfront = {
       distribution: 'E1SR2PB8XTR9RC',
       objectPaths: ['/*']
-  };
+    };
+  }
+
+  if (deployTarget === 'whitelabel') {
+    ENV.build.environment = 'whitelabel';
+    // configure other plugins for production deploy target here
+
+    ENV.cloudfront = {
+      distribution: 'E1SR2PB8XTR9RC',
+      objectPaths: ['/*']
+    };
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
