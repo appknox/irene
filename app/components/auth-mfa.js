77f849694fa88425b931d720b8e3fb780d8d4c9c
Praseetha-KR <praseetha04@gmail.com>
2019-03-05 22:27:37 +0530

    Updated UI for select MFA methods

--- app/components/auth-mfa.js
+++ app/components/auth-mfa.js
@@ -11,40 +11,17 @@ const AuthMfaComponent = Component.extend({
   ajax: service(),
   notify: service('notification-messages-service'),
   user: null,
-  showMFAIntro: true,
-  showBarCode: false,
-  enableMFAOTP: null,
-  disableMFAOTP: null,
 
   isEnablingMFA: false,
   isDisablingMFA: false,
 
-  tEnterOTP: t("enterOTP"),
   tMFAEnabled: t("mfaEnabled"),
   tMFADisabled: t("mfaDisabled"),
 
-  didInsertElement() {
-    const provisioningURL = this.get("user.provisioningURL");
-    // eslint-disable-next-line no-undef
-    return new QRious({
-      element: this.element.querySelector("canvas"),
-      background: 'white',
-      backgroundAlpha: 0.8,
-      foreground: 'black',
-      foregroundAlpha: 0.8,
-      level: 'H',
-      padding: 25,
-      size: 300,
-      value: provisioningURL
-    });
-  },
-
 
   actions: {
-    openMFAEnableModal() {
-      this.set("showMFAEnableModal", true);
-      this.set("showMFAIntro", true);
-      this.set("showBarCode", false);
+    openMFASelectMethodModal() {
+      this.set("showMFASelectMethodModal", true);
     },
 
     openMFADisableModal() {
@@ -52,43 +29,13 @@ const AuthMfaComponent = Component.extend({
     },
 
     closeMFAEnableModal() {
-      this.set("showMFAEnableModal", false);
+      this.set("showMFASelectMethodModal", false);
     },
 
     closeMFADisableModal() {
       this.set("showMFADisableModal", false);
     },
 
-    showBarCode() {
-      this.set("showBarCode", true);
-      this.set("showMFAIntro", false);
-    },
-
-    enableMFA() {
-      const tEnterOTP = this.get("tEnterOTP");
-      const tMFAEnabled = this.get("tMFAEnabled");
-      const enableMFAOTP = this.get("enableMFAOTP");
-      for (let otp of [enableMFAOTP]) {
-        if (!isValidOTP(otp)) { return this.get("notify").error(tEnterOTP); }
-      }
-      const data =
-        {otp: enableMFAOTP};
-      this.set("isEnablingMFA", true);
-      this.get("ajax").post(ENV.endpoints.enableMFA, {data})
-      .then(() => {
-        this.get("notify").success(tMFAEnabled);
-        if(!this.isDestroyed) {
-          this.set("enableMFAOTP", "");
-          this.set("showMFAEnableModal", false);
-          this.set("isEnablingMFA", false);
-        }
-      }, (error) => {
-        if(!this.isDestroyed) {
-          this.set("isEnablingMFA", false);
-          this.get("notify").error(error.payload.message);
-        }
-      });
-    },
 
     disableMFA() {
       const tEnterOTP = this.get("tEnterOTP");
--- app/components/mfa-select/component.js
+++ app/components/mfa-select/component.js
@@ -1,5 +1,97 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import ENV from 'irene/config/environment';
+import { translationMacro as t } from 'ember-i18n';
+
+const isValidOTP = otp => otp && otp.length > 5;
 
 export default Component.extend({
 
+  i18n: service(),
+  ajax: service(),
+  notify: service('notification-messages-service'),
+  user: null,
+
+  showEnableMFAModal: true,
+  showMethodSelect: true,
+  showMethodApp: false,
+  showMethodEmail: false,
+  enableAppMFAOTP: null,
+  isEnablingAppMFA: false,
+  isEnablingEmailMFA: false,
+
+  tEnterOTP: t("enterOTP"),
+  tMFAEnabled: t("mfaEnabled"),
+  tMFADisabled: t("mfaDisabled"),
+
+  didInsertElement() {
+    const provisioningURL = this.get("user.provisioningURL");
+    // eslint-disable-next-line no-undef
+    return new QRious({
+      element: this.element.querySelector("canvas"),
+      background: 'white',
+      backgroundAlpha: 0.8,
+      foreground: 'black',
+      foregroundAlpha: 0.8,
+      level: 'H',
+      padding: 25,
+      size: 300,
+      value: provisioningURL
+    });
+  },
+
+
+  actions: {
+    setMethodSelect() {
+      this.set("showMethodSelect", true);
+      this.set("showMethodApp", false);
+      this.set("showMethodEmail", false);
+    },
+    setMethodApp() {
+      this.set("showMethodSelect", false);
+      this.set("showMethodApp", true);
+      this.set("showMethodEmail", false);
+    },
+    setMethodEmail() {
+      this.set("showMethodSelect", false);
+      this.set("showMethodApp", false);
+      this.set("showMethodEmail", true);
+    },
+
+    closeMFASelectModal() {
+      this.set("showEnableMFAModal", false);
+    },
+
+    enableEmailMFA() {
+
+    },
+
+    enableAppMFA() {
+      const tEnterOTP = this.get("tEnterOTP");
+      const tMFAEnabled = this.get("tMFAEnabled");
+      const enableAppMFAOTP = this.get("enableAppMFAOTP");
+
+      for (let otp of [enableAppMFAOTP]) {
+        if (!isValidOTP(otp)) { return this.get("notify").error(tEnterOTP); }
+      }
+      const data = {otp: enableAppMFAOTP};
+      this.set("isEnablingAppMFA", true);
+
+      this.get("ajax").post(ENV.endpoints.enableMFA, {data})
+      .then(() => {
+        this.get("notify").success(tMFAEnabled);
+        if(!this.isDestroyed) {
+          this.set("enableAppMFAOTP", "");
+          this.set("showMFAEnableModal", false);
+          this.set("isEnablingAppMFA", false);
+        }
+      }, (error) => {
+        if(!this.isDestroyed) {
+          this.set("isEnablingAppMFA", false);
+          this.get("notify").error(error.payload.message);
+        }
+      });
+    },
+
+  }
 });
