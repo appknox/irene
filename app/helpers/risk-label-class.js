f2ca474fe22fb77841914613d4ee977acfe010eb
Praseetha-KR <praseetha04@gmail.com>
2019-03-28 16:09:12 +0530

    Added more tests for risk-label-class

--- app/helpers/risk-label-class.js
+++ app/helpers/risk-label-class.js
@@ -3,18 +3,22 @@ import ENUMS from 'irene/enums';
 
 export function riskLabelClass(params) {
   let risk = params[0];
+  if (risk === null || risk === '') {
+    return ''
+  }
   if(typeof risk === "object") {
     risk = risk.value;
   }
   risk = parseInt(risk);
-  switch (risk) {
-    case ENUMS.RISK.UNKNOWN: return `is-progress`;
-    case ENUMS.RISK.NONE: return `is-success`;
-    case ENUMS.RISK.LOW: return `is-info`;
-    case ENUMS.RISK.MEDIUM: return `is-warning`;
-    case ENUMS.RISK.HIGH: return `is-danger`;
-    case ENUMS.RISK.CRITICAL: return `is-critical`;
+  const labels = {
+    [ENUMS.RISK.UNKNOWN]: 'is-progress',
+    [ENUMS.RISK.NONE]: 'is-success',
+    [ENUMS.RISK.LOW]: 'is-info',
+    [ENUMS.RISK.MEDIUM]: 'is-warning',
+    [ENUMS.RISK.HIGH]: 'is-danger',
+    [ENUMS.RISK.CRITICAL]: 'is-critical',
   }
+  return labels[risk] || '';
 }
 
 export default helper(riskLabelClass);
--- tests/unit/helpers/risk-label-class-test.js
+++ tests/unit/helpers/risk-label-class-test.js
@@ -4,9 +4,43 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | risk label class');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = riskLabelClass([1]);
-  assert.ok(result);
-  assert.equal(result, `is-info`);
+test('it return correct class for valid risks', function(assert) {
+  let unknown = riskLabelClass([-1]);
+  assert.equal(unknown, 'is-progress');
+
+  let passed = riskLabelClass([0]);
+  assert.equal(passed, 'is-success');
+
+  let low = riskLabelClass([1]);
+  assert.equal(low, 'is-info');
+
+  let medium = riskLabelClass([2]);
+  assert.equal(medium, 'is-warning');
+
+  let high = riskLabelClass([3]);
+  assert.equal(high, 'is-danger');
+
+  let critical = riskLabelClass([4]);
+  assert.equal(critical, 'is-critical');
+});
+
+test('it works for invalid risk values', function(assert) {
+  let invalid1 = riskLabelClass([5]);
+  assert.equal(invalid1, '');
+
+  let invalid2 = riskLabelClass([-2]);
+  assert.equal(invalid2, '');
+});
+
+test('it works for non integer inputs', function(assert) {
+  let boolInput = riskLabelClass([true]);
+  assert.equal(boolInput, '');
+
+  let objInput = riskLabelClass([{}]);
+  assert.equal(objInput, '');
+});
+
+test('it works for empty input', function(assert) {
+  let nullInput = riskLabelClass([]);
+  assert.equal(nullInput, '');
 });
