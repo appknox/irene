646e7aa9c976fcf52d3f1fcfc75d8bfcfbae4669
Praseetha-KR <praseetha04@gmail.com>
2019-03-28 13:57:30 +0530

    Added risk-label-class helper

---
+++ tests/unit/helpers/risk-label-class-test.js
@@ -0,0 +1,12 @@
+
+import { riskLabelClass } from 'irene/helpers/risk-label-class';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | risk label class');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = riskLabelClass([42]);
+  assert.ok(result);
+});
+
