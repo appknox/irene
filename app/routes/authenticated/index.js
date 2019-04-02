37ce68ca0a5be9081980a112a544b75d141ff91b
yashwin <yashwinpoojary@gmail.com>
2019-04-02 14:53:07 +0530

    get all the vuln and normalize it

--- app/routes/authenticated/index.js
+++ app/routes/authenticated/index.js
@@ -1,11 +1,9 @@
 import Route from '@ember/routing/route';
 
 const IndexRoute = Route.extend({
-
   beforeModel() {
     this.transitionTo('/projects');
   }
-}
-);
+});
 
 export default IndexRoute;
