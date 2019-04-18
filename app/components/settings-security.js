dd780ebbf84a8f410a5b5c77886ddcad87e3f603
Praseetha-KR <praseetha04@gmail.com>
2019-04-19 00:34:28 +0530

    Pass mfas context to multi-factor-auth component

--- app/components/settings-security.js
+++ app/components/settings-security.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  mfas: computed(function() {
+    return this.get('store').findAll('mfa');
+  })
 });
