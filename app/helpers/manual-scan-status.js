e57f5c320a3ebb142f1b2ced85debde255bab9bf
Praseetha-KR <praseetha04@gmail.com>
2019-04-24 20:24:07 +0530

    Refactored manualScanStatus helper

--- app/helpers/manual-scan-status.js
+++ app/helpers/manual-scan-status.js
@@ -2,19 +2,19 @@ import { helper } from '@ember/component/helper';
 import ENUMS from 'irene/enums';
 
 export function manualScanStatus(params) {
-
-  const currentRole = params[0];
-
-  if (currentRole === ENUMS.MANUAL.NONE) {
-    return "Not started";
-  } else if (currentRole === ENUMS.MANUAL.REQUESTED) {
-    return "Requested";
-  } else if (currentRole === ENUMS.MANUAL.ASSESSING) {
-    return "In progress";
-  }else if (currentRole === ENUMS.MANUAL.DONE) {
-    return "Completed";
+  let status = null;
+  try {
+    status = parseInt(params[0]);
+  } catch(_) {
+    return ''
   }
+  const statusLabels = {
+    [ENUMS.MANUAL.NONE]: 'Not started',
+    [ENUMS.MANUAL.REQUESTED]: 'Requested',
+    [ENUMS.MANUAL.ASSESSING]: 'In progress',
+    [ENUMS.MANUAL.DONE]: 'Completed',
+  }
+  return statusLabels[status] || '';
 }
-
 
 export default helper(manualScanStatus);
