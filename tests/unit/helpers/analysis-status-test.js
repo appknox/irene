93283568a87edd3834603fa930c7f87bda6164cf
Praseetha-KR <praseetha04@gmail.com>
2019-04-26 02:50:28 +0530

    Fixed tests: analysis list & status

--- tests/unit/helpers/analysis-status-test.js
+++ tests/unit/helpers/analysis-status-test.js
@@ -7,5 +7,5 @@ module('Unit | Helper | analysis status');
 // Replace this with your real tests.
 test('it works', function(assert) {
   let result = analysisStatus([42]);
-  assert.ok(result);
+  assert.equal(result, '');
 });
