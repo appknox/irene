78f68166f66ba7bdc8a6cc26490490dadc243edd
Sharat M R <cosmosgenius@gmail.com>
2019-05-30 01:03:53 +0530

    add report preference page

--- app/models/profile.js
+++ app/models/profile.js
@@ -2,5 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   files: DS.hasMany('file', {inverse:'profile'}),
-  showUnknownAnalysis: DS.attr('boolean')
+  showUnknownAnalysis: DS.attr('boolean'),
+  reportPreference: DS.attr(),
+  saveReportPreference(data) {
+    const adapter = this.store.adapterFor(this.constructor.modelName);
+    return adapter.saveReportPreference(this, data);
+  }
 });
--- app/routes/authenticated.js
+++ app/routes/authenticated.js
@@ -35,6 +35,7 @@ const AuthenticatedRoute = Route.extend(AuthenticatedRouteMixin, {
 
   async model() {
     const userId = this.get("session.data.authenticated.user_id");
+    await this.get('store').findAll('Vulnerability');
     await this.get('org').load();
     await this.get('analytics').load();
     return this.get('store').find('user', userId);
