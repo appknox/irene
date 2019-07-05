009633f2d4273197d57d130b87ea07f3651da816
Praseetha-KR <praseetha04@gmail.com>
2019-07-05 21:34:51 +0530

    proxy-settings component using form

--- app/components/proxy-settings/component.js
+++ app/components/proxy-settings/component.js
@@ -18,24 +18,15 @@ export default Component.extend({
   tOn: t('on'),
   tOff: t('off'),
 
-  currentProxy:null,
-  proxyPOJO: {},
-
-  init() {
-    this._super(...arguments);
-    const proxyPOJO = this.get('proxyPOJO');
-    const changeset = new Changeset(
-      proxyPOJO, lookupValidator(ProxySettingValidation), ProxySettingValidation
-    );
-    this.set('changeset', changeset);
-  },
+  currentProxy: null,
+  changeset: null,
 
   proxyId: computed('profile.id', function() {
     return this.get('profile.id');
   }),
 
-  hasProxyValues: computed('currentProxy.{host,port}', function() {
-    return !!this.get('currentProxy.host') && !!this.get('currentProxy.port');
+  hasProxyValues: computed('changeset.{host,port}', function() {
+    return !!this.get('changeset.host') && !!this.get('changeset.port');
   }),
 
   getCurrentProxy: task(function * (){
@@ -49,25 +40,14 @@ export default Component.extend({
 
   syncSettings: task(function * () {
     let currentProxy = yield this.get('getCurrentProxy').perform();
-    if (!currentProxy) {
-      return;
-    }
-    yield this.set('currentProxy', currentProxy);
-
-    let changeset = yield this.get('changeset');
-    yield changeset.set('host', currentProxy.get('host'));
-    yield changeset.set('port', parseInt(currentProxy.get('port')));
-    yield changeset.set('enabled', currentProxy.get('enabled'));
+    const changeset = new Changeset(
+      currentProxy,
+      lookupValidator(ProxySettingValidation),
+      ProxySettingValidation
+    );
+    this.set('currentProxy', currentProxy);
     this.set('changeset', changeset);
   }).on('init'),
-
-  saveProxy: task(function *() {
-    const status = yield this.get('saveChanges').perform();
-    if (status) {
-      this.get('notify').success(this.get('tProxySettingsSaved'));
-      triggerAnalytics('feature', ENV.csb.changeProxySettings);
-    }
-  }),
 
   clearProxy: task(function *() {
     const currentProxy = yield this.get('currentProxy');
@@ -93,14 +73,16 @@ export default Component.extend({
       }
       return false;
     }
-    var currentProxy = yield this.get('currentProxy');
-    yield currentProxy.setProperties({
-      'host': changeset.get('host'),
-      'port': changeset.get('port'),
-      'enabled': changeset.get('enabled'),
-    });
-    yield currentProxy.save();
+    yield changeset.save()
     return true;
+  }),
+
+  saveProxy: task(function *() {
+    const status = yield this.get('saveChanges').perform();
+    if (status) {
+      this.get('notify').success(this.get('tProxySettingsSaved'));
+      triggerAnalytics('feature', ENV.csb.changeProxySettings);
+    }
   }),
 
   enableProxy: task(function *(event) {
