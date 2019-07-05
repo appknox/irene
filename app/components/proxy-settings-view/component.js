072f2b11656ab4c0b774860a99e21c45abc3836c
Praseetha-KR <praseetha04@gmail.com>
2019-07-05 22:11:53 +0530

    proxy toggle view without changeset

--- app/components/proxy-settings-view/component.js
+++ app/components/proxy-settings-view/component.js
@@ -1,12 +1,10 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { on } from '@ember/object/evented';
 import { inject as service } from '@ember/service';
-import { task, waitForProperty } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import { translationMacro as t } from 'ember-i18n';
-import lookupValidator from 'ember-changeset-validations';
-import Changeset from 'ember-changeset';
 import ENV from 'irene/config/environment';
-import ProxySettingValidation from 'irene/validations/proxy-settings';
 import triggerAnalytics from 'irene/utils/trigger-analytics';
 
 export default Component.extend({
@@ -14,93 +12,43 @@ export default Component.extend({
   notify: service('notification-messages-service'),
 
   tProxyTurned: t('proxyTurned'),
+  tPleaseTryAgain: t('plaseTryAgain'),
   tOn: t('on'),
   tOff: t('off'),
 
-  currentProxy: null,
-  proxyPOJO: {},
-
-  init() {
-    this._super(...arguments);
-    const proxyPOJO = this.get('proxyPOJO');
-    const changeset = new Changeset(
-      proxyPOJO, lookupValidator(ProxySettingValidation), ProxySettingValidation
-    );
-    this.set('changeset', changeset);
-  },
-
-  proxyId: computed('profile.id', function() {
-    return this.get('profile.id');
+  proxy: computed('profile.id', function() {
+    let profileId = this.get('profile.id');
+    return this.get('store').findRecord('proxy-setting', profileId);
   }),
 
-  hasProxyValues: computed('currentProxy.{host,port}', function() {
-    return !!this.get('currentProxy.host') && !!this.get('currentProxy.port');
-  }),
-
-  getCurrentProxy: task(function * (){
-    yield waitForProperty(this, 'profile.id', function(pid) {
-      return !!pid
-    });
-    const proxyId = this.get('proxyId');
-    let record = yield this.get('store').findRecord('proxy-setting', proxyId);
-    return record;
-  }),
-
-  syncSettings: task(function * () {
-    let currentProxy = yield this.get('getCurrentProxy').perform();
-    if(!currentProxy) {
-      return;
-    }
-    yield this.set('currentProxy', currentProxy);
-
-    let changeset = yield this.get('changeset');
-    yield changeset.set('host', currentProxy.get('host'));
-    yield changeset.set('port', parseInt(currentProxy.get('port')));
-    yield changeset.set('enabled', currentProxy.get('enabled'));
-    this.set('changeset', changeset);
-  }).on('init'),
-
-  saveChanges: task(function *() {
-    const changeset = this.get('changeset');
-    yield changeset.validate();
-    const isValid = changeset.get('isValid');
-    if(!isValid) {
-      if(changeset.get('errors') && changeset.get('errors')[0].validation) {
-        this.get('notify').error(
-          changeset.get('errors')[0].validation[0],
-          ENV.notifications
-        );
-      }
-      return false;
-    }
-    var currentProxy = yield this.get('currentProxy');
-    yield currentProxy.setProperties({
-      'host': changeset.get('host'),
-      'port': changeset.get('port'),
-      'enabled': changeset.get('enabled'),
-    });
-    yield currentProxy.save();
-    return true;
-  }),
-
-  enableProxy: task(function *(event) {
-    const changeset = this.get('changeset');
+  /* Proxy enable or disable */
+  toggleProxy: task(function *(event) {
     let enabled = event.target.checked;
-    changeset.set('enabled', enabled);
-    const status = yield this.get('saveChanges').perform();
-    const statusText = changeset.get('enabled') ? this.get('tOn') : this.get('tOff');
-    if (status) {
-      this.get('notify').info(this.get('tProxyTurned') + statusText.string.toUpperCase());
-      if (enabled) {
-        triggerAnalytics('feature', ENV.csb.enableProxy);
-      } else {
-        triggerAnalytics('feature', ENV.csb.disableProxy);
-      }
+
+    var proxy = yield this.get('proxy');
+    proxy.set('enabled', enabled);
+    yield proxy.save();
+  }).evented(),
+
+  toggleProxySucceeded: on('toggleProxy:succeeded', function() {
+    let enabled = this.get('proxy.enabled');
+    const statusText = enabled ? this.get('tOn') : this.get('tOff');
+    this.get('notify').info(this.get('tProxyTurned') + statusText.string.toUpperCase());
+    if (enabled) {
+      triggerAnalytics('feature', ENV.csb.enableProxy);
     } else {
-      if (enabled) {
-        this.get('notify').error(changeset.get('errors')[0].validation[0]);
-      }
-      event.preventDefault();
+      triggerAnalytics('feature', ENV.csb.disableProxy);
     }
   }),
+
+  toggleProxyErrored: on('toggleProxy:errored', function(_, err) {
+    let errMsg = this.get('tPleaseTryAgain');
+    if (err.errors && err.errors.length) {
+      errMsg = err.errors[0].detail || errMsg;
+    } else if(err.message) {
+      errMsg = err.message;
+    }
+    this.get('notify').error(errMsg);
+  }),
+
 });
