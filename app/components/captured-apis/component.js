db36cd885c70f450476badfeaa3841c5ab027e1f
Praseetha-KR <praseetha04@gmail.com>
2019-07-13 15:49:13 +0530

    Cleanup captured-api component, calculate selectedCount inside component instead of param

--- app/components/captured-apis/component.js
+++ app/components/captured-apis/component.js
@@ -1,35 +1,42 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import PaginateMixin from 'irene/mixins/paginate';
 import { task } from 'ember-concurrency';
-
 import ENV from 'irene/config/environment';
+import PaginateMixin from 'irene/mixins/paginate';
+
 export default Component.extend(PaginateMixin, {
+  selectedCount: 0,
 
-  countCapturedAPI: task(function * (){
-    let data = {fileId: this.get('file.id'), is_active:true};
-    const url = [ENV.endpoints.files, this.get('file.id'), "capturedapis"].join('/');
-    return yield this.get("ajax").request(url,{namespace: ENV.namespace_v2, data});
+  targetObject: 'capturedapi',
+  sortProperties: ['id:asc'],
 
+  extraQueryStrings: computed('file.id', function() {
+    const query ={fileId: this.get('file.id')};
+    return JSON.stringify(query, Object.keys(query).sort());
   }),
-  someAction: task(function * (capturedApiObject, isActive){
-    yield capturedApiObject.set('isActive', !(isActive));
-    yield capturedApiObject.save();
-    try{
-      let filterCapturedAPIData = yield this.get('countCapturedAPI').perform()
-      yield this.set('count', filterCapturedAPIData.count)
-        
-    }catch(error){
-      this.get("notify").error(error.toString())
+
+  didInsertElement() {
+    this.get('setSelectedCount').perform();
+  },
+
+  getSelectedApis: task(function * () {
+    let data = {fileId: this.get('file.id'), is_active: true};
+    const url = [ENV.endpoints.files, this.get('file.id'), 'capturedapis'].join('/');
+    return yield this.get('ajax').request(url, {namespace: ENV.namespace_v2, data});
+  }),
+
+  setSelectedCount: task(function * () {
+    try {
+      let selectedApis = yield this.get('getSelectedApis').perform();
+      yield this.set('selectedCount', selectedApis.count);
+    } catch(error) {
+      this.get('notify').error(error.toString());
     }
   }),
-  targetObject: "capturedapi",
-  sortProperties: ["id:asc"],
-  extraQueryStrings: computed("query", function() {
-    const query =
-      {fileId: this.get("file.id")};
-    return JSON.stringify(query, Object.keys(query).sort());
-  })
 
-}
-);
+  toggleApi: task(function * (capturedApi, isActive){
+    yield capturedApi.set('isActive', !(isActive));
+    yield capturedApi.save();
+    yield this.get('setSelectedCount').perform();
+  }),
+});
