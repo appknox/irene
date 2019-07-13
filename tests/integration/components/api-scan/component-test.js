df6cc0e362a48a47355d855bc6945e762bf88bba
Praseetha-KR <praseetha04@gmail.com>
2019-07-13 16:52:00 +0530

    dymmy tests for api scan

---
+++ tests/integration/components/api-scan/component-test.js
@@ -0,0 +1,11 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('api-scan', 'Integration | Component | api scan', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.ok(true);
+  assert.equal(this.$().text().trim(), '');
+});
+
