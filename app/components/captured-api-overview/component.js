bed4a5c29f0faeaff806b9732b6c64980fb62103
Praseetha-KR <praseetha04@gmail.com>
2019-07-13 15:48:35 +0530

    Cleanup captured-api-overview component

--- app/components/captured-api-overview/component.js
+++ app/components/captured-api-overview/component.js
@@ -2,31 +2,24 @@ import Component from '@ember/component';
 import { task } from 'ember-concurrency';
 import { on } from '@ember/object/evented';
 
-export default Component.extend({    
-
-setApiScanStatus: task(function * () {
-  
-    let isActive = this.get('capturedapi.isActive')
-    const capturedapi = yield this.get("capturedapi")
-    try{
-      yield this.get('someAction').perform(capturedapi, isActive);
-
-    }catch(error){
-      this.get("notify").error(error.toString());
-    }
-
+export default Component.extend({
+  toggleApiSelect: task(function * () {
+    let isActive = this.get('capturedapi.isActive');
+    const capturedapi = yield this.get('capturedapi');
+    yield this.get('toggleApi').perform(capturedapi, isActive);
   }).evented(),
-  setApiScanStatusSucceeded: on('setApiScanStatus:succeeded', function() {
+
+  toggleApiSelectSucceeded: on('toggleApiSelect:succeeded', function() {
     this.get('notify').success('Successfully saved the captured api status');
   }),
 
-  setApiScanStatusErrored: on('setApiScanStatus:errored', function(_, error) {
+  toggleApiSelectErrored: on('toggleApiSelect:errored', function(_, error) {
     let errMsg = this.get('tPleaseTryAgain');
     if (error.errors && error.errors.length) {
       errMsg = error.errors[0].detail || errMsg;
     } else if(error.message) {
       errMsg = error.message;
     }
-    this.get("notify").error(errMsg);
+    this.get('notify').error(errMsg);
   }),
 });
