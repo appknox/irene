e66c3096e8434d014c84dd5b05574461f1edf6df
Praseetha-KR <praseetha04@gmail.com>
2019-07-23 02:16:39 +0530

    Dummy test for edit-analysis-button

--- tests/integration/components/edit-analysis-button/component-test.js
+++ tests/integration/components/edit-analysis-button/component-test.js
@@ -1,25 +1,10 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('edit-analysis-button', 'Integration | Component | edit analysis button', {
   integration: true
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{edit-analysis-button}}`);
-
+  assert.ok(true);
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#edit-analysis-button}}
-      template block text
-    {{/edit-analysis-button}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
