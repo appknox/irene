86ee4f7ef61a8a39069c7fdfc56b461336e5635e
Vaibhav <raivaibhav08@gmail.com>
2019-08-23 16:45:10 +0530

    Fix: Redirect register to schedule demo page
    Redirect only if config in not enabled,additionally
    change the page not found content

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -3,6 +3,7 @@ import Route from '@ember/routing/route';
 
 const NotFoundRoute = Route.extend({
   title: `Not Found${config.platform}`,
+  templateName: 'error',
   redirect() {
     const url = this.router.location.formatURL('/not-found');
     if (window.location.pathname !== url) {
--- app/routes/register.js
+++ app/routes/register.js
@@ -1,8 +1,14 @@
 import RSVP from 'rsvp';
 import Route from '@ember/routing/route';
+import ENV from 'irene/config/environment';
 
 const RegisterRoute = Route.extend({
   title: 'Registration',
+  beforeModel(){
+    if(ENV.registrationLink) {
+      window.location.href = ENV.registrationLink;
+    }
+  },
   model() {
     return new RSVP.Promise(function(resolve){
       window.grecaptcha.ready(function(){
