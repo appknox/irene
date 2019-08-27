99b5c5bfe9639891570c7c2f53aed18db775715d
Praseetha-KR <praseetha04@gmail.com>
2019-08-27 16:11:45 +0530

    Teams list condition fix

--- app/components/organization-team-list.js
+++ app/components/organization-team-list.js
@@ -1,7 +1,7 @@
 import PaginateMixin from 'irene/mixins/paginate';
 
 import { inject as service } from '@ember/service';
-import { observer, computed } from '@ember/object';
+import { observer } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend(PaginateMixin, {
@@ -15,7 +15,4 @@ export default Component.extend(PaginateMixin, {
   newInvitationsObserver: observer("realtime.OrganizationTeamCounter", function() {
     return this.incrementProperty("version");
   }),
-
-  hasMember: computed.gt('org.selected.membersCount', 0)
-
 });
