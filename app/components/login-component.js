77bcc9543cfddfc48124a839950ef39cfa24d6d6
Vaibhav Rai <vaibhav@Vaibhavs-MacBook-Pro.local>
2019-10-23 12:52:45 +0530

    Add: Option to enforce SSO
    Add option to enforce sso accordingly
    change the login flow with invite members
    flow.

--- app/components/login-component.js
+++ app/components/login-component.js
@@ -1,21 +1,34 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { task } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
 import { getOwner } from '@ember/application';
 import ENV from 'irene/config/environment';
 import { isUnauthorizedError } from 'ember-ajax/errors';
+import { isEmpty } from '@ember/utils';
+import { on } from '@ember/object/evented';
+import { t } from 'ember-intl';
 
 const LoginComponentComponent = Component.extend({
   session: service('session'),
   notify: service('notification-messages-service'),
   MFAEnabled: false,
   isLogingIn: false,
-  isSSOLogingIn: false,
   identification: "",
   password: "",
   otp: "",
   isNotEnterprise: !ENV.isEnterprise,
   isRegistrationEnabled: ENV.isRegistrationEnabled,
+  isSS0Enabled: null,
+  isisSS0Enforced: null,
+  tPleaseTryAgain: t("pleaseTryAgain"),
+  tSomethingWentWrongContactSupport: t("somethingWentWrongContactSupport"),
+  tPleaseEnterValidEmail: t("pleaseEnterValidEmail"),
+  tPleaseEnterValidAccountDetail: t("pleaseEnterValidAccountDetail"),
+
+  ssoNotverified: computed('isSS0Enabled', function(){
+    return isEmpty(this.get('isSS0Enabled'))
+  }),
 
   registrationLink: computed(function () {
     if (ENV.registrationLink) {
@@ -29,6 +42,7 @@ const LoginComponentComponent = Component.extend({
       return ENV.registrationLink;
     }
   }),
+
   hasRegistrationLink: computed('registrationLink', function () {
     return !!this.get('registrationLink');
   }),
@@ -45,6 +59,7 @@ const LoginComponentComponent = Component.extend({
     }
     return false;
   },
+
   isTrue(value) {
     if (value == undefined) {
       return false;
@@ -55,6 +70,48 @@ const LoginComponentComponent = Component.extend({
     return !!value
   },
 
+  SSOAuthenticate: task(function*(){
+    const url = `${ENV.endpoints.saml2}?token=${this.get('token')}&return_to=${window.location.origin}/saml2/redirect`;
+    const data = yield this.get('ajax').request(url);
+    yield window.location.href = data.url;
+  }).evented(),
+
+  SSOAuthenticateErrored: on('SSOAuthenticate:errored', function(_, err){
+    const status = err.status
+    this.set('token', null)
+    this.set('isSS0Enabled', null);
+    if (status===400 && (err.payload.token || err.payload.return_to)) {
+      this.get('notify').error(this.get('tSomethingWentWrongContactSupport'), ENV.notifications);
+      return;
+    }
+    this.get('notify').error(this.get('tPleaseTryAgain'));
+  }),
+
+  verifySSO: task(function *(){
+    let identification = this.get('identification');
+    if (!identification) {
+      return yield this.get("notify").error(this.get('tPleaseEnterValidEmail'), ENV.notifications);
+    }
+    const data = yield this.get('ajax').post('sso/check',{
+      namespace: ENV.namespace_v2,
+      data: {
+        username: identification
+      }
+    })
+    yield this.set('isSS0Enabled', data.is_sso);
+    yield this.set('isSS0Enforced', data.is_sso_enforced);
+    yield this.set('token', data.token)
+  }).evented(),
+
+  verifySSOErrored: on('verifySSO:errored', function(){
+    this.set('isSS0Enabled', null);
+    this.set('isSS0Enforced', null);
+  }),
+
+  verifySSOSucceded: on('verifySSO:succeded', async function(){
+    await this.get('redirectSaml').perform();
+  }),
+
   actions: {
     authenticate() {
       let identification = this.get('identification');
@@ -62,7 +119,7 @@ const LoginComponentComponent = Component.extend({
       const otp = this.get("otp");
 
       if (!identification || !password) {
-        return this.get("notify").error("Please enter username and password", ENV.notifications);
+        return this.get("notify").error(this.get('tPleaseEnterValidEmail'), ENV.notifications);
       }
       identification = identification.trim();
       password = password.trim();
@@ -87,21 +144,13 @@ const LoginComponentComponent = Component.extend({
               }
             }
           }
-          this.get("notify").error("Please enter valid account details", ENV.notifications);
+          this.get("notify").error(this.get('tPleaseEnterValidAccountDetail'), ENV.notifications);
         })
     },
-
-    SSOAuthenticate() {
-      this.set("isSSOLogingIn", true);
-      const url = `${ENV.endpoints.saml2}?return_to=${window.location.origin}/saml2/redirect`;
-
-      this.get("ajax").request(url)
-        .then(function (data) {
-          window.location.href = data.url;
-        })
-        .catch(function (err) {
-          this.get("notify").error(err.payload.message);
-        });
+    inputChange(){
+      if (!this.get('ssoNotverified')) {
+        this.set('isSS0Enabled', null);
+      }
     }
   }
 });
---
+++ tests/integration/components/multisso-account/component-test.js
@@ -0,0 +1,11 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('multisso-account', 'Integration | Component | multisso account', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  assert.ok(true);
+  assert.equal(this.$().text().trim(), '');
+});
