d49a11d9aec5f9c7047c1947d37840e1ded8b14d
Sharat M R <cosmosgenius@gmail.com>
2019-10-26 00:55:21 +0530

    Added hipaa-details component

---
+++ app/components/hipaa-details/component.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: "li",
+
+  actions: {
+    showMoreDetails() {
+      this.set("readMoreDetails", true);
+    },
+    hideMoreDetails() {
+      this.set("readMoreDetails", false);
+    }
+  }
+});
