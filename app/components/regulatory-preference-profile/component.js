85146abb5e95f971f5e6d52488d923108cf28ef3
Sharat M R <cosmosgenius@gmail.com>
2019-10-30 01:46:54 +0530

    Regulatory preference state UI, added notification

--- app/components/regulatory-preference-profile/component.js
+++ app/components/regulatory-preference-profile/component.js
@@ -1,9 +1,19 @@
 import Component from '@ember/component';
-import { task } from 'ember-concurrency';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { on } from '@ember/object/evented';
+import { task } from 'ember-concurrency';
+import { t } from 'ember-intl';
 import ENUMS from 'irene/enums';
 
 export default Component.extend({
+  intl: service(),
+  notify: service('notification-messages-service'),
+
+  tPleaseTryAgain: t('pleaseTryAgain'),
+  tRegulatoryPreferenceReset: t('regulatoryPreferenceReset'),
+  tPreferenceSetTo: t('preferenceSetTo'),
+
   regulatoryVisibilityOptions: ENUMS.REGULATORY_STATUS.BASE_VALUES,
 
   project: null,
@@ -16,24 +26,100 @@ export default Component.extend({
     return this.store.peekRecord('profile', profileId);
   }),
 
+
+  /* save pcidss */
   savePcidss: task(function *(status){
     const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
     yield profile.setShowPcidss({value: status});
-  }).restartable(),
+  }).restartable().evented(),
 
+  savePcidssSucceeded: on('savePcidss:succeeded', function(instance) {
+    const [status] = instance.args;
+    const statusDisplay = status ? 'SHOW': 'HIDE'
+    this.get('notify').success(`PCI-DSS ${this.get('tPreferenceSetTo')} ${statusDisplay}`);
+  }),
+
+  savePcidssErrored: on('savePcidss:errored', function(_, err) {
+    let errMsg = this.get('tPleaseTryAgain');
+    if (err.errors && err.errors.length) {
+      errMsg = err.errors[0].detail || errMsg;
+    } else if (err.payload && err.payload.detail) {
+      errMsg = err.payload.detail;
+    } else if (err.message) {
+      errMsg = err.message;
+    }
+    this.get('notify').error(errMsg);
+  }),
+
+
+  /* delete pcidss */
   deletePcidss: task(function *(){
     const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
     yield profile.unsetShowPcidss();
-  }).restartable(),
+  }).restartable().evented(),
 
+  deletePcidssSucceeded: on('deletePcidss:succeeded', function() {
+    this.get('notify').info(this.get('tRegulatoryPreferenceReset'));
+  }),
+
+  deletePcidssErrored: on('deletePcidss:errored', function(_, err) {
+    let errMsg = this.get('tPleaseTryAgain');
+    if (err.errors && err.errors.length) {
+      errMsg = err.errors[0].detail || errMsg;
+    } else if (err.payload && err.payload.detail) {
+      errMsg = err.payload.detail;
+    } else if (err.message) {
+      errMsg = err.message;
+    }
+    this.get('notify').error(errMsg);
+  }),
+
+
+  /* save hipaa */
   saveHipaa: task(function *(status){
     const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
     yield profile.setShowHipaa({value: status});
-  }).restartable(),
+  }).restartable().evented(),
 
+  saveHipaaSucceeded: on('saveHipaa:succeeded', function(instance) {
+    const [status] = instance.args;
+    const statusDisplay = status ? 'SHOW': 'HIDE'
+    this.get('notify').success(`HIPAA ${this.get('tPreferenceSetTo')} ${statusDisplay}`);
+  }),
+
+  saveHipaaErrored: on('saveHipaa:errored', function(_, err) {
+    let errMsg = this.get('tPleaseTryAgain');
+    if (err.errors && err.errors.length) {
+      errMsg = err.errors[0].detail || errMsg;
+    } else if (err.payload && err.payload.detail) {
+      errMsg = err.payload.detail;
+    } else if (err.message) {
+      errMsg = err.message;
+    }
+    this.get('notify').error(errMsg);
+  }),
+
+
+  /* delete hipaa */
   deleteHipaa: task(function *(){
     const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
     yield profile.unsetShowHipaa();
-  }).restartable(),
+  }).restartable().evented(),
+
+  deleteHipaaSucceeded: on('deleteHipaa:succeeded', function() {
+    this.get('notify').info(this.get('tRegulatoryPreferenceReset'));
+  }),
+
+  deleteHipaaErrored: on('deleteHipaa:errored', function(_, err) {
+    let errMsg = this.get('tPleaseTryAgain');
+    if (err.errors && err.errors.length) {
+      errMsg = err.errors[0].detail || errMsg;
+    } else if (err.payload && err.payload.detail) {
+      errMsg = err.payload.detail;
+    } else if (err.message) {
+      errMsg = err.message;
+    }
+    this.get('notify').error(errMsg);
+  }),
 
 });
