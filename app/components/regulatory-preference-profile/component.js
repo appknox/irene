5a5a840e8bcb4492b51d9a2572656408328e095e
Sharat M R <cosmosgenius@gmail.com>
2019-10-29 23:48:19 +0530

    set/unset regulatory preferences in profile

--- app/components/regulatory-preference-profile/component.js
+++ app/components/regulatory-preference-profile/component.js
@@ -2,7 +2,6 @@ import Component from '@ember/component';
 import { task } from 'ember-concurrency';
 import { computed } from '@ember/object';
 import ENUMS from 'irene/enums';
-import regulatoryStatusMap from 'irene/utils/regulatory-status-map';
 
 export default Component.extend({
   regulatoryVisibilityOptions: ENUMS.REGULATORY_STATUS.BASE_VALUES,
@@ -17,35 +16,24 @@ export default Component.extend({
     return this.store.peekRecord('profile', profileId);
   }),
 
-  reportPreference: computed('profile.id', function() {
-    return this.get('profile.reportPreference');
-  }),
-
-  pcidssToString: computed("profile.reportPreference.show_pcidss", function() {
-    const pcidss = this.get("profile.reportPreference.show_pcidss");
-    return String(regulatoryStatusMap.vk[pcidss]);
-  }),
-
-  hipaaToString: computed("profile.reportPreference.show_hipaa", function() {
-    const hipaa = this.get("profile.reportPreference.show_hipaa");
-    return String(regulatoryStatusMap.vk[hipaa]);
-  }),
-
   savePcidss: task(function *(status){
-    yield this.get('saveReportPreference').perform({
-      show_pcidss: regulatoryStatusMap.kv[status]
-    });
+    const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
+    yield profile.setShowPcidss({value: status});
+  }).restartable(),
+
+  deletePcidss: task(function *(){
+    const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
+    yield profile.unsetShowPcidss();
   }).restartable(),
 
   saveHipaa: task(function *(status){
-    yield this.get('saveReportPreference').perform({
-      show_hipaa: regulatoryStatusMap.kv[status]
-    });
+    const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
+    yield profile.setShowHipaa({value: status});
   }).restartable(),
 
-  saveReportPreference: task(function *(newPref) {
-    const profile = this.get('profile');
-    let pref = profile.get('reportPreference');
-    yield profile.saveReportPreference({...pref, ...newPref});
+  deleteHipaa: task(function *(){
+    const profile = this.store.peekRecord('profile', this.get('project.activeProfileId'));
+    yield profile.unsetShowHipaa();
   }).restartable(),
+
 });
