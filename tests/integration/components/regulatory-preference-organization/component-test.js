a90cce0fd22d81d01f6af2742c91a190a5fbf2db
Sharat M R <cosmosgenius@gmail.com>
2019-10-30 02:12:49 +0530

    Cleanup + dummy tests

--- tests/integration/components/regulatory-preference-organization/component-test.js
+++ tests/integration/components/regulatory-preference-organization/component-test.js
@@ -1,25 +1,10 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('regulatory-preference-organization', 'Integration | Component | regulatory preference organization', {
   integration: true
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{regulatory-preference-organization}}`);
-
+  assert.ok(true);
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#regulatory-preference-organization}}
-      template block text
-    {{/regulatory-preference-organization}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
