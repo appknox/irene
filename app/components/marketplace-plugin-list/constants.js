578bba19baef79867c3daee47a91353ee084c48d
Praseetha-KR <praseetha04@gmail.com>
2019-11-08 02:32:02 +0530

    Marketplace style fixes + reduced text in popup

--- app/components/marketplace-plugin-list/constants.js
+++ app/components/marketplace-plugin-list/constants.js
@@ -1,37 +1,37 @@
 export default {
     instructions: `
-    <ul class="bullet-list margin-h-1">
+    <ul class="bullet-list margin-r-1 margin-l-1h">
       <li class="margin-b-1">
-        Get Personal Access Token from <a href="/settings/developersettings" target="_blank">Developer Settings</a>
+        Generate a personal access token from <a href="/settings/developersettings" target="_blank">Developer Settings</a>
       </li>
       <li class="margin-b-1">
-        Download the appknox binary with following command:<br>
+        Set environment variable <code class="black-text">APPKNOX_ACCESS_TOKEN</code> with the personal token:<br>
+<pre class="code-block">
+$ export APPKNOX_ACCESS_TOKEN=&lt;personal access token&gt;
+</pre>
+      </li>
+      <li class="margin-b-1">
+        Download the appknox binary:<br>
 <pre class="code-block">
 $ curl -L https://github.com/appknox/appknox-go/releases/download/1.0.1/appknox-\`uname -s\`-x86_64 &gt; /usr/local/bin/appknox && chmod +x /usr/local/bin/appknox
 </pre>
       </li>
       <li class="margin-b-1">
-        Set an environment variable <strong>APPKNOX_ACCESS_TOKEN</strong> with the personal token. For example:<br>
+        Upload the app file to Appknox platform for scanning:<br>
 <pre class="code-block">
-$ export APPKNOX_ACCESS_TOKEN=&lt;personal token&gt;
+$ appknox upload &lt;path to apk/ipa file&gt;
 </pre>
       </li>
       <li class="margin-b-1">
-        Use the following command to upload the respective file to Appknox. For example: For Android, applications do the below:<br>
-<pre class="code-block">
-$ appknox upload &lt;path to apk&gt;
-</pre>
-      </li>
-      <li class="margin-b-1">
-        To check if your file contains any vulnerabilities with the risk greater than or equal to when the static scan is completed after the file upload. Use following command. E.g.:<br>
+        Check if the automated scanner detected any vulnerability above (or equal to) the specified risk level:<br>
 <pre class="code-block">
 $ appknox cicheck &lt;file-id&gt; --risk_threshold &lt;low|medium|high|critical&gt;
 </pre>
       </li>
-      <li class="margin-b-1">
+      <li class="margin-b-2">
         Tip: You can combine the upload and cicheck command as below which will list down all the vulnerabilities equal to or above the specified risk threshold and will exit with an error status.<br>
 <pre class="code-block">
-$ appknox upload &lt;path to apk&gt; | xargs appknox cicheck --risk-threshold &lt;low|medium|high|critical&gt;
+$ appknox upload &lt;path to apk/ipa file&gt; | xargs appknox cicheck --risk-threshold &lt;low|medium|high|critical&gt;
 </pre>
       </li>
     </ul>
