ea15573a42fde0154bc7a17345b885f645d363da
Praseetha-KR <praseetha04@gmail.com>
2019-11-08 19:01:11 +0530

    Permission check for integration cards

--- app/components/marketplace-integration-card/component.js
+++ app/components/marketplace-integration-card/component.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+import { t } from 'ember-intl';
 
 export default Component.extend({
+  me: service('me'),
+  notify: service('notification-messages-service'),
+
+  tIntegrationsPermissionDenied: t('integrationsPermissionDenied'),
+
+  showIntegrationsPermissionDenied: task(function* () {
+    yield this.get('notify').error(this.get('tIntegrationsPermissionDenied'));
+  }),
 });
