049581995f709ff609ffecd4841f0988e40e63ae
Praseetha-KR <praseetha04@gmail.com>
2019-11-08 19:00:24 +0530

    Added translations for marketplace-integrations

--- app/components/marketplace-integration-list/component.js
+++ app/components/marketplace-integration-list/component.js
@@ -1,30 +1,44 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { t } from 'ember-intl';
 
 export default Component.extend({
-    data: [
-        {
-            "title": "GitHub",
-            "description": "Integrate Appknox to GitHub",
-            "logo": "images/github-icon.png",
-            "link": "/organization/settings",
-        },
-        {
-            "title": "Jira",
-            "description": "Integrate Appknox to Jira",
-            "logo": "images/jira-icon.png",
-            "link": "/organization/settings",
-        },
-        {
-            "title": "Slack",
-            "description": "Coming Soon",
-            "logo": "images/slack-icon.png",
-            "link": "",
-        },
-        {
-            "title": "GitLab",
-            "description": "Coming Soon",
-            "logo": "images/gitlab-icon.png",
-            "link": "",
-        }
-    ],
+  intl: service(),
+
+  tGithub: t("github"),
+  tJira: t("jira"),
+  tSlack: t("slack"),
+  tGitlab: t("gitlab"),
+  tIntegrateAppknoxTo: t("integrateAppknoxTo"),
+  tComingSoon: t("comingSoon"),
+
+  data: computed(function() {
+    return [
+      {
+        "title": this.get("tGithub"),
+        "description": this.get("tIntegrateAppknoxTo") + this.get("tGithub"),
+        "logo": "images/github-icon.png",
+        "link": "authenticated.organization.settings",
+      },
+      {
+        "title": this.get("tJira"),
+        "description": this.get("tIntegrateAppknoxTo") + this.get("tJira"),
+        "logo": "images/jira-icon.png",
+        "link": "authenticated.organization.settings",
+      },
+      {
+        "title": this.get("tSlack"),
+        "description": this.get("tComingSoon"),
+        "logo": "images/slack-icon.png",
+        "link": "",
+      },
+      {
+        "title": this.get("tGitlab"),
+        "description": this.get("tComingSoon"),
+        "logo": "images/gitlab-icon.png",
+        "link": "",
+      }
+    ]
+  }),
 });
