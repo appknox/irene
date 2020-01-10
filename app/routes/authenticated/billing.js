0672237734a80797fa5e4f72c5195a0cf3fba989
Parthipan N <natkunamparthipan@gmail.com>
2020-01-10 15:13:59 +0530

    change the UI style of pricing cardsand add success notification

--- app/routes/authenticated/billing.js
+++ app/routes/authenticated/billing.js
@@ -7,11 +7,19 @@ const AuthenticatedBillingRoute = Route.extend(ScrollTopMixin, {
   title: `Billing${config.platform}`,
   organization: service('organization'),
   store: service('store'),
+  showNotification: false,
+
+  beforeModel: function(transition){
+    this.set('showNotification',!!transition.queryParams.success);
+  },
+
   async model() {
     const {showBilling} = this.get('organization.selected');
     const plans = await this.get('store').findAll('pricing-plan');
+    const showNotification = this.get('showNotification');
     return {
       showBilling,
+      showNotification,
       hasPaymentHistory: false,
       plans
     }
