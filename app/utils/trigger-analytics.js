13d5654db41304302dcd82576fec303446eb61a8
Parthipan N <natkunamparthipan@gmail.com>
2020-01-13 12:55:58 +0530

    add user role datum in the data pushed to CSB
    split the user data object destructuring in multi lines

--- app/utils/trigger-analytics.js
+++ app/utils/trigger-analytics.js
@@ -12,8 +12,13 @@ const triggerAnalytics = (type, data) =>{
       }
       if (type === "login") {
         const logInFeatureData = ENV.csb.userLoggedIn;
-        const {userName:custom_username, userEmail:email, accountId:account_id, accountName:custom_Organization} = data;
-        
+        const {
+          userName:custom_username,
+          userEmail:email,
+          accountId:account_id,
+          accountName:custom_Organization,
+          userRole:custom_role
+        } = data;
         window.analytics.login(data.userId, data.accountId);
 
         // Identify user to populate name and/or email along with proper org association on CSB dashboard
@@ -22,10 +27,10 @@ const triggerAnalytics = (type, data) =>{
           email,
           account_id,
           custom_Organization,
+          custom_role,
           first_name:custom_username
         }
         window.analytics.identify(data.userId,userIdentificationObject);
-        
         // To publish user's login as an activity on the CSB dashboard and visulaization
         return window.analytics.feature(logInFeatureData.feature, logInFeatureData.module, logInFeatureData.product);
       }
