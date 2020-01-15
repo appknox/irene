1a12c2eeddfa8acafd8e166bbf12ea02939d3420
Parthipan N <natkunamparthipan@gmail.com>
2020-01-15 15:22:09 +0530

    add payment subscription adapter and model

---
+++ app/adapters/payment-subscription.js
@@ -0,0 +1,25 @@
+import DRFAdapter from './drf';
+import ENV from 'irene/config/environment';
+import IreneAdapterMixin from 'irene/mixins/data-adapter-mixin';
+import { inject as service } from '@ember/service';
+
+export default DRFAdapter.extend(IreneAdapterMixin,{
+    host: ENV.host,
+    namespace: ENV.namespace,
+    addTrailingSlashes: false,
+    organization: service('organization'),
+
+    _buildURL(moduleName, id) {
+      const baseurl = `${this.get('host')}/${this.get('namespace')}/organizations/${this.get('organization').selected.id}/plans/subscriptions`;
+      if (id) {
+          return `${baseurl}/${encodeURIComponent(id)}`;
+      }
+      return baseurl;
+    },
+
+    switchPlanURL(id) {
+      const planBaseurl = this._buildURL(null,id);
+      return `${planBaseurl}/switch`;
+    }
+
+});
---
+++ app/models/payment-subscription.js
@@ -0,0 +1,27 @@
+import DS from 'ember-data';
+import {computed} from "@ember/object";
+
+export default DS.Model.extend({
+  // STATUS_ACTIVE: 0,
+  // STATUS_CANCELLED: 1,
+  // STATUS_EXPIRED: 2,
+  // STATUS_INVALID_CARD: 3,
+  DEFAULT_STATUS: 4,
+  statusTextList : ['Active','Cancelled','Expired','Invalid Card','Errored'],
+
+  plan: DS.attr(),
+  period: DS.attr(),
+  createdOn: DS.attr('date'),
+  expiry: DS.attr('date'),
+  nextBillingDate: DS.attr('date'),
+  lastPaidOn: DS.attr('date'),
+  quantityBought: DS.attr('number'),
+  quantityUsed: DS.attr('date'),
+  status: DS.attr('number'),
+
+  statusText: computed('status',function(){
+    const subscriptionStatusValue = this.get('status');
+    const subscriptionText = this.get('statusTextList')[subscriptionStatusValue];
+    return subscriptionText ? subscriptionText : this.get('statusTextList')[this.get('DEFAULT_STATUS')];
+  })
+});
