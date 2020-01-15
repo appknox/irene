c9eb13d8b5c2f61c37508c61a6692242469015e5
Parthipan N <natkunamparthipan@gmail.com>
2020-01-15 15:22:09 +0530

    add payment subscription adapter and model

---
+++ tests/unit/adapters/payment-subscription-test.js
@@ -0,0 +1,12 @@
+// import { module, test } from 'qunit';
+// import { setupTest } from 'ember-qunit';
+
+// module('Unit | Adapter | payment subscription', function(hooks) {
+//   setupTest(hooks);
+
+//   // Replace this with your real tests.
+//   test('it exists', function(assert) {
+//     let adapter = this.owner.lookup('adapter:payment-subscription');
+//     assert.ok(adapter);
+//   });
+// });
