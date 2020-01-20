718ee9a83741abb5d8a4c515f76cf1ee6969bc39
Parthipan N <natkunamparthipan@gmail.com>
2020-01-20 19:24:07 +0530

    add credit cards table UI component

---
+++ tests/integration/components/credit-cards/component-test.js
@@ -0,0 +1,24 @@
+// import { moduleForComponent, test } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
+
+// moduleForComponent('credit-cards', 'Integration | Component | credit cards', {
+//   integration: true
+// });
+
+// test('it renders', function(assert) {
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });
+
+//   this.render(hbs`{{credit-cards}}`);
+
+//   assert.equal(this.$().text().trim(), '');
+
+//   // Template block usage:
+//   this.render(hbs`
+//     {{#credit-cards}}
+//       template block text
+//     {{/credit-cards}}
+//   `);
+
+//   assert.equal(this.$().text().trim(), 'template block text');
+// });
