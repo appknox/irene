07b15e827e95a2c9e7c1c16b62f83e61d2a1d799
Parthipan N <natkunamparthipan@gmail.com>
2020-01-20 18:36:16 +0530

    add credit card model and adapter

---
+++ tests/unit/adapters/credit-card-test.js
@@ -0,0 +1,12 @@
+// import { module, test } from 'qunit';
+// import { setupTest } from 'ember-qunit';
+
+// module('Unit | Adapter | credit card', function(hooks) {
+//   setupTest(hooks);
+
+//   // Replace this with your real tests.
+//   test('it exists', function(assert) {
+//     let adapter = this.owner.lookup('adapter:credit-card');
+//     assert.ok(adapter);
+//   });
+// });
