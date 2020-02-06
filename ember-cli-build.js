c33ae3e0b2ef3d9f3d0bd7f9a872e308dd3edaed
Sharat M R <cosmosgenius@gmail.com>
2020-02-06 20:12:39 +0530

    migrate billboard

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -62,7 +62,5 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('node_modules/billboard.js/dist/billboard.css');
-
   return app.toTree();
 };
