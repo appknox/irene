b249bf5ed4a2942d0ff824dcf80e9ab61908cf56
Parthipan N <natkunamparthipan@gmail.com>
2020-04-13 19:34:10 +0530

    add a localstorage helper service to perform set/get/clear operation in browser's localStorage

--- app/components/pricing-card/component.js
+++ app/components/pricing-card/component.js
@@ -1,11 +1,16 @@
 import Component from "@ember/component";
 import { task } from "ember-concurrency";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
   tagName: "",
   showCheckoutModal: false,
   planId: null,
   quantity: 1,
+  tempStoreKeyPrefix: "ajs_bill_oneTime",
+
+  tempStore: service("local-storage"),
+  notify: service("notification-messages"),
 
   getStripeSessionId: task(function*() {
     const defaultPlanQuantity = this.get("plan.quantity");
@@ -24,6 +29,23 @@ export default Component.extend({
       const stripe = window.Stripe(
         "pk_test_IMZbFpQo6Uavs7Q77Udp7E8u00c1dRKOsd"
       );
+      try {
+        if (!this.get("plan.isRecurring")) {
+          const paymentData = {
+            timestamp: Date.now()
+          };
+          yield this.get("tempStore").setData(
+            this.get("tempStoreKeyPrefix"),
+            paymentData
+          );
+        } else {
+          yield this.get("tempStore").clearData(this.get("tempStoreKeyPrefix"));
+        }
+      } catch (err) {
+        this.get("notify").error(err.message);
+        return null;
+      }
+
       stripe.redirectToCheckout({ sessionId: stripeCheckoutSessionId });
     }
     return null;
