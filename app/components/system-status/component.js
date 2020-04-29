c5401b77cb78ad83826ecccf31f04e1b11422c7d
Praseetha-KR <praseetha04@gmail.com>
2020-04-29 09:43:26 +0530

    Added api server to status list based on PR #835

--- app/components/system-status/component.js
+++ app/components/system-status/component.js
@@ -1,38 +1,43 @@
 import Component from '@ember/component';
 import { task } from 'ember-concurrency';
 import ENV from 'irene/config/environment';
-import {isNotFoundError} from 'ember-ajax/errors';
+import { isNotFoundError } from 'ember-ajax/errors';
 
 export default Component.extend({
-
   isStorageWorking: false,
   isDeviceFarmWorking: false,
-
-  localClassNameBindings: [
-    'isStorageWorking:storage-operational',
-    'isDeviceFarmWorking:devicefarm-operational'],
+  isAPIServerWorking: false,
 
   didInsertElement() {
-    this.get("getStorageStatus").perform();
+    this.get('getStorageStatus').perform();
     this.get('getDeviceFarmStatus').perform();
+    this.get('getAPIServerStatus').perform();
   },
 
-  getStorageStatus: task(function *() {
+  getStorageStatus: task(function* () {
     try {
-      let status = yield this.get("ajax").request(ENV.endpoints.status);
-      yield this.get('ajax').request(status.data.storage, { headers:{}});
-    } catch(error) {
-      this.set("isStorageWorking", !!isNotFoundError(error));
+      let status = yield this.get('ajax').request(ENV.endpoints.status);
+      yield this.get('ajax').request(status.data.storage, { headers: {} });
+    } catch (error) {
+      this.set('isStorageWorking', !!isNotFoundError(error));
     }
   }).drop(),
 
-  getDeviceFarmStatus: task(function *() {
+  getDeviceFarmStatus: task(function* () {
     try {
-      yield this.get("ajax").request(ENV.endpoints.ping);
-      this.set("isDeviceFarmWorking", true);
-    } catch(error) {
-      this.set("isDeviceFarmWorking", false);
+      yield this.get('ajax').request(ENV.endpoints.ping);
+      this.set('isDeviceFarmWorking', true);
+    } catch (_) {
+      this.set('isDeviceFarmWorking', false);
     }
   }).drop(),
 
+  getAPIServerStatus: task(function* () {
+    try {
+      yield this.get('ajax').request(ENV.endpoints.ping);
+      this.set('isAPIServerWorking', true);
+    } catch (_) {
+      this.set('isAPIServerWorking', false);
+    }
+  }).drop(),
 });
