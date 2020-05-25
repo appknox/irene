bec5e2f039cb39d5ac740ebf0950c56e8c5084ae
Praseetha-KR <praseetha04@gmail.com>
2020-05-26 00:07:56 +0530

    Display license detail - model, adapter & component

---
+++ app/models/license.js
@@ -0,0 +1,30 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+
+
+export default DS.Model.extend({
+  key: DS.attr('string'),
+  startDate: DS.attr('date'),
+  expiryDate: DS.attr('date'),
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  isLimitedScans: DS.attr('boolean'),
+  status: DS.attr('string'),
+  perAppId: DS.attr('string'),
+  perAppName: DS.attr('string'),
+  perAppDescription: DS.attr('string'),
+  perAppQuantity: DS.attr('number'),
+  perScanId: DS.attr('string'),
+  perScanName: DS.attr('string'),
+  perScanDescription: DS.attr('string'),
+  perScanQuantity: DS.attr('number'),
+
+
+  isActive: computed('status', function(){
+    return this.get('status') === 'Active';
+  }),
+
+  isExpired: computed('status', function(){
+    return this.get('status') === 'Expired';
+  }),
+});
