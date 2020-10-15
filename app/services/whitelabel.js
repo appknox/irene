7ad19352a4cd5b3386866f3bd62ccb75d508a9a4
Sharat M R <cosmosgenius@gmail.com>
2020-10-15 20:41:32 +0530

    Migrate constansts to service and add theme support

--- app/services/whitelabel.js
+++ app/services/whitelabel.js
@@ -1,5 +1,4 @@
 import Service from '@ember/service';
-import CONSTANTS from 'irene/utils/constants';
 import ENV from 'irene/config/environment';
 
 /**
@@ -7,11 +6,38 @@ import ENV from 'irene/config/environment';
  */
 export default class WhitelabelService extends Service {
 
+  isEnabled() {
+    return ENV.whitelabel.enabled || false;
+  }
+
+  THEMES = {
+    'DARK': 'dark',
+    'LIGHT': 'light'
+  }
+
+  default_name = "Appknox";
+  defautl_theme = this.THEMES.DARK;
+
+
+  /**
+   * @property {String} theme
+   * Whitelabel theme will be returned or fallback default to `dark`.
+   */
+  get theme() {
+    if(this.isEnabled()) {
+      return ENV.whitelabel.theme
+    }
+    return this.defautl_theme;
+  }
+
   /**
    * @property {String} name
-   * Whitelabel name will be returned or fallback default to Appknox.
+   * Whitelabel name will be returned or fallback default to `Appknox`.
    */
   get name() {
-    return ENV.whitelabel.enabled ? ENV.whitelabel.name || CONSTANTS.WHITELABEL.DEFAULT_NAME : CONSTANTS.WHITELABEL.DEFAULT_NAME;
+    if(this.isEnabled())  {
+      return ENV.whitelabel.name;
+    }
+    return this.default_name;
   }
 }
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,9 +1,6 @@
 const CONSTANTS = {
   ANDROID_STORE_URL_RE: /(http(s?):\/\/play.google.com\/store\/apps\/details\?id=([A-Za-z].*))|(market:\/\/[A-Za-z].*)/,
-  WINDOWS_STORE_URL_RE: /microsoft.com(.*)store\/p\/(.*)\/(.*)/,
-  WHITELABEL: {
-    DEFAULT_NAME: 'Appknox'
-  }
+  WINDOWS_STORE_URL_RE: /microsoft.com(.*)store\/p\/(.*)\/(.*)/
 };
 
 export default CONSTANTS;
