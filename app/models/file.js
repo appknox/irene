c45e841fd842fed57d916e92b6ad692c7201f8cd
Gugan <guganssp@gmail.com>
2020-10-23 12:51:02 +0530

    AP-646: File report btn state

--- app/models/file.js
+++ app/models/file.js
@@ -1,31 +1,22 @@
-import {
-  inject as service
-} from '@ember/service';
-import {
-  computed
-} from '@ember/object';
-import Model, { attr, hasMany, belongsTo }  from '@ember-data/model';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 import BaseModelMixin from 'irene/mixins/base-model';
 import ENUMS from 'irene/enums';
-import {
-  t
-} from 'ember-intl';
-import {
-  RISK_COLOR_CODE
-} from 'irene/utils/constants';
-
+import { t } from 'ember-intl';
+import { RISK_COLOR_CODE } from 'irene/utils/constants';
 
 const _getAnalysesCount = (analysis, risk) => {
-  return analysis.filterBy('computedRisk', risk).get('length')
+  return analysis.filterBy('computedRisk', risk).get('length');
 };
 
 const File = Model.extend(BaseModelMixin, {
   intl: service(),
   project: belongsTo('project', {
-    inverse: 'files'
+    inverse: 'files',
   }),
   profile: belongsTo('profile', {
-    inverse: 'files'
+    inverse: 'files',
   }),
   uuid: attr('string'),
   deviceToken: attr('string'),
@@ -37,7 +28,7 @@ const File = Model.extend(BaseModelMixin, {
   name: attr('string'),
   dynamicStatus: attr('number'),
   analyses: hasMany('analysis', {
-    inverse: 'file'
+    inverse: 'file',
   }),
   report: attr('string'),
   manual: attr('number'),
@@ -54,13 +45,16 @@ const File = Model.extend(BaseModelMixin, {
   supportedCpuArchitectures: attr('string'),
   supportedDeviceTypes: attr('string'),
   canRunAutomatedDynamicscan: attr('boolean'),
+  reports: hasMany('file-report', {
+    async: true,
+  }),
 
   isManualRequested: computed('manual', function () {
-    return this.get("manual") !== ENUMS.MANUAL.NONE
+    return this.get('manual') !== ENUMS.MANUAL.NONE;
   }),
 
   isRunningApiScan: computed('apiScanStatus', function () {
-    const apiScanStatus = this.get("apiScanStatus");
+    const apiScanStatus = this.get('apiScanStatus');
     return apiScanStatus == ENUMS.SCAN_STATUS.RUNNING;
   }),
 
@@ -74,18 +68,18 @@ const File = Model.extend(BaseModelMixin, {
   },
 
   isStaticCompleted: computed('isStaticDone', function () {
-    const isStaticDone = this.get("isStaticDone");
+    const isStaticDone = this.get('isStaticDone');
     return this.scanProgressClass(isStaticDone);
   }),
 
-  tDeviceInQueue: t("deviceInQueue"),
-  tDeviceBooting: t("deviceBooting"),
-  tDeviceDownloading: t("deviceDownloading"),
-  tDeviceInstalling: t("deviceInstalling"),
-  tDeviceLaunching: t("deviceLaunching"),
-  tDeviceHooking: t("deviceHooking"),
-  tDeviceShuttingDown: t("deviceShuttingDown"),
-  tDeviceCompleted: t("deviceCompleted"),
+  tDeviceInQueue: t('deviceInQueue'),
+  tDeviceBooting: t('deviceBooting'),
+  tDeviceDownloading: t('deviceDownloading'),
+  tDeviceInstalling: t('deviceInstalling'),
+  tDeviceLaunching: t('deviceLaunching'),
+  tDeviceHooking: t('deviceHooking'),
+  tDeviceShuttingDown: t('deviceShuttingDown'),
+  tDeviceCompleted: t('deviceCompleted'),
 
   analysesSorting: ['computedRisk:desc'],
   sortedAnalyses: computed.sort('analyses', 'analysesSorting'),
@@ -97,8 +91,10 @@ const File = Model.extend(BaseModelMixin, {
   countRiskNone: 0,
   countRiskUnknown: 0,
 
+  canGenerateReport: attr('boolean'),
+
   doughnutData: computed('analyses.@each.computedRisk', function () {
-    const analyses = this.get("analyses");
+    const analyses = this.get('analyses');
     const r = ENUMS.RISK;
     const countRiskCritical = _getAnalysesCount(analyses, r.CRITICAL);
     const countRiskHigh = _getAnalysesCount(analyses, r.HIGH);
@@ -107,41 +103,36 @@ const File = Model.extend(BaseModelMixin, {
     const countRiskNone = _getAnalysesCount(analyses, r.NONE);
     const countRiskUnknown = _getAnalysesCount(analyses, r.UNKNOWN);
 
-    this.set("countRiskCritical", countRiskCritical); // eslint-disable-line
-    this.set("countRiskHigh", countRiskHigh); // eslint-disable-line
-    this.set("countRiskMedium", countRiskMedium); // eslint-disable-line
-    this.set("countRiskLow", countRiskLow); // eslint-disable-line
-    this.set("countRiskNone", countRiskNone); // eslint-disable-line
-    this.set("countRiskUnknown", countRiskUnknown); // eslint-disable-line
+    this.set('countRiskCritical', countRiskCritical); // eslint-disable-line
+    this.set('countRiskHigh', countRiskHigh); // eslint-disable-line
+    this.set('countRiskMedium', countRiskMedium); // eslint-disable-line
+    this.set('countRiskLow', countRiskLow); // eslint-disable-line
+    this.set('countRiskNone', countRiskNone); // eslint-disable-line
+    this.set('countRiskUnknown', countRiskUnknown); // eslint-disable-line
 
     return {
-      labels: [
-        'CRITICAL',
-        'HIGH',
-        'MEDIUM',
-        'LOW',
-        'PASSED',
-        'UNKNOWN'
+      labels: ['CRITICAL', 'HIGH', 'MEDIUM', 'LOW', 'PASSED', 'UNKNOWN'],
+      datasets: [
+        {
+          label: 'Risks',
+          data: [
+            countRiskCritical,
+            countRiskHigh,
+            countRiskMedium,
+            countRiskLow,
+            countRiskNone,
+            countRiskUnknown,
+          ],
+          backgroundColor: [
+            RISK_COLOR_CODE.CRITICAL,
+            RISK_COLOR_CODE.DANGER,
+            RISK_COLOR_CODE.WARNING,
+            RISK_COLOR_CODE.INFO,
+            RISK_COLOR_CODE.SUCCESS,
+            RISK_COLOR_CODE.DEFAULT,
+          ],
+        },
       ],
-      datasets: [{
-        label: 'Risks',
-        data: [
-          countRiskCritical,
-          countRiskHigh,
-          countRiskMedium,
-          countRiskLow,
-          countRiskNone,
-          countRiskUnknown
-        ],
-        backgroundColor: [
-          RISK_COLOR_CODE.CRITICAL,
-          RISK_COLOR_CODE.DANGER,
-          RISK_COLOR_CODE.WARNING,
-          RISK_COLOR_CODE.INFO,
-          RISK_COLOR_CODE.SUCCESS,
-          RISK_COLOR_CODE.DEFAULT
-        ]
-      }]
     };
   }),
 
@@ -169,10 +160,17 @@ const File = Model.extend(BaseModelMixin, {
     return status === ENUMS.DYNAMIC_STATUS.ERROR;
   }),
 
-  isDynamicStatusQueueAndHasAutomation: computed('dynamicStatus', 'canRunAutomatedDynamicscan', function () {
-    const status = this.get('dynamicStatus');
-    return status === ENUMS.DYNAMIC_STATUS.INQUEUE && this.get('canRunAutomatedDynamicscan');
-  }),
+  isDynamicStatusQueueAndHasAutomation: computed(
+    'dynamicStatus',
+    'canRunAutomatedDynamicscan',
+    function () {
+      const status = this.get('dynamicStatus');
+      return (
+        status === ENUMS.DYNAMIC_STATUS.INQUEUE &&
+        this.get('canRunAutomatedDynamicscan')
+      );
+    }
+  ),
   isDynamicStatusReady: computed('dynamicStatus', function () {
     const status = this.get('dynamicStatus');
     return status === ENUMS.DYNAMIC_STATUS.READY;
@@ -181,72 +179,111 @@ const File = Model.extend(BaseModelMixin, {
   isDynamicStatusNotReady: computed.not('isDynamicStatusReady'),
   isDynamicStatusNotNone: computed.not('isDynamicStatusNone'),
 
-  isDynamicStatusNeitherNoneNorReadyNorError: computed('dynamicStatus', function () {
-    const status = this.get('dynamicStatus');
-    return ![ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE, ENUMS.DYNAMIC_STATUS.ERROR].includes(status);
-  }),
+  isDynamicStatusNeitherNoneNorReadyNorError: computed(
+    'dynamicStatus',
+    function () {
+      const status = this.get('dynamicStatus');
+      return ![
+        ENUMS.DYNAMIC_STATUS.READY,
+        ENUMS.DYNAMIC_STATUS.NONE,
+        ENUMS.DYNAMIC_STATUS.ERROR,
+      ].includes(status);
+    }
+  ),
 
   isDynamicStatusNoneOrError: computed('dynamicStatus', function () {
     const status = this.get('dynamicStatus');
-    return [ENUMS.DYNAMIC_STATUS.NONE, ENUMS.DYNAMIC_STATUS.ERROR].includes(status);
+    return [ENUMS.DYNAMIC_STATUS.NONE, ENUMS.DYNAMIC_STATUS.ERROR].includes(
+      status
+    );
   }),
 
   isDynamicStatusNoneOrReady: computed('dynamicStatus', function () {
     const status = this.get('dynamicStatus');
-    return [ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE].includes(status);
+    return [ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE].includes(
+      status
+    );
   }),
 
   isDynamicStatusStarting: computed('dynamicStatus', function () {
     const status = this.get('dynamicStatus');
-    return ![ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE, ENUMS.DYNAMIC_STATUS.SHUTTING_DOWN].includes(status);
+    return ![
+      ENUMS.DYNAMIC_STATUS.READY,
+      ENUMS.DYNAMIC_STATUS.NONE,
+      ENUMS.DYNAMIC_STATUS.SHUTTING_DOWN,
+    ].includes(status);
   }),
 
   isNeitherNoneNorReady: computed('dynamicStatus', function () {
     const status = this.get('dynamicStatus');
-    return ![ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE].includes(status);
+    return ![ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE].includes(
+      status
+    );
   }),
 
   startingScanStatus: computed('dynamicStatus', function () {
     const status = this.get('dynamicStatus');
-    return ![ENUMS.DYNAMIC_STATUS.READY, ENUMS.DYNAMIC_STATUS.NONE, ENUMS.DYNAMIC_STATUS.SHUTTING_DOWN].includes(status);
+    return ![
+      ENUMS.DYNAMIC_STATUS.READY,
+      ENUMS.DYNAMIC_STATUS.NONE,
+      ENUMS.DYNAMIC_STATUS.SHUTTING_DOWN,
+    ].includes(status);
   }),
 
-  showScheduleAutomatedDynamicScan: computed('dynamicStatus', 'canRunAutomatedDynamicscan', function () {
-    const status = this.get('dynamicStatus');
-    return status !== ENUMS.DYNAMIC_STATUS.INQUEUE && this.get('canRunAutomatedDynamicscan');
-  }),
-
-  statusText: computed('dynamicStatus', 'tDeviceBooting', 'tDeviceDownloading', 'tDeviceHooking', 'tDeviceInQueue', 'tDeviceInstalling', 'tDeviceLaunching', 'tDeviceShuttingDown', 'tdeviceCompleted', function () {
-    const tDeviceInQueue = this.get("tDeviceInQueue");
-    const tDeviceBooting = this.get("tDeviceBooting");
-    const tDeviceDownloading = this.get("tDeviceDownloading");
-    const tDeviceInstalling = this.get("tDeviceInstalling");
-    const tDeviceLaunching = this.get("tDeviceLaunching");
-    const tDeviceHooking = this.get("tDeviceHooking");
-    const tDeviceShuttingDown = this.get("tDeviceShuttingDown");
-    const tDeviceCompleted = this.get("tdeviceCompleted");
-
-    switch (this.get("dynamicStatus")) {
-      case ENUMS.DYNAMIC_STATUS.INQUEUE:
-        return tDeviceInQueue;
-      case ENUMS.DYNAMIC_STATUS.BOOTING:
-        return tDeviceBooting;
-      case ENUMS.DYNAMIC_STATUS.DOWNLOADING:
-        return tDeviceDownloading;
-      case ENUMS.DYNAMIC_STATUS.INSTALLING:
-        return tDeviceInstalling;
-      case ENUMS.DYNAMIC_STATUS.LAUNCHING:
-        return tDeviceLaunching;
-      case ENUMS.DYNAMIC_STATUS.HOOKING:
-        return tDeviceHooking;
-      case ENUMS.DYNAMIC_STATUS.SHUTTING_DOWN:
-        return tDeviceShuttingDown;
-      case ENUMS.DYNAMIC_STATUS.COMPLETED:
-        return tDeviceCompleted;
-      default:
-        return "Unknown Status";
+  showScheduleAutomatedDynamicScan: computed(
+    'dynamicStatus',
+    'canRunAutomatedDynamicscan',
+    function () {
+      const status = this.get('dynamicStatus');
+      return (
+        status !== ENUMS.DYNAMIC_STATUS.INQUEUE &&
+        this.get('canRunAutomatedDynamicscan')
+      );
     }
-  }),
+  ),
+
+  statusText: computed(
+    'dynamicStatus',
+    'tDeviceBooting',
+    'tDeviceDownloading',
+    'tDeviceHooking',
+    'tDeviceInQueue',
+    'tDeviceInstalling',
+    'tDeviceLaunching',
+    'tDeviceShuttingDown',
+    'tdeviceCompleted',
+    function () {
+      const tDeviceInQueue = this.get('tDeviceInQueue');
+      const tDeviceBooting = this.get('tDeviceBooting');
+      const tDeviceDownloading = this.get('tDeviceDownloading');
+      const tDeviceInstalling = this.get('tDeviceInstalling');
+      const tDeviceLaunching = this.get('tDeviceLaunching');
+      const tDeviceHooking = this.get('tDeviceHooking');
+      const tDeviceShuttingDown = this.get('tDeviceShuttingDown');
+      const tDeviceCompleted = this.get('tdeviceCompleted');
+
+      switch (this.get('dynamicStatus')) {
+        case ENUMS.DYNAMIC_STATUS.INQUEUE:
+          return tDeviceInQueue;
+        case ENUMS.DYNAMIC_STATUS.BOOTING:
+          return tDeviceBooting;
+        case ENUMS.DYNAMIC_STATUS.DOWNLOADING:
+          return tDeviceDownloading;
+        case ENUMS.DYNAMIC_STATUS.INSTALLING:
+          return tDeviceInstalling;
+        case ENUMS.DYNAMIC_STATUS.LAUNCHING:
+          return tDeviceLaunching;
+        case ENUMS.DYNAMIC_STATUS.HOOKING:
+          return tDeviceHooking;
+        case ENUMS.DYNAMIC_STATUS.SHUTTING_DOWN:
+          return tDeviceShuttingDown;
+        case ENUMS.DYNAMIC_STATUS.COMPLETED:
+          return tDeviceCompleted;
+        default:
+          return 'Unknown Status';
+      }
+    }
+  ),
 
   setDynamicStatus(status) {
     this.store.push({
@@ -254,9 +291,9 @@ const File = Model.extend(BaseModelMixin, {
         id: this.id,
         type: this.constructor.modelName,
         attributes: {
-          'dynamicStatus': status
-        }
-      }
+          dynamicStatus: status,
+        },
+      },
     });
   },
 
@@ -282,8 +319,7 @@ const File = Model.extend(BaseModelMixin, {
 
   setReady() {
     this.setDynamicStatus(ENUMS.DYNAMIC_STATUS.READY);
-  }
+  },
 });
-
 
 export default File;
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,6 +1,6 @@
 const CONSTANTS = {
   ANDROID_STORE_URL_RE: /(http(s?):\/\/play.google.com\/store\/apps\/details\?id=([A-Za-z].*))|(market:\/\/[A-Za-z].*)/,
-  WINDOWS_STORE_URL_RE: /microsoft.com(.*)store\/p\/(.*)\/(.*)/
+  WINDOWS_STORE_URL_RE: /microsoft.com(.*)store\/p\/(.*)\/(.*)/,
 };
 
 export const RISK_COLOR_CODE = {
@@ -9,14 +9,27 @@ export const RISK_COLOR_CODE = {
   WARNING: '#F5D76E',
   INFO: '#2CC2F8',
   SUCCESS: '#80C081',
-  DEFAULT: '#6B6B6B'
-}
+  DEFAULT: '#6B6B6B',
+};
 //Configurable input constants
 export const INPUT = {
-  MIN_LENGTH: 3
+  MIN_LENGTH: 3,
 };
 
 // File details tag max limit
 export const FILE_TAG_MAX_CHAR = 240;
+export const REPORT = {
+  MAX_LIMIT: 2,
+  STATUS: {
+    GENERATING: 'Generating',
+    GENERATED: 'Generated',
+    GENERATE: 'Generate',
+  },
+  TYPE: {
+    PDF: 'pdf',
+    EXCEL: 'xlsx',
+    JSON: 'json',
+  },
+};
 
 export default CONSTANTS;
