d7afd27563764205a6a95a086ff8106a2f4abe9c
Gugan <guganssp@gmail.com>
2021-02-08 17:30:30 +0530

    Disable analytics for member role

--- app/routes/authenticated/analytics.js
+++ app/routes/authenticated/analytics.js
@@ -1,7 +1,20 @@
 import Route from '@ember/routing/route';
-import { ScrollTopMixin } from '../../mixins/scroll-top';
+import {
+  ScrollTopMixin
+} from '../../mixins/scroll-top';
+import {
+  inject as service
+} from '@ember/service';
 
 export default class AuthenticatedAnalyticsRoute extends ScrollTopMixin(Route) {
+
+  @service me;
+  beforeModel() {
+    if (this.get('me.org.is_member')) {
+      this.transitionTo('authenticated.projects');
+    }
+  }
+
   model() {
     return this.modelFor("authenticated");
   }
