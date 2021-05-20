71ccfa8a4c707bd67c03abd4c0d79ca32bb75f24
Praseetha-KR <praseetha04@gmail.com>
2021-05-20 20:39:13 +0530

    Basic theming for modal, partner client styles refinement

--- app/models/partnerclient-plan.js
+++ app/models/partnerclient-plan.js
@@ -4,6 +4,7 @@ import Model, {
 import {
   computed
 } from '@ember/object';
+import dayjs from 'dayjs';
 
 export default class PartnerclientPlanModel extends Model {
 
@@ -11,6 +12,11 @@ export default class PartnerclientPlanModel extends Model {
   @attr('boolean') limitedScans;
   @attr('number') projectsLimit;
   @attr('date') expiryDate;
+
+  @computed('expiryDate')
+  get isPaymentExpired() {
+    return dayjs().isAfter(this.expiryDate);
+  }
 
   @computed('projectsLimit', 'limitedScans')
   get invalidPayment() {
