f4191072ac79cc76c37ff2d82c155aee73ec349f
Gugan <guganssp@gmail.com>
2021-05-21 11:33:06 +0530

    Removed partner client stat api reference

--- app/adapters/partnerclient-statistic.js
+++
@@ -1,7 +0,0 @@
-import commondrf from './commondrf';
-
-export default class PartnerclientStatisticAdapter extends commondrf {
-  buildURL(modelName, id) {
-    return this.buildURLFromBase(`${this.namespace_v2}/partnerclients/${id}/statistics`);
-  }
-}
