7ed58a2da9a532e784a22e93432b5937b3bc7eef
Praseetha-KR <praseetha04@gmail.com>
2021-05-24 14:48:03 +0530

    Add component clients-nav

---
+++ app/components/partner/clients-nav/index.js
@@ -0,0 +1,30 @@
+import Component from '@glimmer/component';
+import { inject } from '@ember/inject';
+
+export default class PartnerClientNavComponent extends Component {
+  @inject intl;
+  @inject router;
+  @inject partner;
+
+  tabs = [
+    {
+      label: this.intl.t('overview'),
+      enabled: true,
+      link: 'authenticated.partner.clients.overview'
+    },
+    {
+      label: this.intl.t('invitations'),
+      enabled: this.partner.access.invite_clients,
+      link: 'authenticated.partner.clients.invitations'
+    },
+    {
+      label: this.intl.t('registrationRequests'),
+      enabled: this.partner.access.invite_clients,
+      link: 'authenticated.partner.clients.registration-requests'
+    },
+  ];
+
+  get currentRoute() {
+    return this.router.currentRoute.name;
+  }
+}
