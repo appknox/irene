a6ff78e7772474bbf5f951e6964d211ff3123595
Praseetha-KR <praseetha04@gmail.com>
2021-05-25 12:49:03 +0530

    Cleanup and disabled partner tests

--- tests/unit/routes/authenticated/partner/clients/overview-test.js
+++ tests/unit/routes/authenticated/partner/clients/overview-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | authenticated/partner/invitations', function(hooks) {
+module('Unit | Route | authenticated/partner/clients/overview', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:authenticated/partner/invitations');
+    let route = this.owner.lookup('route:authenticated/partner/clients/overview');
     assert.ok(route);
   });
 });
