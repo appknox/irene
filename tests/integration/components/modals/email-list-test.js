a6bfc562699fd86d0ff2fb45b45a425a1c4d4fcf
Gugan <guganssp@gmail.com>
2021-05-31 16:08:15 +0530

    Created email list modal and fixed review comments

---
+++ tests/integration/components/modals/email-list-test.js
@@ -0,0 +1,89 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | modals/email-list', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Modals::EmailList />`);
+    assert.dom('div[data-test-email-list]').exists();
+  });
+
+  test('it renders 1 default email', async function (assert) {
+    const emails = [];
+    for (let i = 0; i <= 5; i++) {
+      emails.push(`test+${i}@test.app`);
+    }
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}}/>`);
+    assert.dom('div[data-test-default-email]').hasText(this.emails.firstObject)
+  });
+
+  test('it renders 3 default emails', async function (assert) {
+    const emails = [];
+    for (let i = 0; i <= 5; i++) {
+      emails.push(`test+${i}@test.app`);
+    }
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}} @defaultCount=3/>`);
+    const defaultEmails = this.emails.slice(0, 3).join(', ');
+    assert.dom('div[data-test-default-email]').hasText(defaultEmails)
+  });
+
+  test('it renders button with remaining count as +5', async function (assert) {
+    const emails = [];
+    for (let i = 0; i <= 5; i++) {
+      emails.push(`test+${i}@test.app`);
+    }
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}}/>`);
+    assert.dom('button[data-test-owner-list-btn]').hasText('+5');
+  });
+
+  test('it should not render remaining count button', async function (assert) {
+    const emails = [];
+    for (let i = 0; i < 1; i++) {
+      emails.push(`test+${i}@test.app`);
+    }
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}}/>`);
+    assert.dom('button[data-test-owner-list-btn]').doesNotExist()
+  });
+
+  test('it should open email list with modal when clicking on remaining count btn', async function (assert) {
+    const emails = [];
+    for (let i = 0; i <= 5; i++) {
+      emails.push(`test+${i}@test.app`);
+    }
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}}/>`);
+    await click(this.element.querySelector('button[data-test-owner-list-btn]'));
+    assert.dom('div[data-test-email-list-modal]').exists();
+  });
+
+  test('it should show 6 emails in the modal', async function (assert) {
+    const emails = [];
+    for (let i = 0; i <= 5; i++) {
+      emails.push(`test+${i}@test.app`);
+    }
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}}/>`);
+    await click(this.element.querySelector('button[data-test-owner-list-btn]'));
+    for (let i = 0; i <= 5; i++) {
+      assert.dom(`div[data-test-email-list-item="${i}"]`).hasText(`${i + 1}. test+${i}@test.app`);
+    }
+  });
+
+  test('it renders empty when empty email is sent', async function (assert) {
+    const emails = [];
+    this.set('emails', emails)
+    await render(hbs`<Modals::EmailList @emails={{this.emails}}/>`);
+
+    assert.dom('div[data-test-default-email]').hasText('')
+  });
+});
--- tests/integration/components/partner/client-info/component-test.js
+++ tests/integration/components/partner/client-info/component-test.js
@@ -19,9 +19,8 @@ import {
 } from 'ember-intl/test-support';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import faker from 'faker';
 
-module('Integration | Component | cards/client-info', function (hooks) {
+module('Integration | Component | partner/client-info', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
   setupIntl(hooks);
@@ -29,29 +28,28 @@ module('Integration | Component | cards/client-info', function (hooks) {
   hooks.beforeEach(async function () {
     await this.server.createList('organization', 2);
     await this.owner.lookup('service:organization').load();
-    // await this.owner.lookup('service:partner').load();
   });
 
-  test('Client thumbnail', async function (assert) {
+  test('it should render client thumbnail', async function (assert) {
     const client = this.server.create('partnerclient');
     this.set('client', client);
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
-    assert.dom('span[data-test-thumbnail] i').hasClass('fa-users')
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
+    assert.dom('span[data-test-thumbnail]').exists();
   });
 
-  test("Client has valid name", async function (assert) {
+  test("it should render client name", async function (assert) {
     const client = this.server.create('partnerclient');
     this.set('client', client);
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
     assert.dom('div[data-test-title]').hasText(this.client.name);
   });
 
-  test("Client doesn't have name", async function (assert) {
+  test("it should handle empty client name", async function (assert) {
     const client = this.server.create('partnerclient', {
       name: null
     });
     this.set('client', client);
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
     assert.dom('div[data-test-title]').hasText(`t:noName:()`);
     assert.dom('div[data-test-title]').hasStyle({
       "color": "rgb(66, 70, 81)",
@@ -60,38 +58,65 @@ module('Integration | Component | cards/client-info', function (hooks) {
     });
   });
 
-  test('No uploads scenario', async function (assert) {
+  //pending client name visibility ellipsis
+
+  test('it should no uploades done by the client', async function (assert) {
     const client = this.server.create('partnerclient');
     client.lastUploadedOn = null;
     this.set('client', client);
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
     assert.dom('div[data-test-last-upload]').hasText('t:noUploads:()')
   });
 
-  test('Last uploaded date shown in relative time', async function (assert) {
+  test('it should show last uploded date in relative format', async function (assert) {
     const client = this.server.create('partnerclient');
     this.set('client', client);
     dayjs.extend(relativeTime)
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
     assert.dom('div[data-test-last-upload-label]').hasText(`t:lastUpload:()`);
     assert.dom('div[data-test-last-upload]').hasText(dayjs(this.client.lastUploadedOn).fromNow())
   });
 
-  test("2 owners for a client", async function (assert) {
+  test("it should render client owner list", async function (assert) {
     const client = this.server.create('partnerclient');
     this.set('client', client);
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
-    assert.dom('span[data-test-first-owner]').hasText(this.client.ownerEmails.firstObject);
-    assert.dom('button[data-test-owner-list-btn]').exists();
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
+    assert.dom('div[data-test-client-owner-emails]').exists();
   });
 
-  test("One owner for a client", async function (assert) {
-    const client = this.server.create('partnerclient', {
-      ownerEmails: [faker.internet.email()]
-    });
+  test('it should render payment plan section, if view_plans enabled', async function (assert) {
+    const client = this.server.create('partnerclient');
+    await this.owner.lookup('service:partner').load();
+    this.server.get('v2/partnerclients/:id/plan', (schema, req) => {
+      return {
+        id: req.params.id,
+        data: {}
+      }
+    })
+
     this.set('client', client);
-    await render(hbs `<Partner::ClientInfo @client={{this.client}}/>`);
-    assert.dom('span[data-test-first-owner]').hasText(this.client.ownerEmails.firstObject);
-    assert.dom('button[data-test-owner-list-btn]').doesNotExist();
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
+    assert.dom('div[data-test-payment-plan-row]').exists();
+    assert.dom('div[data-test-payment-plan-label]').hasText(`t:paymentPlan:()`);
+    assert.dom('div[data-test-payment-plan]').exists()
+  })
+
+  test('it should not render payment plan, if error occurred during plan details fetch', async function (assert) {
+    await this.owner.lookup('service:partner').load();
+    this.server.get('v2/partnerclients/:id/plan', () => {
+      return Response(500)
+    })
+    const client = this.server.create('partnerclient');
+    this.set('client', client);
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
+    assert.dom('div[data-test-payment-plan-label]').hasText(`t:paymentPlan:()`);
+    assert.dom('div[data-test-payment-plan]').doesNotExist();
+  })
+
+  test('it should not render payment plan section, if view_plans is not enabled', async function (assert) {
+    const client = this.server.create('partnerclient');
+    this.set('client', client);
+    await render(hbs`<Partner::ClientInfo @client={{this.client}}/>`);
+    assert.dom('div[data-test-payment-plan-row]').doesNotExist();
   })
 });
