b1be7c6e358b7de7e5bff7b524c659e37831af50
Gugan <guganssp@gmail.com>
2021-06-02 11:52:06 +0530

    Removed computed decorators
    computed decorator may not required for one time instance properties

--- app/components/modals/email-list/index.js
+++ app/components/modals/email-list/index.js
@@ -1,5 +1,5 @@
 import Component from "@glimmer/component";
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { isArray } from '@ember/array';
 
@@ -7,27 +7,22 @@ export default class EmailList extends Component {
 
   @tracked showEmailModal = false;
 
-  @computed('args.defaultCount')
   get defaultCount() {
     return this.args.defaultCount || 1;
   }
 
-  @computed('args.emails')
   get emails() {
     return isArray(this.args.emails) ? this.args.emails : [this.args.emails];
   }
 
-  @computed('defaultCount', 'emails.length')
   get remainingCount() {
     return this.emails.length - this.defaultCount;
   }
 
-  @computed('emails', 'defaultCount')
   get defaultEmails() {
     return this.emails.slice(0, this.defaultCount).join(', ');
   }
 
-  @computed('args.headerTitle')
   get modalHeaderTitle() {
     return this.args.headerTitle || "Email List";
   }
