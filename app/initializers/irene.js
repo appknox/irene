09f7b67f3b4aa86670a77917aca0e4be371aa61c
Sharat M R <cosmosgenius@gmail.com>
2021-06-18 21:56:28 +0530

    remove IRENE_API_SOCKET_PATH and socketPath

--- app/initializers/irene.js
+++ app/initializers/irene.js
@@ -117,7 +117,6 @@ const initialize = function (application) {
 
   ENV.host = handler.getEnv('IRENE_API_HOST');
   ENV.devicefarmHost = handler.getEnv('IRENE_DEVICEFARM_HOST');
-  ENV.socketPath = handler.getEnv('IRENE_API_SOCKET_PATH');
   ENV.isEnterprise = handler.getBoolean('ENTERPRISE');
   ENV.showLicense = handler.getBoolean('IRENE_SHOW_LICENSE');
 
