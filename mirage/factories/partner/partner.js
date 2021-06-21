9638e4e7a281088d621a94883a92a1b61869ca08
Praseetha-KR <praseetha04@gmail.com>
2021-06-21 20:20:02 +0530

    Registration requests tab visibility based on admin_registration privilege + tests

--- mirage/factories/partner/partner.js
+++ mirage/factories/partner/partner.js
@@ -5,6 +5,9 @@ export default Factory.extend({
     return {
       view_plans: false,
       transfer_credits: false,
+      list_projects: false,
+      list_files: false,
+      admin_registration: false,
     };
   },
 });
