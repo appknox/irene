121ab47f46db157c10e72c034d1cc4e2bb7d376c
Praseetha-KR <praseetha04@gmail.com>
2021-06-21 21:45:38 +0530

    Restrict registration-requests route based on privilege

--- app/routes/authenticated/partner/clients/registration-requests.js
+++ app/routes/authenticated/partner/clients/registration-requests.js
@@ -6,7 +6,10 @@ export default class AuthenticatedPartnerRegistrationRequestsRoute extends Route
   @service partner;
 
   beforeModel() {
-    if (!this.get('me.org.can_access_partner_dashboard')) {
+    if (
+      !this.me.get('org.can_access_partner_dashboard') ||
+      !this.partner.get('access.admin_registration')
+    ) {
       this.transitionTo('authenticated.partner.clients');
     }
   }
