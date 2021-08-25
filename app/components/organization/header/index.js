245d9e7b21e107c9e2bae1c5986dae197eb2a401
Gugan <guganssp@gmail.com>
2021-08-25 17:06:07 +0530

    Integrated Edit name functionality

---
+++ app/components/organization/header/index.js
@@ -0,0 +1,104 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { parseError } from 'irene/utils/utils';
+import { validateLength } from 'ember-changeset-validations/validators';
+import Changeset from 'ember-changeset';
+import lookupValidator from 'ember-changeset-validations';
+
+export default class OrganizationHeaderComponent extends Component {
+  // Dependencies
+  @service('notifications') notify;
+  @service intl;
+
+  // Properties
+  /**
+   * @property {String} orgName
+   */
+  @tracked orgnamePOJO = {
+    organizationName: this.selectedOrgName,
+  };
+
+  /**
+   * @property {Boolean} isShowEditOrgName
+   */
+  @tracked isShowEditOrgName = false;
+
+  /**
+   * @property {object} orgNameValidator
+   */
+  get orgNameValidator() {
+    return {
+      organizationName: validateLength({ min: 1, max: 255 }),
+    };
+  }
+
+  get selectedOrgName() {
+    return this.args.organization.name;
+  }
+
+  get isShowEdit() {
+    return this.args.isShowEdit && this.args.isOwner;
+  }
+
+  get isShowSettings() {
+    return (this.args.isAdmin || this.args.isOwner) && this.args.isShowSettings;
+  }
+
+  // Actions
+
+  // Action triggered while DOM initialized {{did-insert}}
+  @action
+  initCompo() {
+    if (this.isShowEdit) {
+      this.changeset = new Changeset(
+        this.orgnamePOJO,
+        lookupValidator(this.orgNameValidator),
+        this.orgNameValidator
+      );
+    }
+  }
+
+  // Action triggered while clicking edit name action elements
+  @action
+  toggleEditNameModal() {
+    this.isShowEditOrgName = !this.isShowEditOrgName;
+    this.orgName = this.selectedOrgName;
+  }
+
+  // Action triggered while clicking at save btn
+  @action
+  updateOrgName() {
+    this.updateOrganizationName.perform();
+  }
+
+  // Action triggered while clicking at back
+  @action
+  backTo() {
+    window.history.back(); //Back to last accessed url
+  }
+
+  // Functions
+
+  // Function to update organization name
+  @task(function* () {
+    yield this.changeset.validate();
+    if (this.changeset.get('isValid')) {
+      try {
+        const org = this.args.organization;
+        yield org.set('name', this.changeset.organizationName);
+        yield org.save();
+        this.notify.success(this.intl.t('organizationNameUpdated'));
+        this.toggleEditNameModal();
+      } catch (err) {
+        this.get('notify').error(
+          parseError(err),
+          this.intl.t('pleaseTryAgain')
+        );
+      }
+    }
+  })
+  updateOrganizationName;
+}
--- tests/integration/components/organization/header-test.js
+++ tests/integration/components/organization/header-test.js
@@ -1,24 +1,54 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, fillIn } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import faker from 'faker';
 import { setupIntl } from 'ember-intl/test-support';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import Service from '@ember/service';
+
+class NotificationsStub extends Service {
+  errorMsg = null;
+  successMsg = null;
+  error(msg) {
+    return (this.errorMsg = msg);
+  }
+  success(msg) {
+    return (this.successMsg = msg);
+  }
+}
 
 module('Integration | Component | organization/header', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
   setupIntl(hooks);
 
-  test('it should show label and org name', async function (assert) {
-    this.set('name', faker.company.companyName());
-    await render(hbs`<Organization::Header @name={{this.name}}/>`);
-    assert.dom(`[data-test-org-name-label]`).hasText(`t:organizationName:()`);
-    assert.dom(`[data-test-org-name-text]`).hasText(this.name);
+  hooks.beforeEach(async function () {
+    await this.server.createList('organization', 1);
+    await this.owner.lookup('service:organization').load();
+    this.owner.register('service:notifications', NotificationsStub);
+  });
+
+  test('it renders org name', async function (assert) {
+    this.set('isOwner', true);
+    this.set(
+      'organization',
+      this.owner.lookup('service:organization').selected
+    );
+    await render(
+      hbs`<Organization::Header @isOwner={{this.isOwner}} @organization={{this.organization}}/>`
+    );
+    assert.dom('[data-test-org-name-label]').hasText(`t:organizationName:()`);
+    assert.dom('[data-test-org-name-text]').hasText(this.organization.name);
   });
 
   test('it should show ellipsis for 255 chars', async function (assert) {
-    this.set('name', 'a'.repeat(255));
-    await render(hbs`<Organization::Header @name={{this.name}}/>`);
+    this.set('isOwner', true);
+    const selectedOrg = this.owner.lookup('service:organization').selected;
+    selectedOrg.set('name', 'a'.repeat(255));
+    this.set('organization', selectedOrg);
+    await render(
+      hbs`<Organization::Header @isOwner={{this.isOwner}} @organization={{this.organization}}/>`
+    );
     const orgNameTextEl = this.element.querySelector(
       `[data-test-org-name-text]`
     );
@@ -28,30 +58,110 @@ module('Integration | Component | organization/header', function (hooks) {
     );
   });
 
-  test('it should not show ellipsis for 50 chars', async function (assert) {
-    this.set('name', 'a'.repeat(50));
-    await render(hbs`<Organization::Header @name={{this.name}}/>`);
-    const orgNameTextEl = this.element.querySelector(
-      `[data-test-org-name-text]`
+  test('it should not show edit button', async function (assert) {
+    this.set('isOwner', false);
+    this.set('isShowEdit', false);
+    this.set(
+      'organization',
+      this.owner.lookup('service:organization').selected
     );
-    assert.false(
-      orgNameTextEl.scrollWidth > orgNameTextEl.clientWidth,
-      'Ellipsis not applied'
+    await render(
+      hbs`<Organization::Header @isOwner={{this.isOwner}} @organization={{this.organization}} @isShowEdit={{this.isShowEdit}}/>`
     );
+    assert.dom('[data-test-org-name-edit-btn]').doesNotExist();
   });
 
-  test('it should show org settings button with link to organization-settings route', async function (assert) {
-    this.set('isAdmin', true);
-    await render(hbs`<Organization::Header @isAdmin={{this.isAdmin}}/>`);
+  test('clicking on edit btn should open edit org name modal', async function (assert) {
+    this.set('isOwner', true);
+    this.set('isShowEdit', true);
+    this.set(
+      'organization',
+      this.owner.lookup('service:organization').selected
+    );
+    await render(
+      hbs`<Organization::Header @isShowEdit={{this.isShowEdit}} @isOwner={{this.isOwner}} @organization={{this.organization}}/>`
+    );
+    await click(this.element.querySelector('[data-test-org-name-edit-btn]'));
+
     assert
-      .dom(`[data-test-org-settings-link]`)
-      .hasAttribute('href', '/organization-settings');
-    assert.dom(`[data-test-org-settings-link]`).doesNotHaveAttribute('target');
+      .dom('[data-test-org-edit-modal-label]')
+      .hasText(`t:editOrganizationNameModalLabel:()`);
+    assert.dom(`[data-test-org-edit-modal-cancel-btn]`).hasText(`t:cancel:()`);
+    assert.dom(`[data-test-org-edit-modal-save-btn]`).hasText(`t:save:()`);
+    assert
+      .dom(`[data-test-org-edit-modal-input]`)
+      .hasValue(this.organization.name);
   });
 
-  test('it should not show org settings button', async function (assert) {
-    this.set('isAdmin', false);
-    await render(hbs`<Organization::Header @isAdmin={{this.isAdmin}}/>`);
-    assert.dom(`[data-test-org-settings-link]`).doesNotExist();
+  test('it should handle org name update success workflow', async function (assert) {
+    this.set('isShowEdit', true);
+    this.server.put('/organizations/:id', (_, req) => {
+      return req.requestBody;
+    });
+    this.set('isOwner', true);
+    this.set(
+      'organization',
+      this.owner.lookup('service:organization').selected
+    );
+    const notify = this.owner.lookup('service:notifications');
+    await render(
+      hbs`<Organization::Header @isShowEdit={{this.isShowEdit}} @isOwner={{this.isOwner}} @organization={{this.organization}}/>`
+    );
+    await click(this.element.querySelector('[data-test-org-name-edit-btn]'));
+
+    await fillIn(`[data-test-org-edit-modal-input]`, 'Test Org');
+
+    await click(`[data-test-org-edit-modal-save-btn]`);
+
+    assert.equal(
+      notify.get('successMsg'),
+      `t:organizationNameUpdated:()`,
+      `t:organizationNameUpdated:()`
+    );
+
+    assert.dom(`[data-test-org-edit-modal]`).doesNotExist();
+    assert.dom(`[data-test-org-name-text]`).hasText(this.organization.name);
+  });
+
+  test('it should show error and disable save btn if org name exceed 255 char', async function (assert) {
+    this.set('isOwner', true);
+    this.set('isShowEdit', true);
+    this.set(
+      'organization',
+      this.owner.lookup('service:organization').selected
+    );
+    await render(
+      hbs`<Organization::Header @isShowEdit={{this.isShowEdit}} @isOwner={{this.isOwner}} @organization={{this.organization}}/>`
+    );
+    await click(this.element.querySelector('[data-test-org-name-edit-btn]'));
+
+    await fillIn(`[data-test-org-edit-modal-input]`, 'T'.repeat(256));
+
+    assert.dom(`[data-test-org-edit-modal-save-btn]`).hasAttribute('disabled');
+
+    assert.dom(`[data-test-input-error-org-name]`).exists();
+
+    await fillIn(`[data-test-org-edit-modal-input]`, '');
+
+    assert.dom(`[data-test-org-edit-modal-save-btn]`).hasAttribute('disabled');
+
+    assert.dom(`[data-test-input-error-org-name]`).exists();
+  });
+
+  test('clicking on cancel btn should close the modal', async function (assert) {
+    this.set('isOwner', true);
+    this.set('isShowEdit', true);
+    this.set(
+      'organization',
+      this.owner.lookup('service:organization').selected
+    );
+    await render(
+      hbs`<Organization::Header @isShowEdit={{this.isShowEdit}} @isOwner={{this.isOwner}} @organization={{this.organization}}/>`
+    );
+    await click(this.element.querySelector('[data-test-org-name-edit-btn]'));
+
+    assert.dom('[data-test-org-edit-modal]').exists();
+    await click(`[data-test-org-edit-modal-cancel-btn]`);
+    assert.dom('[data-test-org-edit-modal]').doesNotExist();
   });
 });
