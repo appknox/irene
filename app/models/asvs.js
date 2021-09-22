929d11992b4693d8f4a0943d4b23bf3e79677efa
Praseetha-KR <praseetha04@gmail.com>
2021-09-22 08:14:15 +0530

    Security dashboard: asvs pluralization & regulatory titles fix

--- app/models/asvs.js
+++ app/models/asvs.js
@@ -1,8 +1,4 @@
 import Model, { attr } from '@ember-data/model';
-import Inflector from 'ember-inflector';
-
-const inflector = Inflector.inflector;
-inflector.irregular('asvs', 'asvses');
 
 export default class Asvs extends Model {
   @attr('string') code;
