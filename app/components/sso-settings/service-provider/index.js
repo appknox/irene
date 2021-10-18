453e4dc5a0524380ef906db4839af934179b0104
Gugan <guganssp@gmail.com>
2021-10-18 17:42:51 +0530

    WIP - component refactor

--- app/components/sso-settings/service-provider/index.js
+++ app/components/sso-settings/service-provider/index.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import { action, set } from '@ember/object';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class SsoSettingsServiceProvider extends Component {
@@ -26,6 +26,7 @@ export default class SsoSettingsServiceProvider extends Component {
   }
 
   get isShowXmlMetadata() {
+    return true;
     return this.activeConfigType == 'xml';
   }
 
--- tests/integration/components/sso-settings/identity-provider-test.js
+++ tests/integration/components/sso-settings/identity-provider-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | sso-settings/service-provider-manual-setting', function(hooks) {
+module('Integration | Component | sso-settings/identity-provider', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<SsoSettings::ServiceProviderManualSetting />`);
+    await render(hbs`<SsoSettings::IdentityProvider />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <SsoSettings::ServiceProviderManualSetting>
+      <SsoSettings::IdentityProvider>
         template block text
-      </SsoSettings::ServiceProviderManualSetting>
+      </SsoSettings::IdentityProvider>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
