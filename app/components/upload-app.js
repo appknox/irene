9c040c9f7b726d61f9be2b12f9923ebe0470ab36
Yibaebi <elliot.yibaebi@yahoo.com>
2022-05-23 11:08:36 +0100

    fix upload-app button migration issue

--- app/components/upload-app.js
+++ app/components/upload-app.js
@@ -1,13 +1,15 @@
 /* eslint-disable prettier/prettier, ember/no-get, ember/no-jquery */
+import { getOwner } from '@ember/application';
 import { inject as service } from '@ember/service';
 import { isEmpty } from '@ember/utils';
-import FileUploader from 'irene/utils/uploader';
-import FileField from 'ember-uploader/components/file-field';
 import { t } from 'ember-intl';
-import $ from 'jquery';
-import { getOwner } from '@ember/application';
-import triggerAnalytics from 'irene/utils/trigger-analytics';
+import FileField from 'ember-uploader/components/file-field';
 import ENV from 'irene/config/environment';
+import triggerAnalytics from 'irene/utils/trigger-analytics';
+import FileUploader from 'irene/utils/uploader';
+import $ from 'jquery';
+
+// export default class HomePageComponent extends Component{}
 
 const UploadAppComponent = FileField.extend({
   store: service('store'),
@@ -15,38 +17,44 @@ const UploadAppComponent = FileField.extend({
   intl: service('intl'),
   rollbar: service('rollbar'),
 
+  tErrorWhileFetching: t('errorWhileFetching'),
+  tErrorWhileUploading: t('errorWhileUploading'),
+  tFileUploadedSuccessfully: t('fileUploadedSuccessfully'),
+  notify: service('notifications'),
 
-  tErrorWhileFetching: t("errorWhileFetching"),
-  tErrorWhileUploading: t("errorWhileUploading"),
-  tFileUploadedSuccessfully: t("fileUploadedSuccessfully"),
-  notify: service("notifications"),
-
-  classNames: ["file-input"],
+  classNames: ['file-input'],
   async filesDidChange(files) {
-    const delegate = this.get("delegate");
+    const delegate = this.get('delegate');
+
     if (isEmpty(files)) {
       return;
     }
-    delegate.set("isUploading", true);
-    delegate.set("progress", 0);
+    // delegate.set('isUploading', true);
+    delegate.setAppUploadStatus(true);
+    // delegate.set('progress', 0);
+    delegate.setAppUploadProgress(0);
+
     const uploader = FileUploader.create({ container: getOwner(this) });
-    uploader.on('progress', e => delegate.set("progress", parseInt(e.percent)));
+    uploader.on('progress', (e) =>
+      // delegate.set('progress', parseInt(e.percent))
+      delegate.setAppUploadProgress(parseInt(e.percent))
+    );
     try {
-      const uploadItem = await this.get("store").queryRecord('uploadApp', {});
+      const uploadItem = await this.get('store').queryRecord('uploadApp', {});
       await uploader.uploadFile(files[0], uploadItem.get('url'));
       await uploadItem.save();
-      triggerAnalytics('feature',ENV.csb.applicationUpload);
-      this.get("notify").success(this.get('tFileUploadedSuccessfully'));
+      triggerAnalytics('feature', ENV.csb.applicationUpload);
+      this.get('notify').success(this.get('tFileUploadedSuccessfully'));
     } catch (e) {
       // eslint-disable-next-line no-console
       const err = this.get('tErrorWhileUploading');
-      this.get("notify").error(err);
+      this.get('notify').error(err);
       this.get('rollbar').critical(err, e);
     }
     $('input[type=file]').val('');
-    delegate.set("isUploading", false);
+    delegate.setAppUploadStatus(false);
+    // delegate.set('isUploading', false);
   },
 });
-
 
 export default UploadAppComponent;
