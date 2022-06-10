6cd81c947a1f7fd36b38f684ef1530efcc333733
Yibaebi <elliot.yibaebi@yahoo.com>
2022-06-10 14:01:47 +0100

    experiment: integrate new pagination with project list page

--- app/components/pagination/index.js
+++ app/components/pagination/index.js
@@ -3,83 +3,107 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class PaginationComponent extends Component {
-  @tracked pageItemsCountSelectOptions = [
-    {
-      selected: true,
-      value: 5,
-    },
-    {
-      selected: false,
-      value: 10,
-    },
-    {
-      selected: false,
-      value: 20,
-    },
-    {
-      selected: false,
-      value: 30,
-    },
-    {
-      selected: false,
-      value: 50,
-    },
-  ];
-
-  @tracked currentPageNumber = 1;
-
-  get totalPages() {
-    return Math.ceil(this.args.totalItems / this.selectedItemsPerPage);
-  }
-
-  get startIndex() {
-    return (this.currentPageNumber - 1) * this.selectedItemsPerPage + 1;
-  }
-
-  get endOffset() {
-    return Math.min(
-      this.startIndex + this.selectedItemsPerPage - 1,
-      this.args.totalItems
-    );
-  }
-
-  get selectedItemsPerPage() {
-    return this.pageItemsCountSelectOptions.find((item) => item.selected).value;
-  }
+  @tracked itemPerPageSelectOptions = this.defaultItemPerPageSelectOptions;
+  @tracked offset = 1;
 
   get currentPageResults() {
     return this.args.results;
   }
 
-  @action goToNextPage() {
-    this.currentPageNumber =
-      this.currentPageNumber < this.totalPages
-        ? this.currentPageNumber + 1
-        : this.totalPages;
-
-    this.args.nextAction({
-      startIdx: this.startIndex,
-      endOffset: this.endOffset,
-      selectedItemsPerPage: this.selectedItemsPerPage,
-    });
+  get defaultItemPerPageSelectOptions() {
+    return this._resolveReceivedPageItemsCountOptions(
+      this.args.itemPerPageSelectOptions
+    );
   }
 
-  @action goToPrevPage() {
-    this.currentPageNumber =
-      this.currentPageNumber > 1 ? this.currentPageNumber - 1 : 1;
+  get disableNext() {
+    return this.offset === this.totalPages;
+  }
+  get disablePrev() {
+    return this.offset === 1;
+  }
 
-    this.args.prevAction({
-      startIdx: this.startIndex,
-      endOffset: this.endOffset,
-      selectedItemsPerPage: this.selectedItemsPerPage,
-    });
+  get endItemIdx() {
+    return Math.min(
+      this.startItemIdx + this.selectedItemsPerPage - 1,
+      this.args.totalItems
+    );
+  }
+
+  get nextAction() {
+    return this._resolveFunctionProp(this.args.nextAction);
+  }
+
+  get onItemsPerPageChange() {
+    return this._resolveFunctionProp(this.args.onPageItemsCountChange);
+  }
+
+  get prevAction() {
+    return this._resolveFunctionProp(this.args.prevAction);
+  }
+
+  get selectedItemsPerPage() {
+    return this.itemPerPageSelectOptions.find((item) => item.selected).value;
+  }
+
+  get startItemIdx() {
+    return (this.offset - 1) * this.selectedItemsPerPage + 1;
+  }
+
+  get totalPages() {
+    return Math.ceil(this.args.totalItems / this.selectedItemsPerPage);
   }
 
   @action
   onItemsPerPageSelect(event) {
-    this.pageItemsCountSelectOptions = this.pageItemsCountSelectOptions.map(
+    this._updatePageItemSelectOptions(event.target.value);
+    this.onItemsPerPageChange({
+      selectedItemsPerPage: this.selectedItemsPerPage,
+    });
+    this._resetOffset();
+  }
+
+  @action goToNextPage() {
+    this.offset =
+      this.offset < this.totalPages ? this.offset + 1 : this.totalPages;
+    this.nextAction({
+      selectedItemsPerPage: this.selectedItemsPerPage,
+      offset: this.offset,
+    });
+  }
+
+  @action goToPrevPage() {
+    this.offset = this.offset > 1 ? this.offset - 1 : 1;
+    this.prevAction({
+      selectedItemsPerPage: this.selectedItemsPerPage,
+      offset: this.offset,
+    });
+  }
+
+  // Check if callback function props are valid before calls
+  _resolveFunctionProp(value) {
+    return value && typeof value === 'function' ? value : () => null;
+  }
+
+  // Check if received page items count items are valid
+  _resolveReceivedPageItemsCountOptions(items) {
+    const allReceivedItemsAreValid =
+      Array.isArray(items) && items.every((item) => !isNaN(Number(item)));
+    const defaultItems = allReceivedItemsAreValid
+      ? items
+      : [10, 20, 30, 40, 40];
+
+    return defaultItems.map((option, idx) => ({
+      selected: idx === 0 || false,
+      value: Number(option),
+    }));
+  }
+
+  // Handler for updating selected items per page
+  _updatePageItemSelectOptions(value) {
+    this.itemPerPageSelectOptions = this.itemPerPageSelectOptions.map(
       (item) => {
-        if (item.value === Number(event.target.value)) {
+        if (item.value === Number(value)) {
           item.selected = true;
         } else {
           item.selected = false;
@@ -88,15 +112,10 @@ export default class PaginationComponent extends Component {
         return item;
       }
     );
+  }
 
-    if (this.currentPageNumber > this.args.totalPages) {
-      this.currentPageNumber = this.totalPages;
-    }
-
-    this.args.onPageItemsCountChange({
-      startIdx: this.startIndex,
-      endOffset: this.endOffset,
-      selectedItemsPerPage: this.selectedItemsPerPage,
-    });
+  // Page offset reset handler
+  _resetOffset() {
+    this.offset = 1;
   }
 }
