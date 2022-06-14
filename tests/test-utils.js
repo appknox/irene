15eb9523a92822b762a6e2c1cdc2a3d1f8426a8e
Praseetha-KR <praseetha04@gmail.com>
2022-06-14 20:46:39 +0530

    Refactor serializer to support list

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,9 +1,24 @@
 import { underscore } from '@ember/string';
 
-export function serializer(payload) {
+function _serialize_object(payload) {
   const serializedPayload = {};
   Object.keys(payload.attrs).map((_key) => {
     serializedPayload[underscore(_key)] = payload[_key];
   });
   return serializedPayload;
+}
+
+export function serializer(data, many = false) {
+  if (many == true) {
+    return {
+      count: data.length,
+      next: null,
+      previous: null,
+      results: data.models.map((d) => {
+        return _serialize_object(d);
+      }),
+    };
+  } else {
+    return _serialize_object(data);
+  }
 }
