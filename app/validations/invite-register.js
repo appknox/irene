2cb59e107ee910d66b65cad223ce10042904d45f
Sharat M R <cosmosgenius@gmail.com>
2022-08-01 17:13:29 +0530

    Change password min length from 6 to 10

--- app/validations/invite-register.js
+++ app/validations/invite-register.js
@@ -1,21 +1,16 @@
-/* eslint-disable prettier/prettier */
 import {
   validatePresence,
   validateLength,
-  validateConfirmation
+  validateConfirmation,
 } from 'ember-changeset-validations/validators';
 import validateEquality from './validators/value-equality';
 
-
 export default {
   username: validatePresence(true),
-  password: [
-    validatePresence(true),
-    validateLength({ min: 6 })
-  ],
+  password: [validatePresence(true), validateLength({ min: 10 })],
   passwordConfirmation: validateConfirmation({ on: 'password' }),
   termsAccepted: validateEquality({
     expected: true,
-    message: 'Please accept terms & conditions to proceed'
-  })
+    message: 'Please accept terms & conditions to proceed',
+  }),
 };
