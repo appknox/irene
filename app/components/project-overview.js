7063919c51c18f330c96ff27310f4c6e6cf70dc0
Yibaebi <elliot.yibaebi@yahoo.com>
2022-08-12 09:50:38 +0100

    complete project overview migration

--- app/components/project-overview.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier */
-import Component from '@ember/component';
-
-const ProjectOverviewComponent = Component.extend({
-  project: null,
-  classNames: ["column" , "is-one-third"]});
-export default ProjectOverviewComponent;
