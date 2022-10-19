104e1aa4f5a252fa96df38a56caada78d0f21ae8
yibaebi <elliot.yibaebi@yahoo.com>
2022-10-19 12:44:04 +0100

    complete security dasboard manual scan updates

--- app/components/analysis-overview/index.js
+++ app/components/analysis-overview/index.js
@@ -69,6 +69,12 @@ export default class AnalysisOverviewComponent extends Component {
     return tags;
   }
 
+  get isManualScanVulnerability() {
+    const analysisVulnerabilitytypes = this.analysis.vulnerability.get('types');
+    const types = ENUMS.VULNERABILITY_TYPE;
+    return analysisVulnerabilitytypes?.includes(types.MANUAL);
+  }
+
   @action confirmCallback() {
     this.markAsPassed.perform();
     this.showMarkPassedConfirmBox = false;
