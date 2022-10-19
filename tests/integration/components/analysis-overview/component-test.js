45358e7accdbbd12d033ad1ce6f8d48d9b67e00a
yibaebi <elliot.yibaebi@yahoo.com>
2022-10-19 12:44:04 +0100

    complete security dasboard manual scan updates

--- tests/integration/components/analysis-overview/component-test.js
+++ tests/integration/components/analysis-overview/component-test.js
@@ -20,19 +20,29 @@ module('Integration | Component | analysis-overview', function (hooks) {
       id: 1,
       types: [ENUMS.VULNERABILITY_TYPE.MANUAL],
     });
+  });
+
+  test('it renders with the right properties when manual scan feature is enabled at the project level', async function (assert) {
+    this.project = this.store.createRecord('project', {
+      id: 1,
+      isManualScanAvailable: true,
+    });
+
+    this.file = this.store.createRecord('file', {
+      id: 1,
+      project: this.project,
+      name: 'Appknox-MFVA',
+    });
+
+    this.vulnerability.name = 'One Time Password';
 
     this.analysis = this.store.createRecord('analysis', {
       id: 1,
+      file: this.file,
       vulnerability: this.vulnerability,
+      risk: ENUMS.RISK.CRITICAL,
+      status: ENUMS.ANALYSIS_STATUS.COMPLETED,
     });
-  });
-
-  test('it renders with the right properties', async function (assert) {
-    this.vulnerability.name = 'One Time Password';
-
-    this.analysis.vulnerability = this.vulnerability;
-    this.analysis.risk = ENUMS.RISK.CRITICAL;
-    this.analysis.status = ENUMS.ANALYSIS_STATUS.COMPLETED;
 
     await render(hbs`<AnalysisOverview  @analysis={{this.analysis }}/>`);
     assert
@@ -52,5 +62,78 @@ module('Integration | Component | analysis-overview', function (hooks) {
       .dom('[data-test-analysis-status]')
       .exists()
       .hasTextContaining(`${analysisStatus([this.analysis.status])}`);
+  });
+
+  test('it renders when manual scan feature is enabled at the project level and vulnerability types include manual scan', async function (assert) {
+    this.project = this.store.createRecord('project', {
+      id: 1,
+      isManualScanAvailable: true,
+    });
+
+    this.file = this.store.createRecord('file', {
+      id: 1,
+      project: this.project,
+      name: 'Appknox-MFVA',
+    });
+
+    this.analysis = this.store.createRecord('analysis', {
+      id: 1,
+      vulnerability: this.vulnerability,
+      file: this.file,
+    });
+
+    await render(hbs`<AnalysisOverview  @analysis={{this.analysis }}/>`);
+    assert.dom('[data-test-analysis-overview-container]').exists();
+  });
+
+  test('it renders when manual scan feature is enabled at the project level and vulnerability types do not include manual scan', async function (assert) {
+    this.vulnerability.types = [
+      ENUMS.VULNERABILITY_TYPE.STATIC,
+      ENUMS.VULNERABILITY_TYPE.DYNAMIC,
+    ];
+
+    this.project = this.store.createRecord('project', {
+      id: 1,
+      isManualScanAvailable: true,
+    });
+
+    this.file = this.store.createRecord('file', {
+      id: 1,
+      project: this.project,
+      name: 'Appknox-MFVA',
+    });
+
+    this.analysis = this.store.createRecord('analysis', {
+      id: 1,
+      vulnerability: this.vulnerability,
+      file: this.file,
+    });
+
+    await render(hbs`<AnalysisOverview  @analysis={{this.analysis }}/>`);
+    assert.dom('[data-test-analysis-overview-container]').exists();
+  });
+
+  test('it does not render when manual scan feature is disabled at the project level and vulnerability types include manual scan', async function (assert) {
+    this.vulnerability.types = [ENUMS.VULNERABILITY_TYPE.MANUAL];
+
+    this.project = this.store.createRecord('project', {
+      id: 1,
+      isManualScanAvailable: false,
+    });
+
+    this.file = this.store.createRecord('file', {
+      id: 1,
+      project: this.project,
+      name: 'Appknox-MFVA',
+    });
+
+    this.analysis = this.store.createRecord('analysis', {
+      id: 1,
+      vulnerability: this.vulnerability,
+      file: this.file,
+    });
+
+    await render(hbs`<AnalysisOverview  @analysis={{this.analysis }}/>`);
+    assert.dom('[data-test-analysis-overview-container]').doesNotExist();
   });
 });
