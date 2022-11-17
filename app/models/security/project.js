0a49a0263bcbdeaab6f92706544103358e064662
yibaebi <elliot.yibaebi@yahoo.com>
2022-11-17 12:55:45 +0100

    complete migration of security project model

--- app/models/security/project.js
+++ app/models/security/project.js
@@ -1,8 +1,8 @@
-/* eslint-disable prettier/prettier, ember/no-classic-classes */
-import Model, { attr, hasMany, belongsTo }  from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
-export default Model.extend({
-  owner: belongsTo('user', {inverse: 'ownedProjects'}),
-  files: hasMany('security/file'),
-  packageName: attr('string')
-});
+export default class SecurityProjectModel extends Model {
+  @belongsTo('user', { inverse: 'ownedProjects' }) owner;
+  @hasMany('security/file') files;
+  @attr('string') packageName;
+  @attr('boolean') isManualScanAvailable;
+}
