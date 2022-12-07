4c78b9c2ca42177e0dbc349b2a8181f6d2768b6b
Sam David <samd948@gmail.com>
2022-12-07 10:27:33 +0530

    refactor ak-checkbox

--- app/components/ak-checkbox/index.js
+++ app/components/ak-checkbox/index.js
@@ -1,33 +1,29 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class AkCheckboxComponent extends Component {
-  @tracked isChecked = false;
-
   get id() {
     return this.args.id || `ak-checkbox-${guidFor(this)}`;
   }
 
-  get checked() {
-    if (this.args.indeterminate) {
+  @action
+  onClick(event) {
+    if (this.args.readonly) {
+      event.preventDefault();
+
       return false;
     }
 
-    return this.args.checked || this.isChecked;
+    if (this.args.onClick) {
+      this.args.onClick(event);
+    }
   }
 
   @action
   onChange(event) {
-    if (this.args.checked === null || this.args.checked === undefined) {
-      this.isChecked = event.target.checked;
-    }
-
     if (this.args.onChange) {
       this.args.onChange(event, event.target.checked);
     }
   }
-
-  noop() {}
 }
--- app/components/ak-checkbox/index.stories.js
+++ app/components/ak-checkbox/index.stories.js
@@ -1,4 +1,5 @@
 import { hbs } from 'ember-cli-htmlbars';
+import { computed } from '@ember/object';
 
 export default {
   title: 'AkCheckbox',
@@ -6,19 +7,100 @@ export default {
   excludeStories: [],
 };
 
+const actions = {
+  handleClick(event) {
+    console.log('click event!', event);
+  },
+  handleChange(event, checked) {
+    console.log('change event!', { event, checked });
+  },
+};
+
 const Template = (args) => ({
   template: hbs`
-    <AkFormControlLabel @label={{this.label}} @disabled={{this.disabled}} as |fcl|>
-        <AkCheckbox @disabled={{fcl.disabled}} @readonly={{this.readonly}} />
-    </AkFormControlLabel>
+    <AkCheckbox 
+      @checked={{this.checked}} 
+      @indeterminate={{this.indeterminate}} 
+      @disabled={{this.disabled}} 
+      @readonly={{this.readonly}}
+      @onClick={{this.handleClick}}
+      @onChange={{this.handleChange}} />
   `,
-  context: args,
+  context: { ...args, ...actions },
 });
 
 export const Basic = Template.bind({});
 
 Basic.args = {
+  disabled: false,
+  readonly: false,
+  checked: true,
+  indeterminate: false,
+};
+
+const LabelTemplate = (args) => ({
+  template: hbs`
+    <AkFormControlLabel @label={{this.label}} @disabled={{this.disabled}} as |fcl|>
+        <AkCheckbox 
+          @checked={{this.checked}} 
+          @indeterminate={{this.indeterminate}} 
+          @disabled={{fcl.disabled}} 
+          @readonly={{this.readonly}}
+          @onClick={{this.handleClick}}
+          @onChange={{this.handleChange}} />
+    </AkFormControlLabel>
+  `,
+  context: { ...args, ...actions },
+});
+
+export const Label = LabelTemplate.bind({});
+
+Label.args = {
   label: 'Check me',
   disabled: false,
   readonly: false,
+  checked: true,
+  indeterminate: false,
 };
+
+const IndeterminateTemplate = (args) => ({
+  template: hbs`
+    {{!-- template-lint-disable no-action --}}
+    <AkFormControlLabel @label="Select all">
+      <AkCheckbox @onChange={{action this.handleChange}} @checked={{this.selectAllChecked}} @indeterminate={{this.selectAllIndeterminate}} />
+    </AkFormControlLabel>
+
+    <AkFormControlLabel @label="Check 1">
+      <AkCheckbox @checked={{this.checked1}} />
+    </AkFormControlLabel>
+
+    <AkFormControlLabel @label="Check 2">
+      <AkCheckbox @checked={{this.checked2}} />
+    </AkFormControlLabel>
+  `,
+  context: {
+    ...args,
+    checked1: true,
+    checked2: false,
+    selectAllChecked: computed('checked1', 'checked2', {
+      get() {
+        return this.checked1 && this.checked2;
+      },
+      set(key, value) {
+        return value;
+      },
+    }),
+    selectAllIndeterminate: computed('checked1', 'checked2', function () {
+      return this.checked1 !== this.checked2;
+    }),
+    handleChange(event, checked) {
+      console.log('select all change:', checked);
+      this.set('checked1', checked);
+      this.set('checked2', checked);
+    },
+  },
+});
+
+export const Indeterminate = IndeterminateTemplate.bind({});
+
+Indeterminate.args = {};
