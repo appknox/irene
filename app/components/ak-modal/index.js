2d2369bc13aa8351e5b338d2f36e5b55909d3a95
Sam David <samd948@gmail.com>
2022-12-19 10:21:50 +0530

    ak-modal use appbar and added blur overlay

--- app/components/ak-modal/index.js
+++ app/components/ak-modal/index.js
@@ -1,5 +1,13 @@
 import Component from '@glimmer/component';
+import styles from './index.scss';
 
 export default class AkModalComponent extends Component {
   noop() {}
+
+  get classes() {
+    return {
+      overlay: styles['ak-modal-overlay'],
+      overlayBlur: styles['ak-modal-overlay-blur'],
+    };
+  }
 }
