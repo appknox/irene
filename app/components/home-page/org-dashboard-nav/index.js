3161b0787af48f763c64c6933b84e590879f2af3
yibaebi <elliot.yibaebi@yahoo.com>
2023-01-09 08:03:48 +0100

    deprecate document360 knowlwdge-base integration

--- app/components/home-page/org-dashboard-nav/index.js
+++ app/components/home-page/org-dashboard-nav/index.js
@@ -3,17 +3,11 @@ import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import * as chat from 'irene/utils/chat';
-import { openKnowledgeBasePanel } from 'irene/utils/knowledge-base';
-
 export default class HomePageOrgDashboardNavComponent extends Component {
   @service integration;
 
   @tracked isUploading = false;
   @tracked progress = 0;
-
-  get showKnowledgeBase() {
-    return this.integration.isDocument360Enabled();
-  }
 
   @action setAppUploadStatus(status) {
     this.isUploading = status;
@@ -25,9 +19,5 @@ export default class HomePageOrgDashboardNavComponent extends Component {
 
   @action openChatBox() {
     chat.openChatBox();
-  }
-
-  @action onOpenKnowledgeBase() {
-    openKnowledgeBasePanel();
   }
 }
