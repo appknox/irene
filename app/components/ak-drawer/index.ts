c0631089daaded49482d04ee24eec0606e707fde
Sam David <samd948@gmail.com>
2023-01-27 15:30:31 +0530

    ak-drawer wormhole & ts changes

--- app/components/ak-drawer/index.ts
+++ app/components/ak-drawer/index.ts
@@ -2,16 +2,30 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { later } from '@ember/runloop';
 
-export default class DrawerComponent extends Component {
-  backdropElement = null;
-  drawerElement = null;
+export interface DrawerSignature {
+  Args: {
+    open?: boolean;
+    anchor?: 'left' | 'right';
+    disableBackdropClick?: boolean;
+    onClose: () => void;
+  };
+  Blocks: { default: [{ closeHandler: () => void }] };
+}
 
-  constructor() {
-    super(...arguments);
+export default class DrawerComponent extends Component<DrawerSignature> {
+  backdropElement: HTMLElement | null = null;
+  drawerElement: HTMLElement | null = null;
+
+  constructor(owner: unknown, args: DrawerSignature['Args']) {
+    super(owner, args);
 
     if (!this.args.onClose) {
       throw new Error('No onClose handler is set');
     }
+  }
+
+  get anchor() {
+    return this.args.anchor || 'left';
   }
 
   @action
@@ -28,7 +42,7 @@ export default class DrawerComponent extends Component {
     }, 300);
 
     if (this.backdropElement) {
-      this.backdropElement.style.opacity = 0;
+      this.backdropElement.style.opacity = '0';
     }
 
     if (this.drawerElement) {
@@ -39,22 +53,28 @@ export default class DrawerComponent extends Component {
   }
 
   @action
-  backdropInserted(element) {
+  backdropInserted(element: HTMLElement) {
     later(() => {
       this.backdropElement = element;
 
-      element.style.opacity = 1;
+      element.style.opacity = '1';
       element.style.transition = 'opacity 225ms ease-in-out 0ms';
-    });
+    }, 0);
   }
 
   @action
-  drawerInserted(element) {
+  drawerInserted(element: HTMLElement) {
     later(() => {
       this.drawerElement = element;
 
       element.style.transform = 'none';
       element.style.transition = 'transform 225ms ease-out 0ms';
-    });
+    }, 0);
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    AkDrawer: typeof DrawerComponent;
   }
 }
