17e22bd9682379c3f5777560e28b1456d1112406
Sharat M R <cosmosgenius@gmail.com>
2023-02-08 15:49:08 +0530

    Add notification-page and tests

---
+++ app/components/notifications-page/index.ts
@@ -0,0 +1,61 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import RouterService from '@ember/routing/router-service';
+import AkNotificationsService from 'irene/services/ak-notifications';
+
+interface LimitOffset {
+  limit: number;
+  offset: number;
+}
+
+export default class NotificationsPageComponent extends Component {
+  @service declare akNotifications: AkNotificationsService;
+  @service declare router: RouterService;
+
+  notificationPageRoute = 'authenticated.dashboard.notifications';
+
+  get limit() {
+    return this.akNotifications.notification_limit;
+  }
+
+  get offset() {
+    return this.akNotifications.notification_offset;
+  }
+
+  get tableData() {
+    return this.akNotifications.notifications;
+  }
+
+  get itemPerPageOptions() {
+    return [10, 25, 50];
+  }
+
+  get totalCount() {
+    return this.akNotifications.notificationsCount;
+  }
+
+  get isLoading() {
+    return this.akNotifications.fetch.isRunning;
+  }
+
+  get isEmpty() {
+    return this.akNotifications.notificationsCount == 0;
+  }
+
+  @action onItemPerPageChange(args: LimitOffset) {
+    const { limit } = args;
+    const offset = 0;
+
+    this.router.transitionTo(this.notificationPageRoute, {
+      queryParams: { app_limit: limit, app_offset: offset },
+    });
+  }
+
+  @action goToPage(args: LimitOffset) {
+    const { limit, offset } = args;
+    this.router.transitionTo(this.notificationPageRoute, {
+      queryParams: { notification_limit: limit, notification_offset: offset },
+    });
+  }
+}
