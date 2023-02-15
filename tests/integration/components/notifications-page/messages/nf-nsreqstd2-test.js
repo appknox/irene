dbdbb7fde85558c81083e3d7ced83f6d86b49d91
Sharat M R <cosmosgenius@gmail.com>
2023-02-16 02:38:53 +0530

    --wip-- [skip ci]

---
+++ tests/integration/components/notifications-page/messages/nf-nsreqstd2-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module(
+  'Integration | Component | notifications-page/messages/nf-nsreqstd2',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test('it renders', async function (assert) {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.set('myAction', function(val) { ... });
+
+      await render(hbs`<NotificationsPage::Messages::NfNsreqstd2 />`);
+
+      assert.equal(this.element.textContent.trim(), '');
+
+      // Template block usage:
+      await render(hbs`
+      <NotificationsPage::Messages::NfNsreqstd2>
+        template block text
+      </NotificationsPage::Messages::NfNsreqstd2>
+    `);
+
+      assert.equal(this.element.textContent.trim(), 'template block text');
+    });
+  }
+);
