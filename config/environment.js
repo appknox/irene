b7c1baf8bcb511ee2ade3105be0d70be2f6e9024
Sam David <samd948@gmail.com>
2023-07-05 16:58:40 +0530

    product version update

--- config/environment.js
+++ config/environment.js
@@ -146,7 +146,7 @@ module.exports = function (environment) {
   var showLicense = handler.getBoolean('IRENE_SHOW_LICENSE');
   var ENV = {
     ENVHandlerCONST: ENVHandlerCONST,
-    productVersion: '23.6',
+    productVersion: '23.7',
     version: Date.now(),
     isDevknox: false,
     isAppknox: false,
