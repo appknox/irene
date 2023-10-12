c58c0b4f98abf63878e3af914af775a92ecb1f16
Sam David <samd948@gmail.com>
2023-10-12 13:41:27 +0530

    serializer cleanup & made drf as default serializer

--- app/serializers/am-app-sync.js
+++
@@ -1,3 +0,0 @@
-import DRFSerializer from './drf';
-
-export default class AmAppSyncSerializer extends DRFSerializer {}
--- app/serializers/organization-archive.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import DRFSerializer from './drf';
-
-export default DRFSerializer.extend({
-});
