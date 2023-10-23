e8bc97f5f198798d1755c97dd85818f3886b938b
yibaebi <elliot.yibaebi@yahoo.com>
2023-10-23 13:47:22 +0100

    remove ember-attacher component in file-overview

--- app/components/ak-tag/component.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, ember/avoid-leaking-state-in-ember-objects, prettier/prettier */
-import Component from '@ember/component';
-
-export default Component.extend({
-  tagName: 'span',
-  localClassNames: ['filetag']
-});
