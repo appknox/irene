bef020f60fe6998823633de458c073171e78c75b
yibaebi <elliot.yibaebi@yahoo.com>
2023-11-22 12:26:15 +0100

    add dast automation scenario models

---
+++ app/models/scan-parameter-group.ts
@@ -0,0 +1,46 @@
+import Model, {
+  AsyncBelongsTo,
+  AsyncHasMany,
+  attr,
+  belongsTo,
+  hasMany,
+} from '@ember-data/model';
+
+import ProjectModel from './project';
+import UserModel from './user';
+import ScanParameterModel from './scan-parameter';
+
+export default class ScanParameterGroupModel extends Model {
+  @attr('string')
+  declare name: string;
+
+  @attr('string')
+  declare description: string;
+
+  @attr('boolean')
+  declare isActive: boolean;
+
+  @attr('boolean')
+  declare isDefault: boolean;
+
+  @attr('date')
+  declare createdOn: Date;
+
+  @attr('date')
+  declare updatedOn: Date;
+
+  @belongsTo('project')
+  declare project: AsyncBelongsTo<ProjectModel>;
+
+  @belongsTo('user')
+  declare lastUpdatedBy: AsyncBelongsTo<UserModel> | null;
+
+  @hasMany('scan-parameter', { inverse: 'scanParameterGroup' })
+  declare scanParameters: AsyncHasMany<ScanParameterModel>;
+}
+
+declare module 'ember-data/types/registries/model' {
+  export default interface ModelRegistry {
+    'scan-parameter-group': ScanParameterGroupModel;
+  }
+}
