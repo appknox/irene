1328b334ef5619997ca3fb2fbc3250f81bc49afe
yibaebi <elliot.yibaebi@yahoo.com>
2024-02-27 11:39:32 +0100

    update e2e tests, test action and mirage setup

--- cypress/support/Actions/common/NetworkActions.ts
+++ cypress/support/Actions/common/NetworkActions.ts
@@ -1,8 +1,19 @@
-import { Method, RouteMatcher } from 'cypress/types/net-stubbing';
+import {
+  CyHttpMessages,
+  Method,
+  RouteMatcher,
+} from 'cypress/types/net-stubbing';
 
 import { mirageServer } from '../../Mirage';
 import { API_ROUTES } from '../../api.routes';
 
+import {
+  extractDynamicSlugs,
+  removeHostFromUrl,
+  replaceSlugsWithAsterisks,
+} from '../../utils';
+
+// Override props for paginated routes that are mocked
 export interface PaginatedReqDataOverrideProps {
   count?: number;
   next?: string | null;
@@ -11,16 +22,55 @@ export interface PaginatedReqDataOverrideProps {
 }
 
 export default class NetworkActions {
-  hideNetworkLogsFor(opts: { route: RouteMatcher; method?: Method }) {
+  /**
+   * Hide network logs for a specific route.
+   *
+   * @param {Object} opts - Options for hiding network logs.
+   * @param {RouteMatcher} opts.route - The route to hide logs for.
+   * @param {Method} [opts.method='GET'] - The HTTP method for the interception (default is 'GET').
+   *
+   * @returns {Cypress.Chainable} - The Cypress chainable object for chaining further commands.
+   *
+   * @example
+   * // Hide network logs for GET requests to "/api/data"
+   * hideNetworkLogsFor({
+   *   route: "/api/data",
+   *   method: 'GET'
+   * });
+   */
+  hideNetworkLogsFor(opts: {
+    route: RouteMatcher;
+    method?: Method;
+  }): Cypress.Chainable {
     return cy.intercept(opts.method || 'GET', opts.route, { log: false });
   }
 
+  /**
+   * Mock a network request.
+   *
+   * @param {Object} opts - Options for mocking the network request.
+   * @param {Method} [opts.method='GET'] - The HTTP method for the interception (default is 'GET').
+   * @param {RouteMatcher} opts.route - The route to mock the request for.
+   * @param {string} opts.alias - The alias to give to the intercepted route for later reference.
+   * @param {object} [opts.dataOverride] - Optional data to override the response payload.
+   *
+   * @returns {Cypress.Chainable} - The Cypress chainable object for chaining further commands.
+   *
+   * @example
+   * // Mock a GET request to "/api/data" with a specific alias and response data override
+   * mockNetworkReq({
+   *   method: 'GET',
+   *   route: "/api/data",
+   *   alias: "getData",
+   *   dataOverride: { status: "success", data: { id: 123, name: "Example" } }
+   * });
+   */
   mockNetworkReq(opts: {
     method?: Method;
     route: RouteMatcher;
     alias: string;
     dataOverride?: object;
-  }) {
+  }): Cypress.Chainable {
     const { method, route, dataOverride, alias } = opts;
 
     return cy
@@ -30,12 +80,38 @@ export default class NetworkActions {
       .as(alias);
   }
 
+  /**
+   * Mock a paginated network request.
+   *
+   * @param {Object} opts - Options for mocking the paginated network request.
+   * @param {Method} [opts.method='GET'] - The HTTP method for the interception (default is 'GET').
+   * @param {RouteMatcher} opts.route - The route to mock the paginated request for.
+   * @param {string} opts.alias - The alias to give to the intercepted route for later reference.
+   * @param {PaginatedReqDataOverrideProps} [opts.resDataOverride] - Optional data to override the response payload.
+   *
+   * @returns {Cypress.Chainable} - The Cypress chainable object for chaining further commands.
+   *
+   * @example
+   * // Mock a paginated GET request to "/api/data" with a specific alias and response data override
+   * mockPaginatedNetworkReq({
+   *   method: 'GET',
+   *   route: "/api/data",
+   *   alias: "getPaginatedData",
+   *   resDataOverride: {
+   *     results: [
+   *       { id: 1, name: "Example 1" },
+   *       { id: 2, name: "Example 2" }
+   *     ],
+   *     // other pagination properties
+   *   }
+   * });
+   */
   mockPaginatedNetworkReq(opts: {
     method?: Method;
     route: RouteMatcher;
     alias: string;
     resDataOverride?: PaginatedReqDataOverrideProps;
-  }) {
+  }): Cypress.Chainable {
     const { method, route, resDataOverride, alias } = opts;
 
     return cy
@@ -50,45 +126,41 @@ export default class NetworkActions {
       .as(alias);
   }
 
-  loadAppConfigWithCustomData(dataOverride?: {
-    serverConfig?: object;
-    frontendConfig?: object;
-  }) {
-    const { serverConfig, frontendConfig } = dataOverride || {};
+  /**
+   * Load application configuration with fixture data.
+   *
+   * @returns {Object} - An object containing aliases for the intercepted routes.
+   * @property {string} frontendConfigAlias - Alias for the frontend configuration route.
+   * @property {string} serverConfigAlias - Alias for the server configuration route.
+   *
+   * @example
+   * // Load application configuration with fixture data and get aliases for further use
+   * const aliases = loadAppConfigWithFixtureData();
+   * // Now you can use aliases.frontendConfigAlias and aliases.serverConfigAlias in your tests
+   */
+  loadAppConfigWithFixtureData(): object {
+    const frontendConfigAlias = API_ROUTES.frontendConfig.alias;
+    const serverConfigAlias = API_ROUTES.serverConfig.alias;
 
-    if (serverConfig) {
-      this.mockNetworkReq({
-        ...API_ROUTES.serverConfig,
-        dataOverride: serverConfig,
-      });
-    }
-
-    if (frontendConfig) {
-      this.mockNetworkReq({
-        ...API_ROUTES.frontendConfig,
-        dataOverride: frontendConfig,
-      });
-    }
-  }
-
-  loadAppConfigWithFixtureData() {
     cy.fixture('common/server_config.json').then((data) => {
       this.mockNetworkReq({
-        ...API_ROUTES.serverConfig,
+        route: API_ROUTES.serverConfig.route,
+        alias: serverConfigAlias,
         dataOverride: data,
       });
     });
 
     cy.fixture('common/frontend_config.json').then((data) => {
       this.mockNetworkReq({
-        ...API_ROUTES.frontendConfig,
+        route: API_ROUTES.frontendConfig.route,
+        alias: frontendConfigAlias,
         dataOverride: data,
       });
     });
 
     return {
-      frontendConfigAlias: API_ROUTES.frontendConfig.alias,
-      serverConfigAlias: API_ROUTES.serverConfig.alias,
+      frontendConfigAlias,
+      serverConfigAlias,
     };
   }
 
@@ -108,6 +180,53 @@ export default class NetworkActions {
         count: projects?.length || 0,
         results: projects,
       },
+    });
+  }
+
+  /**
+   * Intercept a parameterized route
+   *
+   * @template ParamObj - The type of the parameters available in URL.
+   *
+   * @param {Object} opts - Options for intercepting the route.
+   * @param {string} opts.route - The parameterized route to intercept.
+   * @param {Method} [opts.method='GET'] - The HTTP method for the interception (default is 'GET').
+   * @param {(req: CyHttpMessages.IncomingHttpRequest, params: ParamObj) => Promise<void>} [opts.routeHandler] -
+   *   The route handler function that will be called when the route is intercepted.
+   *   It receives the request and the extracted dynamic parameters.
+   *
+   * @returns {Cypress.Chainable} - The Cypress chainable object for chaining further commands.
+   *
+   * @example
+   * // Intercept a POST request to "/path/:id/:name"
+   * interceptParameterizedRoute({
+   *   route: "/path/:id/:name",
+   *   routeHandler: async (req, params) => {
+   *     console.log("Dynamic Parameters:", params);
+   *     // Perform actions based on dynamic parameters
+   *   },
+   *   method: "POST"
+   * });
+   */
+  interceptParameterizedRoute<ParamObj>(opts: {
+    route: string;
+    method?: Method;
+
+    routeHandler?: (
+      req: CyHttpMessages.IncomingHttpRequest,
+      params: ParamObj
+    ) => Promise<void>;
+  }): Cypress.Chainable {
+    const { method, route, routeHandler } = opts;
+
+    // The Asterisk is necessary to match query params if available
+    const routeToIntercept = `${replaceSlugsWithAsterisks(route)}*`;
+
+    return cy.intercept(method || 'GET', routeToIntercept, async (req) => {
+      const path = removeHostFromUrl(req.url);
+      const params = extractDynamicSlugs<ParamObj>(route, path);
+
+      await routeHandler?.(req, params);
     });
   }
 }
--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -3,3 +3,75 @@
  * @returns String with correct alias structure - "@{aliasName}"
  */
 export const getAliasName = (alias: string): `@${string}` => `@${alias}`;
+
+/**
+ * Extracts dynamic parameters from an actual route based on a route template.
+ *
+ * @param {string} routeTemplate - The route template with placeholders for dynamic parameters.
+ * @param {string} actualRoute - The actual route containing dynamic parameter values.
+ * @returns {Record<string, string> } An object representing dynamic parameters with their keys and values.
+ *
+ * @example
+ * const routeTemplate = "/path/:id/:slug";
+ * const actualRoute = "/path/123/example-slug";
+ * const dynamicParams = extractDynamicParams(routeTemplate, actualRoute);
+ */
+
+export function extractDynamicSlugs<ParamObj>(
+  routeTemplate: string,
+  actualRoute: string
+): ParamObj {
+  const templateRegex = new RegExp(
+    routeTemplate.replace(
+      /:(\w+)/g,
+      (_, paramName) => `(?<${paramName}>[^\\/]+)`
+    )
+  );
+
+  const match = actualRoute.match(templateRegex);
+  const params = match?.groups || {};
+
+  return Object.keys(params).reduce(
+    (prev, curr) => ({
+      ...prev,
+      [curr]: params[curr],
+    }),
+    {} as ParamObj
+  );
+}
+
+/**
+ * Replaces all slugs in a route with an asterisk.
+ *
+ * @param {string} route - The route containing slugs.
+ * @returns {string} The route with slugs replaced by asterisks.
+ *
+ */
+
+export function replaceSlugsWithAsterisks(route: string): string {
+  const slugRegex = /:[^\\/]+/g;
+  const routeWithAsterisks = route.replace(slugRegex, '*');
+
+  return routeWithAsterisks;
+}
+
+/**
+ * Removes the host from a given URL.
+ *
+ * @param {string} urlString - The URL to process.
+ * @returns {string} The URL without the host.
+ *
+ * @example
+ * const originalUrl = "https://example.com/path/to/resource";
+ * const urlWithoutHost = removeHostFromUrl(originalUrl);
+ *
+ * console.log("URL without Host:", urlWithoutHost);
+ * // Output: "/path/to/resource"
+ */
+
+export function removeHostFromUrl(urlString: string): string {
+  const url = new URL(urlString);
+  const urlWithoutHost = url.pathname + url.search + url.hash;
+
+  return urlWithoutHost;
+}
