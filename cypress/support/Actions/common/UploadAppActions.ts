ed377438e841486156d87696d938e2118de8647f
Avi Shah <avishah1904@gmail.com>
2024-03-01 13:17:29 +0530

    upload-app-via-link cypress test

--- cypress/support/Actions/common/UploadAppActions.ts
+++ cypress/support/Actions/common/UploadAppActions.ts
@@ -1,4 +1,6 @@
+import { API_ROUTES } from '../../../support/api.routes';
 import APP_TRANSLATIONS from '../../translations';
+import { MirageFactoryDefProps } from '../../../support/Mirage';
 
 export default class UploadAppActions {
   uploadAppStatusLoader = 'uploadAppStatus-loader' as const;
@@ -43,10 +45,249 @@ export default class UploadAppActions {
 
     return cy
       .findByRole('button', {
-        name: new RegExp(APP_TRANSLATIONS.uploadApp, 'i'),
+        name: APP_TRANSLATIONS.uploadApp,
       })
       .then((el) => {
         cy.wrap(el.find('input')).should('exist').selectFile(fileFixtureAlias);
       });
   }
+
+  selectAppViaLink(playStoreUrl: string) {
+    this.closeUploadAppModalIfOpen();
+
+    cy.findByLabelText('upload app via link button')
+      .should('exist')
+      .should('not.have.attr', 'disabled');
+
+    cy.findByLabelText('upload app via link button').click();
+
+    cy.findByText(
+      APP_TRANSLATIONS.uploadAppModule.linkUploadPopupHeader
+    ).should('exist');
+
+    cy.findByPlaceholderText(
+      APP_TRANSLATIONS.uploadAppModule.linkPastePlaceholder
+    ).should('not.have.attr', 'disabled');
+
+    cy.findByPlaceholderText(
+      APP_TRANSLATIONS.uploadAppModule.linkPastePlaceholder
+    )
+      .type(playStoreUrl, { delay: 0 })
+      .then(($input) => {
+        expect($input.val()).to.equal(playStoreUrl);
+      });
+
+    cy.findByLabelText('upload app via link modal confirm button').should(
+      'not.have.attr',
+      'disabled'
+    );
+
+    cy.findByLabelText('upload app via link modal confirm button').click();
+  }
+
+  waitForSBOMProjectList(
+    sbomProjectListAlias: string,
+    uploadedAppPrjIDAlias: string,
+    uploadedFileDetailsAlias: string,
+    uploadedAppPackageNameAlias: string
+  ) {
+    cy.wait(sbomProjectListAlias, { timeout: 6000 }).then(
+      ({ response: res }) => {
+        const sbomPrjListRes = res?.body as {
+          results: Array<MirageFactoryDefProps['sbom-project']>;
+        };
+
+        cy.get<number>(uploadedAppPrjIDAlias).then((prjID) => {
+          const sbomProject = sbomPrjListRes.results.find(
+            (it) => it.project === prjID
+          );
+
+          const sbomPrjFileID = sbomProject?.latest_sb_file;
+
+          // To be used for later assertions
+          cy.wrap(sbomProject).as('uploadedAppSBPrj');
+
+          if (sbomPrjFileID) {
+            const sbFileURL = `${API_ROUTES.sbom.route}/${sbomPrjFileID}`;
+
+            cy.intercept(sbFileURL).as('uploadedAppSBFileReq');
+          }
+        });
+      }
+    );
+
+    // Wait for SB File response
+    cy.wait('@uploadedAppSBFileReq', { timeout: 6000 }).then(
+      ({ response: res }) => {
+        const sbFile = res?.body as MirageFactoryDefProps['sbom-file'];
+
+        const SB_FILE_STATUS = {
+          1: 'PENDING',
+          2: 'IN_PROGRESS',
+          3: 'COMPLETED',
+          4: 'FAILED',
+        };
+
+        const sbFileStatus =
+          SB_FILE_STATUS[sbFile.status as keyof typeof SB_FILE_STATUS];
+
+        // Sanity Check for related sbom project row
+        cy.get<MirageFactoryDefProps['sbom-project']>('@uploadedAppSBPrj').then(
+          (prj) => {
+            cy.findByTestId(`sbomApp-row-${prj?.id}`)
+              .should('exist')
+              .within(() => {
+                // Check if correct file details are visible
+                cy.findByText(sbFileStatus);
+                cy.get(uploadedFileDetailsAlias).its('name').should('exist');
+                cy.get(uploadedAppPackageNameAlias).should('exist');
+              });
+          }
+        );
+      }
+    );
+  }
+
+  checkStaticScanInfo(file: MirageFactoryDefProps['file']) {
+    cy.findByTestId('staticScan-infoContainer', { timeout: 30000 }).then(
+      (el) => {
+        const assertOpts = { container: el, timeout: 30000 };
+
+        cy.findByText(APP_TRANSLATIONS.staticScan, assertOpts).should('exist');
+
+        // Check if static scan has completed or is in progress
+        if (file.is_static_done) {
+          cy.findByText(APP_TRANSLATIONS.completed, assertOpts).should('exist');
+
+          // Check for severity count if static scan is completed
+          cy.findByTestId('fileChart-severityLevelCounts').within(() => {
+            // If static scan is complete counts are stable
+            cy.findAllByText(file.risk_count_high).should('exist');
+            cy.findAllByText(file.risk_count_medium).should('exist');
+            cy.findAllByText(file.risk_count_low).should('exist');
+            cy.findAllByText(file.risk_count_critical).should('exist');
+            cy.findAllByText(file.risk_count_passed).should('exist');
+          });
+        } else {
+          const staticScanProgress = file.static_scan_progress;
+
+          cy.wrap(staticScanProgress).should('be.lessThan', 100);
+
+          // If closer to 100 UI will be updated before assertion is done
+          if (staticScanProgress < 60) {
+            cy.findByText(
+              new RegExp(APP_TRANSLATIONS.scanning, 'i'),
+              assertOpts
+            ).should('exist');
+
+            // Check for completed/incomplete static scan tag in VA Details list
+            cy.findAllByLabelText(
+              `${APP_TRANSLATIONS.static} scan tag tooltip`,
+              { timeout: 30000 }
+            )
+              .first()
+              .should('exist')
+              .then((scanTag) => {
+                if (scanTag) {
+                  const tagMsg = file.is_static_done
+                    ? APP_TRANSLATIONS.scanCompleted
+                    : APP_TRANSLATIONS.scanNotCompleted;
+
+                  cy.wrap(scanTag).trigger('mouseenter');
+
+                  cy.findByText(`${APP_TRANSLATIONS.static} ${tagMsg}`, {
+                    timeout: 30000,
+                  });
+                }
+              });
+          }
+        }
+      }
+    );
+  }
+
+  initiateViaSystemUpload = (fixture: string) => {
+    // Initiate apk upload from fixture
+    cy.fixture(`apps/${fixture}`, null).as('appBinary');
+
+    this.selectAppViaSystem('@appBinary').then(() => this.openUploadAppModal());
+
+    cy.findByTestId('submission-status-dropdown-container').within(() => {
+      cy.contains(APP_TRANSLATIONS.uploadStatus);
+      cy.contains(APP_TRANSLATIONS.viaSystem);
+      cy.contains(new RegExp(APP_TRANSLATIONS.uploading, 'i'));
+      cy.contains(new RegExp(APP_TRANSLATIONS.inProgress, 'i'));
+    });
+
+    // Initiate app upload
+    cy.wait('@uploadAppReq').then(({ response: res }) => {
+      const uploadDetails = res?.body as MirageFactoryDefProps['upload-app'];
+
+      // Necessary to know when upload is completed
+      cy.intercept('PUT', uploadDetails.url).as('uploadSuccess');
+    });
+
+    // Wait for upload completion
+    cy.wait('@uploadSuccess', { timeout: 60000 });
+
+    cy.findByText(APP_TRANSLATIONS.fileUploadedSuccessfully, {
+      timeout: 60000,
+    });
+
+    // Intercepts uploaded file submission request from uploaded file info
+    cy.wait('@uploadAppReqPOST', { timeout: 60000 }).then(
+      ({ response: res }) => {
+        const uploadDetails = res?.body as MirageFactoryDefProps['upload-app'];
+
+        const uploadedAppSubURL = `${API_ROUTES.submissionItem.route}/${uploadDetails.submission_id}`;
+
+        // Intercept upload app submission request
+        cy.intercept('GET', uploadedAppSubURL).as('uploadedAppSubmission');
+        cy.wrap(uploadDetails.submission_id).as('uploadedAppSubID');
+      }
+    );
+  };
+
+  initiateViaLinkUpload = (url: string) => {
+    this.selectAppViaLink(url);
+
+    cy.findByTestId('submission-status-dropdown-container', {
+      timeout: 60000,
+    }).within(() => {
+      cy.contains(APP_TRANSLATIONS.uploadStatus);
+      cy.contains(APP_TRANSLATIONS.viaLink);
+    });
+
+    // Initiate link upload
+    cy.wait('@uploadAppLinkReq').then(({ response: res }) => {
+      expect(res?.statusCode).to.equal(201);
+
+      const uploadDetails =
+        res?.body as MirageFactoryDefProps['upload-app-link'];
+
+      this.openUploadAppModal();
+
+      cy.findByText(APP_TRANSLATIONS.viaLink).should('exist');
+
+      cy.findByText(APP_TRANSLATIONS.uploadStatus).should('exist');
+
+      cy.findByTestId(`upload-app-status-details-${uploadDetails.id}`, {
+        timeout: 30000,
+      });
+    });
+
+    // Intercepts uploaded file submission request from uploaded file info
+    cy.wait('@uploadAppLinkReqPOST', { timeout: 60000 }).then(
+      ({ response: res }) => {
+        const uploadDetails =
+          res?.body as MirageFactoryDefProps['upload-app-link'];
+
+        const uploadedAppSubURL = `${API_ROUTES.submissionItem.route}/${uploadDetails.id}`;
+
+        // Intercept upload app submission request
+        cy.intercept('GET', uploadedAppSubURL).as('uploadedAppSubmission');
+        cy.wrap(uploadDetails.id).as('uploadedAppSubID');
+      }
+    );
+  };
 }
