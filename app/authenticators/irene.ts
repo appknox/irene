c287552309e7fe8c1bba2e5cbb2adef8b69eaf4a
yibaebi <elliot.yibaebi@yahoo.com>
2024-03-05 12:33:32 +0100

    complete login redirect fix

---
+++ app/authenticators/irene.ts
@@ -0,0 +1,22 @@
+import ENV from 'irene/config/environment';
+import BaseAuthenticator, { LoginSuccessDataProps, processData } from './base';
+
+class IreneAuthenticator extends BaseAuthenticator {
+  async authenticate(identification: string, password: string, otp: string) {
+    const data = {
+      username: identification,
+      password,
+      otp,
+    };
+
+    const url = ENV['ember-simple-auth']['loginEndPoint'];
+
+    return this.ajax.post(url, { data }).then((data: LoginSuccessDataProps) => {
+      this.restoreLastTransition(data.user_id);
+
+      return processData(data);
+    });
+  }
+}
+
+export default IreneAuthenticator;
