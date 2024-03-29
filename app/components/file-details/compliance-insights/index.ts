382c915b2683e24dacc94504889a69098e150ea2
Avi Shah <avishah1904@gmail.com>
2024-03-29 13:35:23 +0530

    frontend changes for owasp mobile top 10 2024

--- app/components/file-details/compliance-insights/index.ts
+++ app/components/file-details/compliance-insights/index.ts
@@ -2,6 +2,12 @@ import Component from '@glimmer/component';
 import FileModel from 'irene/models/file';
 import ENUMS from 'irene/enums';
 import { ECOption } from 'irene/components/ak-chart';
+import { inject as service } from '@ember/service';
+import Store from '@ember-data/store';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import OwaspMobile2024Model from 'irene/models/owaspmobile2024';
+import OwaspModel from 'irene/models/owasp';
 
 export interface FileDetailsComplianceInsightsSignature {
   Args: {
@@ -12,6 +18,9 @@ export interface FileDetailsComplianceInsightsSignature {
 type OwaspChartDataOptions = { mobileOptions: ECOption; webOptions: ECOption };
 
 export default class FileDetailsComplianceInsightsComponent extends Component<FileDetailsComplianceInsightsSignature> {
+  @service declare store: Store;
+  @tracked owaspTitles: { [key: string]: string } = {};
+
   get analyses() {
     return this.args.file.analyses;
   }
@@ -37,6 +46,27 @@ export default class FileDetailsComplianceInsightsComponent extends Component<Fi
     return owasps;
   }
 
+  get owaspmobile2024s() {
+    const owaspmobile2024s: string[] = [];
+
+    const risks = [
+      ENUMS.RISK.CRITICAL,
+      ENUMS.RISK.HIGH,
+      ENUMS.RISK.MEDIUM,
+      ENUMS.RISK.LOW,
+    ];
+
+    this.analyses.forEach((analysis) => {
+      analysis.owaspmobile2024.forEach((owaspmobile2024) => {
+        if (risks.includes(analysis.get('risk'))) {
+          owaspmobile2024s.push(owaspmobile2024.id);
+        }
+      });
+    });
+
+    return owaspmobile2024s;
+  }
+
   get owaspData(): OwaspChartDataOptions {
     // { A1: 0, A2: 0, ... }
     const owaspACounts = Array.from(Array(10)).reduce(
@@ -50,17 +80,35 @@ export default class FileDetailsComplianceInsightsComponent extends Component<Fi
       {}
     ) as Record<string, number>;
 
-    this.owasps.forEach((owasp) => {
-      const [key] = owasp.split('_');
+    // Iterate through owaspmobile2024s if available
+    if (this.owaspmobile2024s.length > 0) {
+      this.owaspmobile2024s.forEach((owaspmobile2024) => {
+        const [key] = owaspmobile2024.split('_');
 
-      if (key && typeof owaspACounts[key] !== 'undefined') {
-        owaspACounts[key]++;
-      }
+        if (key && typeof owaspACounts[key] !== 'undefined') {
+          owaspACounts[key]++;
+        }
 
-      if (key && typeof owaspMCounts[key] !== 'undefined') {
-        owaspMCounts[key]++;
-      }
-    });
+        if (key && typeof owaspMCounts[key] !== 'undefined') {
+          owaspMCounts[key]++;
+        }
+      });
+    } else {
+      // Iterate through owasps if owaspmobile2024s is empty
+      this.owasps.forEach((owasp) => {
+        const [key] = owasp.split('_');
+
+        if (key && typeof owaspACounts[key] !== 'undefined') {
+          owaspACounts[key]++;
+        }
+
+        if (key && typeof owaspMCounts[key] !== 'undefined') {
+          owaspMCounts[key]++;
+        }
+      });
+    }
+
+    this.loadData.perform();
 
     return {
       mobileOptions: this.createChartOptions(owaspMCounts),
@@ -68,29 +116,31 @@ export default class FileDetailsComplianceInsightsComponent extends Component<Fi
     };
   }
 
-  get tooltipLabels() {
-    return {
-      A1: 'Injection',
-      A2: 'Broken Authentication and Session Management',
-      A3: 'Cross Site Scripting',
-      A4: 'IDOR',
-      A5: 'Security Misconfiguration',
-      A6: 'Sensitive Data Exposure',
-      A7: 'Missing function ACL',
-      A8: 'CSRF',
-      A9: 'Using components with known vulnerabilities',
-      A10: 'Unvalidated Redirects and Forwards',
-      M1: 'Improper Platform Usage',
-      M2: 'Insecure Data Storage',
-      M3: 'Insecure Communication',
-      M4: 'Insecure Authentication',
-      M5: 'Insufficient Cryptography',
-      M6: 'Insecure Authorization',
-      M7: 'Client Code Quality',
-      M8: 'Code Tampering',
-      M9: 'Reverse Engineering',
-      M10: 'Extraneous Functionality',
-    };
+  loadData = task(async () => {
+    let data: (OwaspMobile2024Model | OwaspModel)[] | undefined = undefined;
+
+    if (this.owaspmobile2024s.length > 0) {
+      const owaspmobile2024Data = await this.store.findAll('owaspmobile2024');
+      data = owaspmobile2024Data.toArray();
+    } else if (this.owaspmobile2024s.length === 0 && this.owasps.length > 0) {
+      const owaspData = await this.store.findAll('owasp');
+      data = owaspData.toArray();
+    }
+
+    const tooltipLabels: { [key: string]: string } = {};
+
+    if (data) {
+      data.forEach((item: { code: string; title: string }) => {
+        const key = item.code.split(':')[0] as keyof typeof tooltipLabels;
+        tooltipLabels[key] = item.title;
+      });
+    }
+
+    this.owaspTitles = tooltipLabels;
+  });
+
+  get tooltipLabels(): { [key: string]: string } {
+    return this.owaspTitles;
   }
 
   createChartOptions(dataset: Record<string, number>): ECOption {
@@ -103,7 +153,9 @@ export default class FileDetailsComplianceInsightsComponent extends Component<Fi
         show: true,
         formatter: (params: any) => {
           const label: string =
-            this.tooltipLabels[params.name as keyof typeof this.tooltipLabels];
+            this.tooltipLabels[
+              params.name as keyof typeof this.tooltipLabels
+            ] || 'unknown';
 
           return `${label} <br/> ${params.name}: ${params.value}`;
         },
