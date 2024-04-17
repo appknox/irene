327f2086ad8abfb93380b438ed540e0a01b8afc0
Avi Shah <avishah1904@gmail.com>
2024-04-17 13:16:31 +0530

    register pages UI revamp

---
+++ tests/integration/components/via-login-page-test.js
@@ -0,0 +1,152 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, fillIn, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import Service from '@ember/service';
+import { Response } from 'miragejs';
+
+class NotificationsStub extends Service {
+  errorMsg = null;
+  successMsg = null;
+
+  error(msg) {
+    this.errorMsg = msg;
+  }
+  success(msg) {
+    this.successMsg = msg;
+  }
+}
+
+module('Integration | Component | via-login-page', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+  setupMirage(hooks);
+
+  test('it renders', async function (assert) {
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{false}} @showLoginLink={{true}} />`
+    );
+
+    assert.dom('[data-test-registration-form]').exists();
+
+    assert
+      .dom('[data-test-registration-form-title]')
+      .hasText('t:completeRegistration:()');
+
+    assert.dom('[data-test-register-btn]').exists();
+  });
+
+  test('it should render email and company name always', async function (assert) {
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{false}} @showLoginLink={{true}} />`
+    );
+
+    assert.dom('[data-test-registration-input-email]').exists();
+
+    assert.dom('[data-test-registration-input-company]').exists();
+  });
+
+  test('it should render first name and last name if enable_name is true', async function (assert) {
+    this.set('enable_name', false);
+
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{false}}></UserRegistration::ViaLoginPage>`
+    );
+
+    assert.dom('[data-test-registration-input-fname]').doesNotExist();
+
+    assert.dom('[data-test-registration-input-lname]').doesNotExist();
+
+    this.set('enable_name', true);
+
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{true}}></UserRegistration::ViaLoginPage>`
+    );
+
+    assert.dom('[data-test-registration-input-fname]').exists();
+
+    assert.dom('[data-test-registration-input-lname]').exists();
+  });
+
+  test('it should render login link', async function (assert) {
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{false}} @showLoginLink={{true}} />`
+    );
+
+    assert.dom('[data-test-login-link]').exists();
+  });
+
+  test('it should show success message after registration', async function (assert) {
+    assert.expect(12);
+
+    this.server.post('/v2/registration', (_, req) => {
+      const body = JSON.parse(req.requestBody);
+
+      assert.strictEqual(body.email, 'a@a.com');
+      assert.strictEqual(body.company, 'company');
+      assert.strictEqual(body.recaptcha, 'notenabled');
+
+      return new Response(201);
+    });
+
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{false}} @showLoginLink={{true}} />`
+    );
+
+    assert.dom('[data-test-register-btn]').exists().isDisabled();
+
+    await fillIn('[data-test-registration-input-email]', 'a@a.com');
+
+    await fillIn('[data-test-registration-input-company]', 'company');
+
+    assert.dom('[data-test-register-btn]').exists().isNotDisabled();
+
+    await click('[data-test-register-btn]');
+
+    assert.dom('[data-test-registration-confirm]').exists();
+
+    assert
+      .dom('[data-test-register-confirm-title]')
+      .exists()
+      .hasText('t:registerConfirmation:()');
+
+    assert
+      .dom('[data-test-register-confirm-message]')
+      .exists()
+      .hasText('t:checkEmail:()');
+  });
+
+  test('it should show error message if registration fails', async function (assert) {
+    this.owner.register('service:notifications', NotificationsStub);
+
+    const notify = this.owner.lookup('service:notifications');
+
+    this.server.post('/v2/registration', () => {
+      return new Response(
+        404,
+        {},
+        {
+          detail: 'Not found',
+        }
+      );
+    });
+
+    await render(
+      hbs`<UserRegistration::ViaLoginPage @enable_name={{false}} @showLoginLink={{true}} />`
+    );
+
+    assert.dom('[data-test-register-btn]').exists().isDisabled();
+
+    await fillIn('[data-test-registration-input-email]', 'a@a.com');
+
+    await fillIn('[data-test-registration-input-company]', 'company');
+
+    assert.dom('[data-test-register-btn]').exists().isNotDisabled();
+
+    await click('[data-test-register-btn]');
+
+    assert.ok(notify.errorMsg);
+  });
+});
---
+++ tests/integration/components/via-partner-invite-test.js
@@ -0,0 +1,219 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, fillIn, find, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl, t } from 'ember-intl/test-support';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import Service from '@ember/service';
+import { Response } from 'miragejs';
+
+class NotificationsStub extends Service {
+  errorMsg = null;
+  successMsg = null;
+
+  error(msg) {
+    this.errorMsg = msg;
+  }
+  success(msg) {
+    this.successMsg = msg;
+  }
+}
+
+module(
+  'Integration | Component | user-registration/via-partner-invite',
+  function (hooks) {
+    setupRenderingTest(hooks);
+    setupIntl(hooks);
+    setupMirage(hooks);
+
+    hooks.beforeEach(function () {
+      this.server.get('/v2/registration-via-invite', () => {
+        return new Response(200);
+      });
+    });
+
+    test('it renders', async function (assert) {
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      assert.dom('[data-test-registration-form]').exists();
+
+      assert
+        .dom('[data-test-registration-form-title]')
+        .hasText('t:completeRegistration:()');
+
+      assert.dom('[data-test-registration-terms-accept]').exists();
+
+      assert.dom('[data-test-register-btn]').exists();
+    });
+
+    test('it should render email and company name always', async function (assert) {
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      assert.dom('[data-test-registration-input-email]').exists();
+
+      assert.dom('[data-test-registration-input-company]').exists();
+
+      assert.dom('[data-test-registration-input-email]');
+
+      assert.dom('[data-test-registration-input-company]');
+    });
+
+    test('it should show validation error for empty username and password too short', async function (assert) {
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      assert.dom('[data-test-register-btn]').exists().isDisabled();
+
+      await fillIn('[data-test-registration-input-username]', '');
+
+      await fillIn('[data-test-registration-input-password]', 'test');
+
+      assert
+        .dom('[data-test-helper-text="username"]')
+        .hasText("Username can't be blank");
+
+      assert
+        .dom('[data-test-helper-text="password"]')
+        .hasText('Password is too short (minimum is 10 characters)');
+    });
+
+    test('register button should be enabled for valid entries', async function (assert) {
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      assert.dom('[data-test-register-btn]').exists().isDisabled();
+
+      await fillIn('[data-test-registration-input-username]', 'test');
+
+      await fillIn('[data-test-registration-input-password]', 'test@12345');
+
+      await fillIn(
+        '[data-test-registration-input-confirm-password]',
+        'test@12345'
+      );
+
+      await click('[data-test-registration-terms-accept]');
+
+      assert.dom('[data-test-register-btn]').exists().isNotDisabled();
+    });
+
+    test('it should send proper registration request', async function (assert) {
+      assert.expect(8);
+
+      this.server.post('/v2/registration-via-invite', (_, req) => {
+        const body = req.requestBody;
+
+        const data = body
+          .split('&')
+          .map((pair) => pair.split('='))
+          .reduce((acc, [key, value]) => {
+            value = decodeURIComponent(value.replace(/\+/g, ' '));
+            return { ...acc, [key]: value };
+          }, {});
+
+        assert.strictEqual(data.username, 'test');
+        assert.strictEqual(data.password, 'test@12345');
+        assert.strictEqual(data.confirm_password, 'test@12345');
+        assert.strictEqual(data.terms_accepted, 'true');
+
+        return new Response(201);
+      });
+
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      assert.dom('[data-test-register-btn]').exists().isDisabled();
+
+      await fillIn('[data-test-registration-input-username]', 'test');
+
+      await fillIn('[data-test-registration-input-password]', 'test@12345');
+
+      await fillIn(
+        '[data-test-registration-input-confirm-password]',
+        'test@12345'
+      );
+
+      await click('[data-test-registration-terms-accept]');
+
+      assert.dom('[data-test-register-btn]').exists().isNotDisabled();
+
+      await click('[data-test-register-btn]');
+    });
+
+    test('it should show error message if registration fails', async function (assert) {
+      this.owner.register('service:notifications', NotificationsStub);
+
+      const notify = this.owner.lookup('service:notifications');
+
+      this.server.post('/v2/registration-via-invite', () => {
+        return new Response(500, {}, { error: 'Server Error (500)' });
+      });
+
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      await fillIn('[data-test-registration-input-username]', 'test');
+
+      await fillIn('[data-test-registration-input-password]', 'test@12345');
+
+      await fillIn(
+        '[data-test-registration-input-confirm-password]',
+        'test@12345'
+      );
+
+      await click('[data-test-registration-terms-accept]');
+
+      assert.dom('[data-test-register-btn]').exists().isNotDisabled();
+
+      await click('[data-test-register-btn]');
+
+      assert.ok(notify.errorMsg);
+    });
+
+    test('it should show invalid page if token is invalid', async function (assert) {
+      this.server.get('/v2/registration-via-invite', () => {
+        return new Response(400, {}, { token: ['Invalid token'] });
+      });
+
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      assert.dom('[data-test-token-error]').exists();
+
+      assert.dom('[data-test-error-SvgIcon]').exists();
+
+      assert.dom('[data-test-error-text]').hasText(t('somethingWentWrong'));
+
+      assert.strictEqual(
+        find('[data-test-error-helperText]').innerHTML,
+        t('invalidTokenError', { htmlSafe: true }).toString()
+      );
+    });
+
+    test('it should show error helper text when there is registration error', async function (assert) {
+      this.server.post('/v2/registration-via-invite', () => {
+        return new Response(
+          400,
+          {},
+          { password: ['This password is too common'] }
+        );
+      });
+
+      await render(hbs`<UserRegistration::ViaPartnerInvite />`);
+
+      await fillIn('[data-test-registration-input-username]', 'test');
+
+      await fillIn('[data-test-registration-input-password]', 'test@12345');
+
+      await fillIn(
+        '[data-test-registration-input-confirm-password]',
+        'test@12345'
+      );
+
+      await click('[data-test-registration-terms-accept]');
+
+      assert.dom('[data-test-register-btn]').exists().isNotDisabled();
+
+      await click('[data-test-register-btn]');
+
+      assert
+        .dom('[data-test-helper-text="password"]')
+        .hasText('This password is too common');
+    });
+  }
+);
