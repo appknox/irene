a871b0e9a9ba5fca476e2f40100a8a24eff6ec8b
Sam David <samd948@gmail.com>
2024-09-03 15:47:47 +0530

    billing page global css refactor

--- app/components/organization-billing/invoice-list/index.ts
+++ app/components/organization-billing/invoice-list/index.ts
@@ -1,17 +1,20 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-
-import Store from '@ember-data/store';
 import { task } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
-import InvoiceModel from 'irene/models/invoice';
+import type IntlService from 'ember-intl/services/intl';
+
 // eslint-disable-next-line ember/use-ember-data-rfc-395-imports
-import DS from 'ember-data';
+import type DS from 'ember-data';
+import type Store from '@ember-data/store';
+
+import type InvoiceModel from 'irene/models/invoice';
 
 type InvoicesResponse = DS.AdapterPopulatedRecordArray<InvoiceModel>;
 
 export default class OrganizationBillingInvoiceListComponent extends Component {
   @service declare store: Store;
+  @service declare intl: IntlService;
 
   @tracked invoices: InvoicesResponse | null = null;
 
@@ -21,13 +24,52 @@ export default class OrganizationBillingInvoiceListComponent extends Component {
     this.fetchInvoices.perform();
   }
 
+  get columns() {
+    return [
+      {
+        name: this.intl.t('invoiceId'),
+        valuePath: 'invoiceId',
+        textAlign: 'center',
+      },
+      {
+        name: this.intl.t('planName'),
+        valuePath: 'planName',
+        textAlign: 'center',
+      },
+      {
+        name: this.intl.t('amountPaid'),
+        valuePath: 'amount',
+        textAlign: 'center',
+      },
+      {
+        name: this.intl.t('date'),
+        valuePath: 'paidDate',
+        textAlign: 'center',
+      },
+      {
+        name: this.intl.t('status'),
+        valuePath: 'paidStatus',
+        textAlign: 'center',
+      },
+      {
+        name: this.intl.t('invoice'),
+        component: 'organization-billing/invoice-list/download-action' as const,
+        textAlign: 'center',
+      },
+    ];
+  }
+
+  get invoiceList() {
+    return this.invoices?.slice() || [];
+  }
+
+  get hasInvoices() {
+    return this.invoiceList.length > 0;
+  }
+
   fetchInvoices = task(async () => {
     this.invoices = (await this.store.findAll('invoice')) as InvoicesResponse;
   });
-
-  get hasInvoices() {
-    return (this.invoices?.toArray() || []).length > 0;
-  }
 }
 
 declare module '@glint/environment-ember-loose/registry' {
