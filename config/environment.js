a23154996bac1175fd6e224e966b59dd036de5af
Praseetha-KR <praseetha04@gmail.com>
2024-11-14 00:51:18 +0530

    Product version: 24.11, version: 17.1.1

--- config/environment.js
+++ config/environment.js
@@ -143,7 +143,7 @@ module.exports = function (environment) {
   var showLicense = handler.getBoolean('IRENE_SHOW_LICENSE');
   var ENV = {
     ENVHandlerCONST: ENVHandlerCONST,
-    productVersion: '24.10.2',
+    productVersion: '24.11',
     version: Date.now(),
     isDevknox: false,
     isAppknox: false,
