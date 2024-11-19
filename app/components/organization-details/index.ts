3ae97f0af175130628b69de7eee935d6eca21f11
yibaebi <elliot.yibaebi@yahoo.com>
2024-11-20 00:25:40 +0100

    complete page content alignment across all dashboard pages

--- app/components/organization-details/index.ts
+++ app/components/organization-details/index.ts
@@ -2,15 +2,20 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
-class OrganizationDetails extends Component {
-  @service intl;
-  @service me;
-  @service organization;
-  @service router;
-  @service('notifications') notify;
+import type IntlService from 'ember-intl/services/intl';
+import type RouterService from '@ember/routing/router-service';
+import type MeService from 'irene/services/me';
+import type OrganizationService from 'irene/services/organization';
+
+export default class OrganizationDetailsComponent extends Component {
+  @service declare intl: IntlService;
+  @service declare me: MeService;
+  @service declare organization: OrganizationService;
+  @service declare router: RouterService;
+  @service('notifications') declare notify: NotificationService;
 
   get isAdmin() {
-    return this.me.org.get('is_admin');
+    return this.me.org?.get('is_admin');
   }
 
   get tabItems() {
@@ -18,7 +23,7 @@ class OrganizationDetails extends Component {
       {
         id: 'namespaces',
         route: 'authenticated.dashboard.organization.namespaces',
-        badgeCount: this.organization.selected.namespacesCount,
+        badgeCount: this.organization.selected?.namespacesCount,
         hasBadge: this.isAdmin,
         label: this.intl.t('namespaces'),
         iconName: 'assignment',
@@ -27,7 +32,7 @@ class OrganizationDetails extends Component {
         id: 'users',
         route: 'authenticated.dashboard.organization.users',
         hidden: !this.isAdmin,
-        badgeCount: this.organization.selected.membersCount,
+        badgeCount: this.organization.selected?.membersCount,
         hasBadge: true,
         label: this.intl.t('users'),
         iconName: 'people',
@@ -35,7 +40,7 @@ class OrganizationDetails extends Component {
       {
         id: 'teams',
         route: 'authenticated.dashboard.organization.teams',
-        badgeCount: this.organization.selected.teamsCount,
+        badgeCount: this.organization.selected?.teamsCount,
         hasBadge: this.isAdmin,
         label: this.intl.t('teams'),
         iconName: 'groups',
@@ -48,4 +53,8 @@ class OrganizationDetails extends Component {
   }
 }
 
-export default OrganizationDetails;
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    OrganizationDetails: typeof OrganizationDetailsComponent;
+  }
+}
