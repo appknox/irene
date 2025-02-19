5eaef1b43e19777a1e879ca5a15287472fb334a6
SmitGala <smitmanojgala@gmail.com>
2025-02-19 14:17:05 +0530

    License Page New UI

--- app/components/license-detail/index.ts
+++ app/components/license-detail/index.ts
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
 import dayjs from 'dayjs';
-import LicenseModel from 'irene/models/license';
+import { service } from '@ember/service';
+
+import type LicenseModel from 'irene/models/license';
+import type IntlService from 'ember-intl/services/intl';
 
 export interface LicenseDetailSignature {
   Args: {
@@ -9,12 +12,76 @@ export interface LicenseDetailSignature {
 }
 
 export default class LicenseDetailComponent extends Component<LicenseDetailSignature> {
+  @service declare intl: IntlService;
+
+  get license() {
+    return this.args.license;
+  }
+
   get startDate() {
-    return dayjs(this.args.license.startDate).format('DD MMM YYYY');
+    return dayjs(this.license?.startDate).format('DD MMM YYYY');
   }
 
   get expiryDate() {
-    return dayjs(this.args.license.expiryDate).format('DD MMM YYYY');
+    return dayjs(this.license?.expiryDate).format('DD MMM YYYY');
+  }
+
+  get featuresDetails() {
+    if (this.license?.isLimitedScans) {
+      return [
+        {
+          label: this.intl.t('noOfScans'),
+          value: this.license?.perScanQuantity,
+        },
+        {
+          label: this.intl.t('planType'),
+          value: this.license?.perScanDescription,
+        },
+      ];
+    }
+
+    return [
+      {
+        label: this.intl.t('noOfApps'),
+        value: this.license?.perAppQuantity,
+      },
+      {
+        label: this.intl.t('planType'),
+        value: this.license?.perAppDescription,
+      },
+    ];
+  }
+
+  get licenseSections() {
+    return [
+      {
+        icon: 'shape-line',
+        title: this.intl.t('feature'),
+        details: this.featuresDetails,
+      },
+      {
+        icon: 'calendar-month',
+        title: this.intl.t('validity'),
+        details: [
+          { label: this.intl.t('startDate'), value: this.startDate },
+          { label: this.intl.t('expiryDate'), value: this.expiryDate },
+        ],
+      },
+      {
+        icon: 'person',
+        title: this.intl.t('registration'),
+        details: [
+          {
+            label: this.intl.t('licenseKey'),
+            value: this.license?.key,
+          },
+          {
+            label: this.intl.t('licensedTo'),
+            value: `${this.license?.name} <${this.license?.email}>`,
+          },
+        ],
+      },
+    ];
   }
 }
 
