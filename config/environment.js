6ffbf14aa25e9c304ba41551d3cb61570a288a77
Sam David <samd948@gmail.com>
2025-03-27 17:30:23 +0530

    Release Appknox: 25.3.1, version: 19.0.1

--- config/environment.js
+++ config/environment.js
@@ -144,7 +144,7 @@ module.exports = function (environment) {
   var ENV = {
     ENVHandlerCONST: ENVHandlerCONST,
     productVersions: {
-      appknox: '25.3',
+      appknox: '25.3.1',
       storeknox: '25.3',
     },
     version: Date.now(),
