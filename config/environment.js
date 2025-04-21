7aa73767685b709e7c72bbc7cbad60844a5565f9
Elliot Yibaebi <yibaebi@appknox.com>
2025-04-21 11:42:51 +0100

    Bump Product Version: 25.3.2 ---> 25.4

--- config/environment.js
+++ config/environment.js
@@ -144,7 +144,7 @@ module.exports = function (environment) {
   var ENV = {
     ENVHandlerCONST: ENVHandlerCONST,
     productVersions: {
-      appknox: '25.3.2',
+      appknox: '25.4',
       storeknox: '25.3',
     },
     version: Date.now(),
