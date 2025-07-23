0ed6f7ae1fe6f9d5a74e549496242e2a641e01dc
Avi Shah <avishah1904@gmail.com>
2025-07-23 13:25:03 +0530

    add armorcode integration steps in marketplace

--- app/components/marketplace/plugin-card/index.ts
+++ app/components/marketplace/plugin-card/index.ts
@@ -1,7 +1,9 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 import { htmlSafe } from '@ember/template';
+import type IntlService from 'ember-intl/services/intl';
 
 interface MarketplacePluginCardSignature {
   Args: {
@@ -12,12 +14,22 @@ interface MarketplacePluginCardSignature {
       description: string;
       instructions: ReturnType<typeof htmlSafe> | string;
       logo: string;
+      modalHeading?: string;
     };
   };
 }
 
 export default class MarketplacePluginCardComponent extends Component<MarketplacePluginCardSignature> {
+  @service declare intl: IntlService;
+
   @tracked showInstructionsModal = false;
+
+  get modalHeading() {
+    return (
+      this.args.data.modalHeading ||
+      this.intl.t('integrateAppknoxToCICDPipeline')
+    );
+  }
 
   @action
   openInstructionsModal() {
