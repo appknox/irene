b5195d5c78365d4c36dc946c567a1b92a047cacd
yibaebi <elliot.yibaebi@yahoo.com>
2025-07-23 01:22:04 +0100

    complete va summary email notification integrations

---
+++ app/components/account-settings/notification-settings/settings-item/index.ts
@@ -0,0 +1,75 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { service } from '@ember/service';
+import type IntlService from 'ember-intl/services/intl';
+
+import ENV from 'irene/config/environment';
+import type AjaxService from 'irene/services/ajax';
+
+import type {
+  AccountSettingsNotificationSettings,
+  AccountSettingsNotificationSettingsToggleKey,
+} from 'irene/components/account-settings/notification-settings';
+
+export interface AccountSettingsNotificationSettingsItemSignature {
+  Element: HTMLElement;
+  Args: {
+    item: AccountSettingsNotificationSettings;
+    updateUserNotifPref: (
+      key: AccountSettingsNotificationSettingsToggleKey,
+      value: boolean
+    ) => void;
+  };
+}
+
+export default class AccountSettingsNotificationSettingsItemComponent extends Component<AccountSettingsNotificationSettingsItemSignature> {
+  @service declare ajax: AjaxService;
+  @service declare intl: IntlService;
+  @service('notifications') declare notify: NotificationService;
+
+  get isDisabled() {
+    return (
+      this.args.item.disable_toggling ||
+      this.toggleNotificationPreference.isRunning
+    );
+  }
+
+  get prefUpdateSuccessMessage() {
+    return this.intl.t('accountNotifSettings.notificationPreferenceUpdated');
+  }
+
+  get prefUpdateFailedMessage() {
+    return this.intl.t(
+      'accountNotifSettings.notificationPreferenceUpdateFailed'
+    );
+  }
+
+  @action
+  toggleSetting(_event: Event, checked?: boolean) {
+    this.toggleNotificationPreference.perform(!!checked);
+  }
+
+  toggleNotificationPreference = task(async (checked: boolean) => {
+    try {
+      const url = `${ENV.endpoints['userVaNotificationPref']}/update_user_va_notification_pref`;
+
+      await this.ajax.put(url, {
+        data: { [this.args.item.id]: checked },
+        namespace: ENV.namespace_v2,
+      });
+
+      this.args.updateUserNotifPref(this.args.item.id, checked);
+      this.notify.success(this.prefUpdateSuccessMessage);
+    } catch (error) {
+      this.args.updateUserNotifPref(this.args.item.id, !checked);
+      this.notify.error(this.prefUpdateFailedMessage);
+    }
+  });
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'AccountSettings::NotificationSettings::SettingsItem': typeof AccountSettingsNotificationSettingsItemComponent;
+  }
+}
---
+++ tests/unit/routes/authenticated/dashboard/account-settings/notification-settings-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module(
+  'Unit | Route | authenticated/dashboard/account-settings/notification-settings',
+  function (hooks) {
+    setupTest(hooks);
+
+    test('it exists', function (assert) {
+      let route = this.owner.lookup(
+        'route:authenticated/dashboard/account-settings/notification-settings'
+      );
+
+      assert.ok(route);
+    });
+  }
+);
