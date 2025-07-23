6458da1f9af192030469e4b25d43fec49d36d10e
yibaebi <elliot.yibaebi@yahoo.com>
2025-07-23 01:22:04 +0100

    complete va summary email notification integrations

--- app/router.ts
+++ app/router.ts
@@ -89,6 +89,7 @@ Router.map(function () {
         this.route('general');
         this.route('security');
         this.route('developersettings');
+        this.route('notification-settings');
       });
 
       this.route('billing');
@@ -236,6 +237,7 @@ Router.map(function () {
           this.route('general');
           this.route('security');
           this.route('developersettings');
+          this.route('notification-settings');
         });
 
         this.route('app-monitoring', { path: '/store-monitoring' });
