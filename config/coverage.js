f54789ffae892208f8bedb7a984c9b730fc3e42a
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2026-01-12 12:58:14 +0100

    --wip-- [skip ci]

--- config/coverage.js
+++ config/coverage.js
@@ -1,3 +1,6 @@
 module.exports = {
   useBabelInstrumenter: true,
+  reporters: ['lcov', 'json-summary', 'text-summary'],
+  coverageFolder: 'coverage',
+  excludes: ['**/mirage/**/*', '**/tests/**/*', '**/config/**/*'],
 };
