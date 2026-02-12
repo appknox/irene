e652550c143d76cd3e434077d3f047d206082011
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2026-02-12 15:55:54 +0100

    refactor: enhance SBOM UI test cases

--- mirage/factories/sbom-scan-summary.ts
+++ mirage/factories/sbom-scan-summary.ts
@@ -1,11 +1,13 @@
 import { Factory } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
-export default Factory.extend({
+export const SBOM_SCAN_SUMMARY_FACTORY_DEF = {
   component_count: faker.number.int(1000),
   library_count: faker.number.int(125),
   framework_count: faker.number.int(125),
   application_count: faker.number.int(125),
   file_count: faker.number.int(125),
   machine_learning_model_count: faker.number.int(125),
-});
+};
+
+export default Factory.extend(SBOM_SCAN_SUMMARY_FACTORY_DEF);
