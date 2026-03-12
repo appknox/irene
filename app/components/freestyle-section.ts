6a9f981cd5014c406b3667051548a226df609582
SmitGala <smitmanojgala@gmail.com>
2026-03-12 12:30:22 +0530

    POC: Ember Freestyle

---
+++ app/components/freestyle-section.ts
@@ -0,0 +1,7 @@
+import FreestyleSectionAddon from 'ember-freestyle/components/freestyle-section';
+
+export default class FreestyleSection extends FreestyleSectionAddon {
+  get show(): boolean {
+    return true;
+  }
+}
