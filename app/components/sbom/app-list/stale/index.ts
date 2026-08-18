fa48529c985b1a13f8587b1ded24b2c722432a56
avraichur96 <abhinavv@appknox.com>
2026-08-18 17:53:32 +0530

    update: component staleness marker

--- app/components/sbom/app-list/stale/index.ts
+++ app/components/sbom/app-list/stale/index.ts
@@ -1,37 +1,30 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+
 import type IntlService from 'ember-intl/services/intl';
 import type SbomProjectModel from 'irene/models/sbom-project';
 
-export interface SbomProjectListAppNameSignature {
+export interface SbomAppListStaleSignature {
   Args: {
     sbomProject: SbomProjectModel;
   };
 }
 
-export default class SbomProjectListAppNameComponent extends Component<SbomProjectListAppNameSignature> {
+export default class SbomAppListStaleComponent extends Component<SbomAppListStaleSignature> {
   @service declare intl: IntlService;
-
-  get packageName() {
-    return this.args.sbomProject.project.get('packageName');
-  }
-
-  get name() {
-    return this.args.sbomProject.project.get('lastFile')?.get('name');
-  }
 
   get isStale() {
     return Boolean(this.args.sbomProject.latestSbFile?.get('isStale'));
   }
 
-  get staleTooltip() {
+  get tooltip() {
     return this.intl.t('sbomModule.staleFileTooltip');
   }
 }
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
-    'Sbom::AppList::AppName': typeof SbomProjectListAppNameComponent;
-    'sbom/app-list/app-name': typeof SbomProjectListAppNameComponent;
+    'Sbom::AppList::Stale': typeof SbomAppListStaleComponent;
+    'sbom/app-list/stale': typeof SbomAppListStaleComponent;
   }
 }
